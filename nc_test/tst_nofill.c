121a0c5bf3d2e515d17960474d8517a03b8eed30
Russ Rew <russ@unidata.ucar.edu>
2011-04-25 03:19:44 +0000

    Removed unnecesssary staics, fixed comments.

--- nc_test/tst_nofill.c
+++ nc_test/tst_nofill.c
@@ -95,6 +95,11 @@ create_file(char *file_name, int fill_mode, size_t* sizehintp)
    int sfc_temp_dims[RANK_sfc_temp];
    int zonal_wnd_dims[RANK_zonal_wnd];
 
+   size_t zonal_wnd_start[RANK_zonal_wnd];
+   size_t zonal_wnd_count[RANK_zonal_wnd];
+   float zonal_wnd[LON_LEN*LAT_LEN*TIME_LEN];
+   int ii;
+
    int old_fill_mode;
    size_t default_initialsize = 0;
 
@@ -181,145 +186,164 @@ create_file(char *file_name, int fill_mode, size_t* sizehintp)
    stat = nc_enddef (ncid);
    check_err(stat,__LINE__,__FILE__);
 
-    static size_t time_start[RANK_time];
-    static size_t time_count[RANK_time];
-    static double time[] = {1.};
-    time_len = 1;
-    time_start[0] = 0;
-    time_count[0] = time_len;
+   {				/* store time */
+       size_t time_start[RANK_time];
+       size_t time_count[RANK_time];
+       double time[TIME_LEN] = {1.};
+       time_len = 1;
+       time_start[0] = 0;
+       time_count[0] = time_len;
+       stat = nc_put_vara_double(ncid, time_id, time_start, time_count, time);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store lat */
+       float lat[] = {90, 88.5, 87, 85.5, 84, 82.5, 81, 79.5, 78, 76.5, 75, 73.5, 72, 70.5, 69, 67.5, 66, 64.5, 63, 61.5, 60, 58.5, 57, 55.5, 54, 52.5, 51, 49.5, 48, 46.5, 45, 43.5, 42, 40.5, 39, 37.5, 36, 34.5, 33, 31.5, 30, 28.5, 27, 25.5, 24, 22.5, 21, 19.5, 18, 16.5, 15, 13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0, -1.5, -3, -4.5, -6, -7.5, -9, -10.5, -12, -13.5, -15, -16.5, -18, -19.5, -21, -22.5, -24, -25.5, -27, -28.5, -30, -31.5, -33, -34.5, -36, -37.5, -39, -40.5, -42, -43.5, -45, -46.5, -48, -49.5, -51, -52.5, -54, -55.5, -57, -58.5, -60, -61.5, -63, -64.5, -66, -67.5, -69, -70.5, -72, -73.5, -75, -76.5, -78, -79.5, -81, -82.5, -84, -85.5, -87, -88.5, -90};
+       stat = nc_put_var_float(ncid, lat_id, lat);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store lon */
+       float lon[] = {0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12, 13.5, 15, 16.5, 18, 19.5, 21, 22.5, 24, 25.5, 27, 28.5, 30, 31.5, 33, 34.5, 36, 37.5, 39, 40.5, 42, 43.5, 45, 46.5, 48, 49.5, 51, 52.5, 54, 55.5, 57, 58.5, 60, 61.5, 63, 64.5, 66, 67.5, 69, 70.5, 72, 73.5, 75, 76.5, 78, 79.5, 81, 82.5, 84, 85.5, 87, 88.5, 90, 91.5, 93, 94.5, 96, 97.5, 99, 100.5, 102, 103.5, 105, 106.5, 108, 109.5, 111, 112.5, 114, 115.5, 117, 118.5, 120, 121.5, 123, 124.5, 126, 127.5, 129, 130.5, 132, 133.5, 135, 136.5, 138, 139.5, 141, 142.5, 144, 145.5, 147, 148.5, 150, 151.5, 153, 154.5, 156, 157.5, 159, 160.5, 162, 163.5, 165, 166.5, 168, 169.5, 171, 172.5, 174, 175.5, 177, 178.5, 180, 181.5, 183, 184.5, 186, 187.5, 189, 190.5, 192, 193.5, 195, 196.5, 198, 199.5, 201, 202.5, 204, 205.5, 207, 208.5, 210, 211.5, 213, 214.5, 216, 217.5, 219, 220.5, 222, 223.5, 225, 226.5, 228, 229.5, 231, 232.5, 234, 235.5, 237, 238.5, 240, 241.5, 243, 244.5, 246, 247.5, 249, 250.5, 252, 253.5, 255, 256.5, 258, 259.5, 261, 262.5, 264, 265.5, 267, 268.5, 270, 271.5, 273, 274.5, 276, 277.5, 279, 280.5, 282, 283.5, 285, 286.5, 288, 289.5, 291, 292.5, 294, 295.5, 297, 298.5, 300, 301.5, 303, 304.5, 306, 307.5, 309, 310.5, 312, 313.5, 315, 316.5, 318, 319.5, 321, 322.5, 324, 325.5, 327, 328.5, 330, 331.5, 333, 334.5, 336, 337.5, 339, 340.5, 342, 343.5, 345, 346.5, 348, 349.5, 351, 352.5, 354, 355.5, 357, 358.5};
+       stat = nc_put_var_float(ncid, lon_id, lon);
+       check_err(stat,__LINE__,__FILE__);
+   }
 
-    static float lat[] = {90, 88.5, 87, 85.5, 84, 82.5, 81, 79.5, 78, 76.5, 75, 73.5, 72, 70.5, 69, 67.5, 66, 64.5, 63, 61.5, 60, 58.5, 57, 55.5, 54, 52.5, 51, 49.5, 48, 46.5, 45, 43.5, 42, 40.5, 39, 37.5, 36, 34.5, 33, 31.5, 30, 28.5, 27, 25.5, 24, 22.5, 21, 19.5, 18, 16.5, 15, 13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0, -1.5, -3, -4.5, -6, -7.5, -9, -10.5, -12, -13.5, -15, -16.5, -18, -19.5, -21, -22.5, -24, -25.5, -27, -28.5, -30, -31.5, -33, -34.5, -36, -37.5, -39, -40.5, -42, -43.5, -45, -46.5, -48, -49.5, -51, -52.5, -54, -55.5, -57, -58.5, -60, -61.5, -63, -64.5, -66, -67.5, -69, -70.5, -72, -73.5, -75, -76.5, -78, -79.5, -81, -82.5, -84, -85.5, -87, -88.5, -90};
+   {				/* store lvl */
+       float lvl[] = {1000, 995, 990, 985, 975, 950, 925, 900, 875, 850, 800, 750, 700, 600, 500, 450, 400, 350, 300, 275, 250, 225, 200, 175, 150, 100, 70, 50, 30, 20, 10};
+       stat = nc_put_var_float(ncid, lvl_id, lvl);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store sfc_pres */
+       size_t sfc_pres_start[RANK_sfc_pres];
+       size_t sfc_pres_count[RANK_sfc_pres];
+       float sfc_pres[LON_LEN*LAT_LEN];
+       
+       for(ii = 0; ii < LAT_LEN * LON_LEN; ii++) {
+	   sfc_pres[ii] = 6;
+       }
+       sfc_pres_start[0] = 0;
+       sfc_pres_start[1] = 0;
+       sfc_pres_start[2] = 0;
+       sfc_pres_count[0] = time_len;
+       sfc_pres_count[1] = lat_len;
+       sfc_pres_count[2] = lon_len;
+       stat = nc_put_vara_float(ncid, sfc_pres_id, sfc_pres_start, sfc_pres_count, sfc_pres);
+       check_err(stat,__LINE__,__FILE__);
+   }
 
-    static float lon[] = {0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12, 13.5, 15, 16.5, 18, 19.5, 21, 22.5, 24, 25.5, 27, 28.5, 30, 31.5, 33, 34.5, 36, 37.5, 39, 40.5, 42, 43.5, 45, 46.5, 48, 49.5, 51, 52.5, 54, 55.5, 57, 58.5, 60, 61.5, 63, 64.5, 66, 67.5, 69, 70.5, 72, 73.5, 75, 76.5, 78, 79.5, 81, 82.5, 84, 85.5, 87, 88.5, 90, 91.5, 93, 94.5, 96, 97.5, 99, 100.5, 102, 103.5, 105, 106.5, 108, 109.5, 111, 112.5, 114, 115.5, 117, 118.5, 120, 121.5, 123, 124.5, 126, 127.5, 129, 130.5, 132, 133.5, 135, 136.5, 138, 139.5, 141, 142.5, 144, 145.5, 147, 148.5, 150, 151.5, 153, 154.5, 156, 157.5, 159, 160.5, 162, 163.5, 165, 166.5, 168, 169.5, 171, 172.5, 174, 175.5, 177, 178.5, 180, 181.5, 183, 184.5, 186, 187.5, 189, 190.5, 192, 193.5, 195, 196.5, 198, 199.5, 201, 202.5, 204, 205.5, 207, 208.5, 210, 211.5, 213, 214.5, 216, 217.5, 219, 220.5, 222, 223.5, 225, 226.5, 228, 229.5, 231, 232.5, 234, 235.5, 237, 238.5, 240, 241.5, 243, 244.5, 246, 247.5, 249, 250.5, 252, 253.5, 255, 256.5, 258, 259.5, 261, 262.5, 264, 265.5, 267, 268.5, 270, 271.5, 273, 274.5, 276, 277.5, 279, 280.5, 282, 283.5, 285, 286.5, 288, 289.5, 291, 292.5, 294, 295.5, 297, 298.5, 300, 301.5, 303, 304.5, 306, 307.5, 309, 310.5, 312, 313.5, 315, 316.5, 318, 319.5, 321, 322.5, 324, 325.5, 327, 328.5, 330, 331.5, 333, 334.5, 336, 337.5, 339, 
-340.5, 342, 343.5, 345, 346.5, 348, 349.5, 351, 352.5, 354, 355.5, 357, 358.5};
-
-    static float lvl[] = {1000, 995, 990, 985, 975, 950, 925, 900, 875, 850, 800, 750, 700, 600, 500, 450, 400, 350, 300, 275, 250, 225, 200, 175, 150, 100, 70, 50, 30, 20, 10};
-
-    static size_t sfc_pres_start[RANK_sfc_pres];
-    static size_t sfc_pres_count[RANK_sfc_pres];
-    static float sfc_pres[LON_LEN*LAT_LEN];
-
-    sfc_pres_start[0] = 0;
-    sfc_pres_start[1] = 0;
-    sfc_pres_start[2] = 0;
-    sfc_pres_count[0] = time_len;
-    sfc_pres_count[1] = lat_len;
-    sfc_pres_count[2] = lon_len;
-
-    static size_t temp_scrn_start[RANK_temp_scrn];
-    static size_t temp_scrn_count[RANK_temp_scrn];
-    static float temp_scrn[LON_LEN*LAT_LEN];
-
-
-    temp_scrn_start[0] = 0;
-    temp_scrn_start[1] = 0;
-    temp_scrn_start[2] = 0;
-    temp_scrn_count[0] = time_len;
-    temp_scrn_count[1] = lat_len;
-    temp_scrn_count[2] = lon_len;
-
-    static size_t qsair_scrn_start[RANK_qsair_scrn];
-    static size_t qsair_scrn_count[RANK_qsair_scrn];
-    static float qsair_scrn[LON_LEN*LAT_LEN];
-
-    qsair_scrn_start[0] = 0;
-    qsair_scrn_start[1] = 0;
-    qsair_scrn_start[2] = 0;
-    qsair_scrn_count[0] = time_len;
-    qsair_scrn_count[1] = lat_len;
-    qsair_scrn_count[2] = lon_len;
-
-    static size_t topog_start[RANK_topog];
-    static size_t topog_count[RANK_topog];
-    static float topog[LON_LEN*LAT_LEN];
-    topog_start[0] = 0;
-    topog_start[1] = 0;
-    topog_start[2] = 0;
-    topog_count[0] = time_len;
-    topog_count[1] = lat_len;
-    topog_count[2] = lon_len;
-
-    static size_t mslp_start[RANK_mslp];
-    static size_t mslp_count[RANK_mslp];
-    static float mslp[LON_LEN*LAT_LEN];
-    mslp_start[0] = 0;
-    mslp_start[1] = 0;
-    mslp_start[2] = 0;
-    mslp_count[0] = time_len;
-    mslp_count[1] = lat_len;
-    mslp_count[2] = lon_len;
-
-    static size_t sfc_temp_start[RANK_sfc_temp];
-    static size_t sfc_temp_count[RANK_sfc_temp];
-    static float sfc_temp[LON_LEN*LAT_LEN];
-
-    sfc_temp_start[0] = 0;
-    sfc_temp_start[1] = 0;
-    sfc_temp_start[2] = 0;
-    sfc_temp_count[0] = time_len;
-    sfc_temp_count[1] = lat_len;
-    sfc_temp_count[2] = lon_len;
-
-    static size_t zonal_wnd_start[RANK_zonal_wnd];
-    static size_t zonal_wnd_count[RANK_zonal_wnd];
-    static float zonal_wnd[LON_LEN*LAT_LEN*TIME_LEN];
-
-    int ilat, ilon, itime, j3, j2;
-    /* Put non-zero values in data variables, so can distinguish
-     * nofill from fill cases. */
-    j3 = 0;
-    j2 = 0;
-    for(ilat = 0; ilat < LAT_LEN; ilat++) {
-	for(ilon = 0; ilon < LON_LEN; ilon++) {
-	    for(itime = 0; itime < TIME_LEN; itime++) {
-		zonal_wnd[j3] = 42;
-		j3++;
-	    }
-	    temp_scrn[j2] = 11;
-	    qsair_scrn[j2] = 22;
-	    topog[j2] = 33;
-	    mslp[j2] = 44;
-	    sfc_temp[j2] = 55;
-	    j2++;
-	}
-    }
-
-    stat = nc_put_vara_double(ncid, time_id, time_start, time_count, time);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_var_float(ncid, lat_id, lat);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_var_float(ncid, lon_id, lon);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_var_float(ncid, lvl_id, lvl);
-    check_err(stat,__LINE__,__FILE__);
-
-    stat = nc_put_vara_float(ncid, sfc_pres_id, sfc_pres_start, sfc_pres_count, sfc_pres);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_vara_float(ncid, temp_scrn_id, temp_scrn_start, temp_scrn_count, temp_scrn);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_vara_float(ncid, qsair_scrn_id, qsair_scrn_start, qsair_scrn_count, qsair_scrn);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_vara_float(ncid, topog_id, topog_start, topog_count, topog);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_vara_float(ncid, mslp_id, mslp_start, mslp_count, mslp);
-    check_err(stat,__LINE__,__FILE__);
-    stat = nc_put_vara_float(ncid, sfc_temp_id, sfc_temp_start, sfc_temp_count, sfc_temp);
-    check_err(stat,__LINE__,__FILE__);
-
-    for(i = LVL_LEN - 1; i>=0; i--)
-    {
-        zonal_wnd_start[0] = 0;
-        zonal_wnd_start[1] = i;
-        zonal_wnd_start[2] = 0;
-        zonal_wnd_start[3] = 0;
-        zonal_wnd_count[0] = time_len;
-        zonal_wnd_count[1] = 1;
-        zonal_wnd_count[2] = lat_len;
-        zonal_wnd_count[3] = lon_len;
-        stat = nc_put_vara_float(ncid, zonal_wnd_id, zonal_wnd_start, zonal_wnd_count, zonal_wnd);
-        check_err(stat,__LINE__,__FILE__);
-    }
-
+   {				/* store temp_scrn */
+       size_t temp_scrn_start[RANK_temp_scrn];
+       size_t temp_scrn_count[RANK_temp_scrn];
+       float temp_scrn[LON_LEN*LAT_LEN];
+       
+       for(ii = 0; ii < LAT_LEN * LON_LEN; ii++) {
+	   temp_scrn[ii] = 11;
+       }
+       temp_scrn_start[0] = 0;
+       temp_scrn_start[1] = 0;
+       temp_scrn_start[2] = 0;
+       temp_scrn_count[0] = time_len;
+       temp_scrn_count[1] = lat_len;
+       temp_scrn_count[2] = lon_len;
+       stat = nc_put_vara_float(ncid, temp_scrn_id, temp_scrn_start, temp_scrn_count, temp_scrn);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store qsair_scrn */
+       size_t qsair_scrn_start[RANK_qsair_scrn];
+       size_t qsair_scrn_count[RANK_qsair_scrn];
+       float qsair_scrn[LON_LEN*LAT_LEN];
+       
+       for(ii = 0; ii < LAT_LEN * LON_LEN; ii++) {
+	   qsair_scrn[ii] = 22;
+       }
+       qsair_scrn_start[0] = 0;
+       qsair_scrn_start[1] = 0;
+       qsair_scrn_start[2] = 0;
+       qsair_scrn_count[0] = time_len;
+       qsair_scrn_count[1] = lat_len;
+       qsair_scrn_count[2] = lon_len;
+       stat = nc_put_vara_float(ncid, qsair_scrn_id, qsair_scrn_start, qsair_scrn_count, qsair_scrn);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store topog */
+       size_t topog_start[RANK_topog];
+       size_t topog_count[RANK_topog];
+       float topog[LON_LEN*LAT_LEN];
+       
+       for(ii = 0; ii < LAT_LEN * LON_LEN; ii++) {
+	   topog[ii] = 33;
+       }
+       topog_start[0] = 0;
+       topog_start[1] = 0;
+       topog_start[2] = 0;
+       topog_count[0] = time_len;
+       topog_count[1] = lat_len;
+       topog_count[2] = lon_len;
+       stat = nc_put_vara_float(ncid, topog_id, topog_start, topog_count, topog);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store mslp */
+       size_t mslp_start[RANK_mslp];
+       size_t mslp_count[RANK_mslp];
+       float mslp[LON_LEN*LAT_LEN];
+       
+       for(ii = 0; ii < LAT_LEN * LON_LEN; ii++) {
+	   mslp[ii] = 44;
+       }
+       mslp_start[0] = 0;
+       mslp_start[1] = 0;
+       mslp_start[2] = 0;
+       mslp_count[0] = time_len;
+       mslp_count[1] = lat_len;
+       mslp_count[2] = lon_len;
+       stat = nc_put_vara_float(ncid, mslp_id, mslp_start, mslp_count, mslp);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {				/* store sfc_temp */
+       size_t sfc_temp_start[RANK_sfc_temp];
+       size_t sfc_temp_count[RANK_sfc_temp];
+       float sfc_temp[LON_LEN*LAT_LEN];
+       
+       for(ii = 0; ii < LAT_LEN * LON_LEN; ii++) {
+	   sfc_temp[ii] = 55;
+       }
+       sfc_temp_start[0] = 0;
+       sfc_temp_start[1] = 0;
+       sfc_temp_start[2] = 0;
+       sfc_temp_count[0] = time_len;
+       sfc_temp_count[1] = lat_len;
+       sfc_temp_count[2] = lon_len;
+       stat = nc_put_vara_float(ncid, sfc_temp_id, sfc_temp_start, sfc_temp_count, sfc_temp);
+       check_err(stat,__LINE__,__FILE__);
+   }
+   
+   {		      /* store zonal_wnd */
+       /* Bug exposed when written in reverse order. */
+       /* for(i = LVL_LEN - 1; i>=0; i--) */
+       for(i = 0; i < LVL_LEN; i++)
+       {
+	   int izw;
+	   for(izw = 0; izw < TIME_LEN * LAT_LEN * LON_LEN; izw++) {
+	       zonal_wnd[izw] = 100 + i;
+	   }
+	   zonal_wnd_start[0] = 0;
+	   zonal_wnd_start[1] = i;
+	   zonal_wnd_start[2] = 0;
+	   zonal_wnd_start[3] = 0;
+	   zonal_wnd_count[0] = time_len;
+	   zonal_wnd_count[1] = 1;
+	   zonal_wnd_count[2] = lat_len;
+	   zonal_wnd_count[3] = lon_len;
+	   stat = nc_put_vara_float(ncid, zonal_wnd_id, zonal_wnd_start, zonal_wnd_count, zonal_wnd);
+	   check_err(stat,__LINE__,__FILE__);
+       }
+   }
    stat = nc_close(ncid);
    check_err(stat,__LINE__,__FILE__);
    return 0;
@@ -331,7 +355,7 @@ main(int argc, char **argv)
     size_t sizehint = 2100000;	/* default if not set on command line,
 				 * exposes bug.  It turns out any
 				 * value between 2091953 and 2150032
-				 * exposes the bug, whereas all other
+				 * triggers bug, whereas all other
 				 * values work fine. */
 
     if (argc > 1) {
