d1a6aa38d8298f672a648879db9360048949a8db
Ed Hartnett <ed@unidata.ucar.edu>
2011-05-19 17:36:47 +0000

    found HDF5 dimscale bug

--- nc_test4/tst_sync.c
+++ nc_test4/tst_sync.c
@@ -16,28 +16,6 @@ int
 main(int argc, char **argv)
 {
    printf("\n*** Testing netcdf-4 variable syncing.\n");
-   printf("**** testing that sync works in netCDF-4, with test contributed by Jeff W....");
-   {
-      int ncid, var1_id, var2_id, var3_id, dimid;
-
-      nc_set_log_level(3);
-      
-      /* Create a file with one dim and two variables. */
-      if (nc_create(FILE_NAME, NC_NETCDF4, &ncid)) ERR;
-      if (nc_def_dim(ncid, DIM_NAME, 1, &dimid)) ERR;
-      if (nc_def_var(ncid, VAR1_NAME, NC_FLOAT, 1, &dimid, &var1_id)) ERR;
-      if (nc_def_var(ncid, VAR2_NAME, NC_FLOAT, 1,&dimid, &var2_id)) ERR;
-
-      /* Sync the file and add a coordinate variable. */
-      if (nc_sync(ncid)) ERR;
-      if (nc_def_var(ncid, DIM_NAME, NC_FLOAT, 1, &dimid, &var3_id)) ERR;
-
-      /* Close the file. */
-      if (nc_close(ncid)) ERR;
-      nc_set_log_level(0);
-   }
-   SUMMARIZE_ERR;
-#ifdef EXTRA_TESTS
    printf("**** testing that sync works in netCDF-4...");
    {
       int ncid, var1_id, var2_id, var3_id, dimid;
@@ -65,6 +43,28 @@ main(int argc, char **argv)
       if (strcmp(name_in, DIM_NAME) || xtype_in != NC_FLOAT || ndims != 1 || 
 	  dimid_in != 0 || natts != 0) ERR;
       if (nc_close(ncid)) ERR;
+   }
+   SUMMARIZE_ERR;
+#ifdef EXTRA_TESTS
+   printf("**** testing that sync works in netCDF-4, with test contributed by Jeff W....");
+   {
+      int ncid, var1_id, var2_id, var3_id, dimid;
+
+      nc_set_log_level(3);
+      
+      /* Create a file with one dim and two variables. */
+      if (nc_create(FILE_NAME, NC_NETCDF4, &ncid)) ERR;
+      if (nc_def_dim(ncid, DIM_NAME, 1, &dimid)) ERR;
+      if (nc_def_var(ncid, VAR1_NAME, NC_FLOAT, 1, &dimid, &var1_id)) ERR;
+      if (nc_def_var(ncid, VAR2_NAME, NC_FLOAT, 1,&dimid, &var2_id)) ERR;
+
+      /* Sync the file and add a coordinate variable. */
+      if (nc_sync(ncid)) ERR;
+      if (nc_def_var(ncid, DIM_NAME, NC_FLOAT, 1, &dimid, &var3_id)) ERR;
+
+      /* Close the file. */
+      if (nc_close(ncid)) ERR;
+      nc_set_log_level(0);
    }
    SUMMARIZE_ERR;
    printf("**** testing that sync works in netCDF-4, with test contributed by Jeff W....");
