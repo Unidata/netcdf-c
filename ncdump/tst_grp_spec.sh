d64a345918f288bb4d1849a67dada57f1a9d5dd8
Russ Rew <russ@unidata.ucar.edu>
2011-09-21 15:36:00 +0000

    added test for -g option

---
+++ ncdump/tst_grp_spec.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This shell script tests ncdump -g option for specifying groups for
+# which data is to be output.
+# $Id$
+
+set -e
+echo ""
+echo "*** Testing ncdump -g output for specifying group subsets"
+echo "*** creating netcdf file tst_grp_spec.nc from tst_grp_spec.cdl..."
+../ncgen/ncgen -k3 -b $srcdir/tst_grp_spec.cdl
+echo "*** creating tmpgs.cdl from tst_grp_spec.nc with ncdump -g ..."
+./ncdump -g g1,g4 tst_grp_spec.nc > tmpgs.cdl
+echo "*** comparing tmp.cdl with ref_tst_grp_spec.cdl..."
+diff tmpgs.cdl $srcdir/ref_tst_grp_spec.cdl
+
+echo
+echo "*** All ncdump test output for -g option passed!"
+exit 0
