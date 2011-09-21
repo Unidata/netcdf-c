dbd0e9c0b3277762ab24d6a794fd1cdc059d1ef3
Russ Rew <russ@unidata.ucar.edu>
2011-09-21 19:51:55 +0000

    resolved conflicts and errors after merging trunk changes into my branch

--- ncdump/ncdump.c
+++ ncdump/ncdump.c
@@ -22,7 +22,7 @@ Research/Unidata. See \ref copyright file for more info.  */
 #include <netcdf.h>
 #include "utils.h"
 #include "nccomps.h"
-#include "nctime.h"		/* new iso time and calendar stuff */
+#include "nctime0.h"		/* new iso time and calendar stuff */
 #include "dumplib.h"
 #include "ncdump.h"
 #include "vardata.h"
--- ncdump/tst_grp_spec.sh
+++ ncdump/tst_grp_spec.sh
@@ -7,11 +7,11 @@ set -e
 echo ""
 echo "*** Testing ncdump -g output for specifying group subsets"
 echo "*** creating netcdf file tst_grp_spec.nc from tst_grp_spec.cdl..."
-../ncgen/ncgen -k3 -b $srcdir/tst_grp_spec.cdl
+../ncgen/ncgen -k3 -b tst_grp_spec.cdl
 echo "*** creating tmpgs.cdl from tst_grp_spec.nc with ncdump -g ..."
 ./ncdump -g g1,g4 tst_grp_spec.nc > tmpgs.cdl
 echo "*** comparing tmp.cdl with ref_tst_grp_spec.cdl..."
-diff tmpgs.cdl $srcdir/ref_tst_grp_spec.cdl
+diff tmpgs.cdl ref_tst_grp_spec.cdl
 
 echo
 echo "*** All ncdump test output for -g option passed!"
