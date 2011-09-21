f354295303b0f41b54fc74d69e7fdfe12910d9be
Russ Rew <russ@unidata.ucar.edu>
2011-09-21 22:39:18 +0000

    Bug fixes in tests for -g

--- ncdump/tst_grp_spec.sh
+++ ncdump/tst_grp_spec.sh
@@ -6,12 +6,12 @@
 set -e
 echo ""
 echo "*** Testing ncdump -g output for specifying group subsets"
-echo "*** creating netcdf file tst_grp_spec.nc from tst_grp_spec.cdl..."
-../ncgen/ncgen -k3 -b tst_grp_spec.cdl
-echo "*** creating tmpgs.cdl from tst_grp_spec.nc with ncdump -g ..."
-./ncdump -g g1,g4 tst_grp_spec.nc > tmpgs.cdl
-echo "*** comparing tmp.cdl with ref_tst_grp_spec.cdl..."
-diff tmpgs.cdl ref_tst_grp_spec.cdl
+echo "*** creating netcdf file tst_grp_spec.nc from ref_tst_grp_spec0.cdl..."
+../ncgen/ncgen -k3 -o tmp_all.nc -b $srcdir/ref_tst_grp_spec0.cdl
+echo "*** creating tmp_subset.cdl from tmp_all.nc with ncdump -g ..."
+./ncdump -g g1,g4 -n tmp_subset tmp_all.nc > tmp_subset.cdl
+echo "*** comparing tmp_subset.cdl with ref_tst_grp_spec.cdl..."
+diff tmp_subset.cdl $srcdir/ref_tst_grp_spec.cdl
 
 echo
 echo "*** All ncdump test output for -g option passed!"
