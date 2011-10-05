31d81f85ab4f5c7d03759faa6095de0af9b1b14b
Ed Hartnett <ed@unidata.ucar.edu>
2011-10-05 13:43:20 +0000

    enhanced knmi performance testing

--- nc_test4/tst_knmi.c
+++ nc_test4/tst_knmi.c
@@ -30,8 +30,13 @@ redistribution conditions.
 static int 
 read_file(char *filename)
 {
-   int ncid;
+#define CWP "cwp"
+#define XLEN 3712
+#define YLEN 3712
+
+   int ncid, varid;
    struct timeval start_time, end_time, diff_time;
+   short *data;
    int time_us;
 
    printf("**** reading file %s\n", filename);
@@ -41,6 +46,16 @@ read_file(char *filename)
    if (nc4_timeval_subtract(&diff_time, &end_time, &start_time)) ERR_RET;
    time_us = (int)diff_time.tv_sec * MILLION + (int)diff_time.tv_usec;
    printf("File open time (us): %d\n", (int)time_us);
+
+   if (!(data = malloc(sizeof(short) * XLEN * YLEN))) ERR;
+   if (gettimeofday(&start_time, NULL)) ERR_RET;
+   if (nc_inq_varid(ncid, CWP, &varid)) ERR;
+   if (nc_get_var_short(ncid, varid, data)) ERR;
+   if (gettimeofday(&end_time, NULL)) ERR_RET;
+   if (nc4_timeval_subtract(&diff_time, &end_time, &start_time)) ERR_RET;
+   time_us = (int)diff_time.tv_sec * MILLION + (int)diff_time.tv_usec;
+   printf("Data read time (us): %d\n", (int)time_us);
+   free(data);
 
    if (nc_close(ncid))
       ERR_RET;
@@ -65,8 +80,8 @@ main(int argc, char **argv)
    struct timeval start_time, end_time, diff_time;
 
    printf("\n*** Testing netcdf-4 vs. netcdf-3 performance.\n");
-   /* if (read_file(FILE_NAME_1)) ERR; */
-   /* if (read_file(FILE_NAME_2)) ERR; */
+   if (read_file(FILE_NAME_1)) ERR; 
+   if (read_file(FILE_NAME_2)) ERR; 
 
    SUMMARIZE_ERR;
    FINAL_RESULTS;
