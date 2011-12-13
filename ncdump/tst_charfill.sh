66488a47732fcdea013058f6b401b0ab083b1d5a
Dennis Heimbigner <dmh@unidata.ucar.edu>
2011-12-13 03:08:56 +0000

    add test for fix for Jira NCF-131

---
+++ ncdump/tst_charfill.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This shell script runs an ncgen buf in handling character _Fillvalue.
+
+set -e
+echo ""
+echo "*** Running char _Fillvalue bug test."
+
+echo "*** Create tst_charfill.nc from tst_charfill.cdl..."
+rm -f tst_charfill.nc tmp_tst_charfill.cdl
+../ncgen/ncgen -b tst_charfill.cdl
+echo "*** dumping tst_charfill.nc to tmp_tst_charfill.cdl..."
+./ncdump tst_charfill.nc > tmp_tst_charfill.cdl
+echo "*** comparing tmp_tst_charfill.cdl with ref_tst_charfill.cdl..."
+diff tmp_tst_charfill.cdl $srcdir/ref_tst_charfill.cdl
+
+echo
+echo "*** All char _Fillvalue  bug test for netCDF-4 format passed!"
+exit 0
