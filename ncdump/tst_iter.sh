f14cdec9481974fed3236b0222571f695c1646db
Dennis Heimbigner <dmh@unidata.ucar.edu>
2012-03-08 18:13:08 +0000

    Provide better cleanup of tst_iter.sh

--- ncdump/tst_iter.sh
+++ ncdump/tst_iter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This shell script runs an ncdump bug test for netcdf
-# Test if the nciter code is working.
+# Test if the nciter code is working [NCF-154]
 
 set -e
 
@@ -8,13 +8,13 @@ echo "*** Running ncdump nc_iter test."
 
 if test "x$CC" = "x" ; then CC="gcc"; fi
 
-CLEANUP="geniter.c ref_tst_iter.cdl a.out tst_iter.nc tst_iter.cdl tmp"
+CLEANUP="iter.*"
 
 rm -f $CLEANUP
 
-echo "create ref_tst_iter.cdl"
-cat > ref_tst_iter.cdl <<EOF
-netcdf tst_iter {
+echo "create iter.cdl"
+cat > iter.cdl <<EOF
+netcdf iter {
 dimensions:
 x = 5 ;
 y = 256 ;
@@ -24,7 +24,7 @@ int var(x,y,z) ;
 data:
 var =
 EOF
-cat >./geniter.c <<EOF
+cat >./iter.c <<EOF
 #include <stdlib.h>
 #include <stdio.h>
 #define N (5*256*256)
@@ -36,19 +36,19 @@ int main() {
 }
 EOF
 
-$CC ./geniter.c
-./a.out >>ref_tst_iter.cdl
+$CC ./iter.c -o iter.exe
+./iter.exe >>iter.cdl
 
-echo "*** create tst_iter.nc "
-../ncgen/ncgen -k1 -o tst_iter.nc ./ref_tst_iter.cdl
-echo "*** dumping tst_iter.nc to tst_iter.cdl..."
-./ncdump tst_iter.nc > tst_iter.cdl
-echo "*** reformat tst_iter.cdl"
-mv tst_iter.cdl tmp
-sed -e 's/\([0-9][,]\) /\1@/g' <tmp |tr '@' '\n' |sed -e '/^$/d' >./tst_iter.cdl
+echo "*** create iter.nc "
+../ncgen/ncgen -k1 -o iter.nc ./iter.cdl
+echo "*** dumping iter.nc to iter.dmp"
+./ncdump iter.nc > iter.dmp
+echo "*** reformat iter.dmp"
+mv iter.dmp iter.tmp
+sed -e 's/\([0-9][,]\) /\1@/g' <iter.tmp |tr '@' '\n' |sed -e '/^$/d' >./iter.dmp
 
-echo "*** comparing tst_iter.cdl with ref_tst_iter.cdl..."
-diff -w tst_iter.cdl ./ref_tst_iter.cdl
+echo "*** comparing iter.dmp with iter.cdl..."
+diff -w ./iter.dmp ./iter.cdl
 
 # cleanup
 rm -f $CLEANUP
