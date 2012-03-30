c417a58747476f472e02c6d6b2d3913a711292f3
Dennis Heimbigner <dmh@unidata.ucar.edu>
2012-03-30 21:51:31 +0000

    add some debugging help to fix snapshot error

--- nc_test4/tst_create_files.c
+++ nc_test4/tst_create_files.c
@@ -58,7 +58,15 @@ main(int argc, char **argv)
        if (nc_def_dim(ncid, "d1", D1, &dimids[1])) ERR;
        if (nc_def_dim(ncid, "d2", D2, &dimids[2])) ERR;
        if (nc_def_var(ncid, LARGE_VAR_NAME, NC_FLOAT, NDIMS3, dimids, &varid)) ERR;
+#ifndef NOBUG
+	{int status;
+	status = nc_def_var_chunking(ncid, varid, NC_CHUNKED, chunk_sizes);
+	if(status)
+	  printf("nc_def_var_chunking fail: %d: %s\n",status,nc_strerror(status));
+	}
+#else
        if (nc_def_var_chunking(ncid, varid, NC_CHUNKED, chunk_sizes)) ERR;
+#endif
        if (nc_enddef(ncid)) ERR;
 
        /* Write the data one slice at a time. */
