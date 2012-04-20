a3d1a09d390951a823bf40e789b9fee2f3b532cf
Russ Rew <russ@unidata.ucar.edu>
2012-04-20 03:15:28 +0000

    Enhanced test to work for array of variable-length strings

--- nc_test4/tst_h_strbug.c
+++ nc_test4/tst_h_strbug.c
@@ -20,23 +20,31 @@
 #define FS_ATT_NAME "fsatt"
 #define VS_VAR_NAME "vsvar"
 #define FS_VAR_NAME "fsvar"
+#define V1D_VAR_NAME "v1dvar"
 #define FSTR_LEN 20
+#define DIM1      4
+#define RANK      1
 
 int
 main()
 {
-    char *vsdata    = "variable-length string";
+    char *vs    = "variable-length string";
     char fsdata[]   = "fixed-length string";
+    char *v1ddata[DIM1] = {"strings","of","variable","length"};
     int i;
     char ch;
 
     printf("\n*** Creating file for checking fix to bugs in accessing strings from HDF5 non-netcdf-4 file.\n");
     {
-	hid_t fileid, spaceid, vstypeid, fstypeid, vsattid, fsattid, vsdsetid, fsdsetid;
+	hid_t fileid, scalar_spaceid, vstypeid, fstypeid, vsattid, fsattid, vsdsetid, fsdsetid;
 	hid_t class;
 	size_t type_size = FSTR_LEN;
+	hid_t v1dattid, v1ddsetid;
+	hid_t v1dspaceid;
+	hsize_t dims[1] = {DIM1};
 
-	if ((spaceid = H5Screate(H5S_SCALAR)) < 0) ERR;
+	if ((scalar_spaceid = H5Screate(H5S_SCALAR)) < 0) ERR;
+	if ((v1dspaceid = H5Screate_simple(RANK, dims, NULL)) < 0) ERR;
 	
 	/* Create variable-length and fixed-length string types. */
 	if ((vstypeid =  H5Tcopy(H5T_C_S1)) < 0) ERR;
@@ -49,145 +57,36 @@ main()
 	if ((fileid = H5Fcreate(FILE_NAME, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT)) < 0) ERR;
 	
 	/* Create scalar datasets of variable- and fixed-length strings. */
-	if ((vsdsetid = H5Dcreate (fileid, VS_VAR_NAME, vstypeid, spaceid, 
+	if ((vsdsetid = H5Dcreate (fileid, VS_VAR_NAME, vstypeid, scalar_spaceid, 
 				   H5P_DEFAULT)) < 0) ERR;
-	if (H5Dwrite (vsdsetid, vstypeid, H5S_ALL, H5S_ALL, H5P_DEFAULT, &vsdata)) ERR;
-	if ((fsdsetid = H5Dcreate (fileid, FS_VAR_NAME, fstypeid, spaceid, 
+	if (H5Dwrite (vsdsetid, vstypeid, H5S_ALL, H5S_ALL, H5P_DEFAULT, &vs)) ERR;
+	if ((fsdsetid = H5Dcreate (fileid, FS_VAR_NAME, fstypeid, scalar_spaceid, 
 				   H5P_DEFAULT)) < 0) ERR;
 	if (H5Dwrite (fsdsetid, fstypeid, H5S_ALL, H5S_ALL, H5P_DEFAULT, &fsdata)) ERR;
+
+	/* Create 1D dataset of variable-length strings. */
+	if ((v1ddsetid = H5Dcreate (fileid, V1D_VAR_NAME, vstypeid, v1dspaceid, 
+				   H5P_DEFAULT)) < 0) ERR;
+	if (H5Dwrite (v1ddsetid, vstypeid, H5S_ALL, H5S_ALL, H5P_DEFAULT, &v1ddata)) ERR;
 	
 	/* Write scalar global attributes of these types. */
-	if ((vsattid = H5Acreate(fileid, VS_ATT_NAME, vstypeid, spaceid, 
+	if ((vsattid = H5Acreate(fileid, VS_ATT_NAME, vstypeid, scalar_spaceid, 
 				 H5P_DEFAULT)) < 0) ERR;
-	if (H5Awrite(vsattid, vstypeid, &vsdata) < 0) ERR;
-	if ((fsattid = H5Acreate(fileid, FS_ATT_NAME, fstypeid, spaceid, 
+	if (H5Awrite(vsattid, vstypeid, &vs) < 0) ERR;
+	if ((fsattid = H5Acreate(fileid, FS_ATT_NAME, fstypeid, scalar_spaceid, 
 				 H5P_DEFAULT)) < 0) ERR;
 	if (H5Awrite(fsattid, fstypeid, &fsdata) < 0) ERR;
 	
 	/* Close up. */
-	if (H5Dclose(vsdsetid) < 0) ERR;
-	if (H5Dclose(fsdsetid) < 0) ERR;
 	if (H5Aclose(vsattid) < 0) ERR;
 	if (H5Aclose(fsattid) < 0) ERR;
-	if (H5Sclose(spaceid) < 0) ERR;
+	if (H5Sclose(scalar_spaceid) < 0) ERR;
+	if (H5Sclose(v1dspaceid) < 0) ERR;
 	if (H5Tclose(vstypeid) < 0) ERR;
 	if (H5Tclose(fstypeid) < 0) ERR;
-	if (H5Fclose(fileid) < 0) ERR;
-    }
-    
-    {
-	hid_t fileid, spaceid, vstypeid, fstypeid, vsattid, fsattid, vsdsetid, fsdsetid;
-	hid_t class;
-	size_t type_size_in;
-	htri_t is_str;
-	char fsdata_in[FSTR_LEN];
-	char *vsdata_in;		/* gets allocated */
-	
-	/* Now reopen the file. */
-	if ((fileid = H5Fopen(FILE_NAME, H5F_ACC_RDONLY, H5P_DEFAULT)) < 0) ERR;
-	
-	/* Check out the vstring attribute. */
-	if ((vsattid = H5Aopen_name(fileid, VS_ATT_NAME)) < 0) ERR;
-	if ((vstypeid = H5Aget_type(vsattid)) < 0) ERR;
-	if ((spaceid = H5Aget_space(vsattid)) < 0) ERR;
-	
-	/* Given this type id, how would we know this is a variable-length string
-	 * attribute? */
-	if ((class = H5Tget_class(vstypeid)) < 0) ERR;
-	if (class != H5T_STRING) ERR;
-	if (!(type_size_in = H5Tget_size(vstypeid))) ERR;
-	if (type_size_in != sizeof (char *)) ERR;
-	if (!(is_str = H5Tis_variable_str(vstypeid))) ERR;
-	
-	/* Make sure this is a scalar. */
-	if (H5Sget_simple_extent_type(spaceid) != H5S_SCALAR) ERR;
-	
-	/* Read the attribute. */
-	if (H5Aread(vsattid, vstypeid, &vsdata_in) < 0) ERR;
-	
-	/* Check the data. */
-	if (strcmp(vsdata, vsdata_in)) ERR;
-	
-	/* Free our memory. */
-	free(vsdata_in);
-	
-	/* Check out the fstring attribute. */
-	if ((fsattid = H5Aopen_name(fileid, FS_ATT_NAME)) < 0) ERR;
-	if ((fstypeid = H5Aget_type(fsattid)) < 0) ERR;
-	if ((spaceid = H5Aget_space(fsattid)) < 0) ERR;
-	
-	/* Given this type id, how would we know this is a fixed-length string
-	 * attribute? */
-	if ((class = H5Tget_class(fstypeid)) < 0) ERR;
-	if (class != H5T_STRING) ERR;
-	if (!(type_size_in = H5Tget_size(fstypeid))) ERR;
-	if (type_size_in != FSTR_LEN) ERR;
-	if ((is_str = H5Tis_variable_str(fstypeid))) ERR;
-	
-	/* Make sure this is a scalar. */
-	if (H5Sget_simple_extent_type(spaceid) != H5S_SCALAR) ERR;
-	
-	/* Read the attribute. */
-	if (H5Aread(fsattid, fstypeid, &fsdata_in) < 0) ERR;
-	
-	/* Check the data. */
-	if (strcmp(fsdata, fsdata_in)) ERR;
-	
-	/* Check out the vstring dataset. */
-	if((vsdsetid = H5Dopen(fileid, VS_VAR_NAME)) < 0) ERR;
-	if ((vstypeid = H5Dget_type(vsdsetid)) < 0) ERR;
-	if ((spaceid = H5Dget_space(vsdsetid)) < 0) ERR;
-	
-	/* Given this type id, how would we know this is a variable-length string
-	 * dataset? */
-	if ((class = H5Tget_class(vstypeid)) < 0) ERR;
-	if (class != H5T_STRING) ERR;
-	if (!(type_size_in = H5Tget_size(vstypeid))) ERR;
-	if (type_size_in != sizeof (char *)) ERR;
-	if (!(is_str = H5Tis_variable_str(vstypeid))) ERR;
-	
-	/* Make sure this is a scalar. */
-	if (H5Sget_simple_extent_type(spaceid) != H5S_SCALAR) ERR;
-	
-	/* Read the dataset. */
-	if (H5Dread(vsdsetid, vstypeid, H5S_ALL, H5S_ALL, H5P_DEFAULT, &vsdata_in) < 0) ERR;
-	
-	/* Check the data. */
-	if (strcmp(vsdata, vsdata_in)) ERR;
-	
-	/* Free our memory. */
-	free(vsdata_in);
-	
-	/* Check out the fstring dataset. */
-	if((fsdsetid = H5Dopen(fileid, FS_VAR_NAME)) < 0) ERR;
-	if ((fstypeid = H5Dget_type(fsdsetid)) < 0) ERR;
-	if ((spaceid = H5Dget_space(fsdsetid)) < 0) ERR;
-	
-	/* Given this type id, how would we know this is a fixed-length string
-	 * dataset? */
-	if ((class = H5Tget_class(fstypeid)) < 0) ERR;
-	if (class != H5T_STRING) ERR;
-	if (!(type_size_in = H5Tget_size(fstypeid))) ERR;
-	if (type_size_in != FSTR_LEN) ERR;
-	if (is_str = H5Tis_variable_str(fstypeid)) ERR;
-	
-	/* Make sure this is a scalar. */
-	if (H5Sget_simple_extent_type(spaceid) != H5S_SCALAR) ERR;
-	
-	/* Read the dataset. */
-	if (H5Dread(fsdsetid, fstypeid, H5S_ALL, H5S_ALL, H5P_DEFAULT, &fsdata_in) < 0) ERR;
-	
-	/* Check the data. */
-	if (strcmp(fsdata, fsdata_in)) ERR;
-	
-	/* Close HDF5 stuff. */
-	if (H5Aclose(vsattid) < 0) ERR;
-	if (H5Aclose(fsattid) < 0) ERR;
 	if (H5Dclose(vsdsetid) < 0) ERR;
 	if (H5Dclose(fsdsetid) < 0) ERR;
-	if (H5Sclose(spaceid) < 0) ERR;
-	if (H5Tclose(vstypeid) < 0) ERR;
-	if (H5Tclose(fstypeid) < 0) ERR;
+	if (H5Dclose(v1ddsetid) < 0) ERR;
 	if (H5Fclose(fileid) < 0) ERR;
     }
 
@@ -203,7 +102,7 @@ main()
 	if (nc_inq_varndims(ncid, varid, &ndims )) ERR;
 	if (ndims != 0) ERR;
 	if (nc_get_var_string(ncid, varid, &data_in)) ERR;
-	if (strcmp(vsdata, data_in));
+	if (strcmp(vs, data_in)) ERR;
 	if (nc_free_string(1, &data_in)) ERR;
 	if (nc_close(ncid)) ERR;
     }
@@ -221,7 +120,7 @@ main()
     	if (nc_inq_varndims(ncid, varid, &ndims )) ERR;
     	if (ndims != 0) ERR;
     	if (nc_get_var_string(ncid, varid, &data_in)) ERR;
-    	if (strcmp(fsdata, data_in));
+    	if (strcmp(fsdata, data_in)) ERR;
 	if (nc_free_string(1, &data_in)) ERR;
     	if (nc_close(ncid)) ERR;
     }
@@ -237,8 +136,8 @@ main()
 	if (nc_inq_att(ncid, NC_GLOBAL, VS_ATT_NAME, &type, &len)) ERR;
 	if (type != NC_STRING) ERR;
 	if (len != 1) ERR;
-        if (nc_get_att_string(ncid, NC_GLOBAL, VS_ATT_NAME, &data_in));
-	if (strcmp(vsdata, data_in));
+        if (nc_get_att_string(ncid, NC_GLOBAL, VS_ATT_NAME, &data_in)) ERR;
+	if (strcmp(vs, data_in)) ERR;
 	if (nc_free_string(1, &data_in)) ERR;
 	if (nc_close(ncid)) ERR;
     }
@@ -250,35 +149,45 @@ main()
 	nc_type type;
 	size_t len;
 	char *data_in;
-	if (!(data_in = malloc(FSTR_LEN))) ERR;
 	if (nc_open(FILE_NAME, NC_NOWRITE, &ncid)) ERR;
 	if (nc_inq_att(ncid, NC_GLOBAL, FS_ATT_NAME, &type, &len)) ERR;
 	if (type != NC_CHAR) ERR;
 	if (len != FSTR_LEN) ERR;
-        if (nc_get_att_string(ncid, NC_GLOBAL, FS_ATT_NAME, &data_in));
-	if (strcmp(fsdata, data_in));
-	if (nc_free_string(1, &data_in)) ERR;
+	if (!(data_in = malloc(len))) ERR;
+        /* if (nc_get_att_string(ncid, NC_GLOBAL, FS_ATT_NAME, &data_in)) ERR; */
+        if (nc_get_att_text(ncid, NC_GLOBAL, FS_ATT_NAME, data_in)) ERR;
+	if (strcmp(fsdata, data_in)) ERR;
+	free(data_in);
 	if (nc_close(ncid)) ERR;
     }
     SUMMARIZE_ERR;
 
-    /* printf("*** Checking reading variable-length HDF5 strings var through netCDF-4 API..."); */
-    /* { */
-    /* 	int ncid, varid, ndims; */
-    /* 	nc_type type; */
-    /* 	char *data_in[1]; */
-    /* 	if (nc_open(FILE_NAME, NC_NOWRITE, &ncid)) ERR; */
-    /* 	if (nc_inq_varid(ncid, VS_VAR_NAME, &varid)) ERR; */
-    /* 	if (nc_inq_vartype(ncid, varid, &type)) ERR; */
-    /* 	if (type != NC_STRING) ERR; */
-    /* 	if (nc_inq_varndims(ncid, varid, &ndims )) ERR; */
-    /* 	if (ndims != 0) ERR; */
-    /* 	if (nc_get_var_string(ncid, varid, data_in)) ERR; */
-    /* 	if (strcmp(vsdata, data_in[0])); */
-    /* 	if (nc_free_string(1, data_in)) ERR; */
-    /* 	if (nc_close(ncid)) ERR; */
-    /* } */
-    /* SUMMARIZE_ERR; */
-
+    printf("*** Checking reading variable-length HDF5 strings var through netCDF-4 API...");
+    {
+    	int ncid, varid, ndims;
+    	nc_type type;
+	int *dimids;
+	size_t nstrings;
+    	char **data_in;
+    	if (nc_open(FILE_NAME, NC_NOWRITE, &ncid)) ERR;
+    	if (nc_inq_varid(ncid, V1D_VAR_NAME, &varid)) ERR;
+    	if (nc_inq_vartype(ncid, varid, &type)) ERR;
+    	if (type != NC_STRING) ERR;
+    	if (nc_inq_varndims(ncid, varid, &ndims )) ERR;
+    	if (ndims != RANK) ERR;
+	if (!(dimids = malloc(ndims * sizeof(int)))) ERR;
+    	if (nc_inq_vardimid(ncid, varid, dimids)) ERR;
+	if (nc_inq_dimlen(ncid, dimids[0], &nstrings)) ERR;
+	if (!(data_in = (char **)malloc(nstrings * sizeof(char *)))) ERR;
+    	if (nc_get_var_string(ncid, varid, data_in)) ERR;
+	for (i = 0; i < nstrings; i++) {
+	    if(strcmp(v1ddata[i], data_in[i])) ERR;
+	}
+    	if (nc_free_string(nstrings, data_in)) ERR;
+	free(data_in);
+	free(dimids);
+    	if (nc_close(ncid)) ERR;
+    }
+    SUMMARIZE_ERR;
     FINAL_RESULTS;
 }
