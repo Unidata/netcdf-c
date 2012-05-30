3370c1a4bdfa28c1e1e0ec6e31c14d4938ef552b
Ward Fisher <wfisher@unidata.ucar.edu>
2012-05-30 22:27:36 +0000

    Updated paths in script.

--- my_static_32_nc4_dap_conf.sh
+++ my_static_32_nc4_dap_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-./configure --disable-doxygen --disable-shared --enable-dap --enable-netcdf-4 --enable-diskless CFLAGS="-ggdb -O0 -I/c/Users/wfisher/Desktop/hdf5-1.8.8/src -I/c/Users/wfisher/Desktop/hdf5-1.8.8/hl/src -I/c/Users/wfisher/Desktop/curl-7.25.0/include/ -DCURL_STATICLIB" LDFLAGS="-ggdb -O0 -L/c/Users/wfisher/Desktop/curl-7.25.0/lib/.libs -L/c/Users/wfisher/Desktop/hdf5-1.8.8/build_win/bin"
+./configure --disable-doxygen --disable-shared --enable-dap --enable-netcdf-4 --enable-diskless CFLAGS="-ggdb -O0 -I/c/Users/wfisher/Desktop/hdf5-1.8.9/src -I/c/Users/wfisher/Desktop/hdf5-1.8.9/hl/src -I/c/Users/wfisher/Desktop/curl-7.25.0/include/ -DCURL_STATICLIB" LDFLAGS="-ggdb -O0 -L/c/Users/wfisher/Desktop/hdf5-1.8.9/build_win/bin -L/c/Users/wfisher/Desktop/curl-7.25.0/lib/.libs -L/c/Users/wfisher/Desktop/hdf5-1.8.9/build_win/bin -L/c/Users/wfisher/Desktop/szip-2.1/build_win32_static/bin"
 
 #-L/c/Users/wfisher/Desktop/curl-7.25.0/lib/.libs/libcurl.a"
