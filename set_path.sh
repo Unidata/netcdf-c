cc888ab3b441d79bddaa899404ab5f643d3d1082
Ward Fisher <wfisher@unidata.ucar.edu>
2012-06-12 19:48:24 +0000

    Added a new convience script. Must be sourced instead of executed in MSYS.

---
+++ set_path.sh
@@ -0,0 +1,12 @@
+## 32-bit, shared
+export PATH=$DEFAULT_PATH:/c/Users/wfisher/Desktop/hdf5-1.8.9/build_win32_dll/bin:/c/Users/wfisher/Desktop/szip-2.1/build_win32/bin:/c/Users/wfisher/Desktop/zlib125dll/dll32:/c/Users/wfisher/Desktop/curl-7.25.0/build_win32/lib/.libs:/c/Users/wfisher/Desktop/win_netcdf/liblib/.libs
+
+## 64-bit, static
+#export PATH=$DEFAULT_PATH:/c/Users/wfisher/Desktop/hdf5-1.8.9/build_win64_static/bin:/c/Users/wfisher/Desktop/szip-2.1/build_win64/bin:/c/Users/wfisher/Desktop/zlib125dll/staticx64:/c/Users/wfisher/Desktop/curl-7.25.0/build_win64/lib/.libs:/c/Users/wfisher/Desktop/win_netcdf/liblib/.libs
+
+## 64-bit, shared
+#export PATH=$DEFAULT_PATH:/c/Users/wfisher/Desktop/hdf5-1.8.9/build_win64_dll/bin:/c/Users/wfisher/Desktop/szip-2.1/build_win64/bin:/c/Users/wfisher/Desktop/zlib125dll/dllx64:/c/Users/wfisher/Desktop/curl-7.25.0/build_win64/lib/.libs:/c/Users/wfisher/Desktop/win_netcdf/liblib/.libs
+
+
+
+echo $PATH
