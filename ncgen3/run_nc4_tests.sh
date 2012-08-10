5e9d4000e104b9977713fa0ef05f1db9ebf9a56d
Ward Fisher <wfisher@unidata.ucar.edu>
2012-08-10 20:55:07 +0000

    Added additional tests in CMake

--- ncgen3/run_nc4_tests.sh
+++ ncgen3/run_nc4_tests.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # This shell script runs the ncdump tests.
 # $Id: run_nc4_tests.sh,v 1.3 2009/09/24 18:19:10 dmh Exp $
+srcdir=`dirname $0`
+cd $srcdir
+srcdir=`pwd`
+
+# compute the build directory
+builddir=`pwd`/..
+# Hack for CYGWIN
+cd $srcdir
+if [ `uname | cut -d "_" -f 1` = "MINGW32" ]; then
+    srcdir=`pwd | sed 's/\/c\//c:\//g'`
+    builddir="$srcdir"/..
+fi
 
 echo "*** Testing ncgen3 for netCDF-4."
 set -e
--- ncgen3/run_tests.sh
+++ ncgen3/run_tests.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 # This shell script runs the ncgen3 tests.
 # $Id: run_tests.sh,v 1.9 2009/09/24 18:19:11 dmh Exp $
+srcdir=`dirname $0`
+cd $srcdir
+srcdir=`pwd`
 
+# compute the build directory
+builddir=`pwd`/..
+# Hack for CYGWIN
+cd $srcdir
+if [ `uname | cut -d "_" -f 1` = "MINGW32" ]; then
+    srcdir=`pwd | sed 's/\/c\//c:\//g'`
+    builddir="$srcdir"/..
+fi
 echo "*** Testing ncgen3."
 set -e
 echo "*** creating classic file c0.nc from c0.cdl..."
