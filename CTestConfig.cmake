fc684dd05f88b96d273040678a17d667bec60147
Ward Fisher <wfisher@unidata.ucar.edu>
2012-10-19 20:48:43 +0000

    Re-added the CTest configuration file.

---
+++ CTestConfig.cmake
@@ -0,0 +1,5 @@
+# CTest/CDash configuration
+SET (CTEST_DROP_METHOD "http")
+SET (CTEST_DROP_SITE "yakov.unidata.ucar.edu/")
+SET (CTEST_DROP_LOCATION "CDash/submit.php?project=WinNETCDF")
+SET (CTEST_DROP_SITE_CDASH TRUE)
