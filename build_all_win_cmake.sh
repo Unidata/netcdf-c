87d71a9c15cc5179742448d99dc018af58124ec8
Ward Fisher <wfisher@unidata.ucar.edu>
2012-11-27 18:49:19 +0000

    Updated build script

--- build_all_win_cmake.sh
+++ build_all_win_cmake.sh
@@ -1,10 +1,75 @@
 #!/bin/bash
 
 #####
-# 32-bit
+# Function to check for error.
+#####
+CHECKERR () {
+    ERRCODE=$?
+    if [[ ! $TEST -eq 0 ]]; then
+	echo "Error $ERRCODE caught. Exit."
+	exit $ERRCODE
+    fi
+}
+
+#####
+# Function to do the building.
+#####
+DOBUILD () {
+    BTYPE="static"
+    if [ x$SHARED = "xON" ]; then
+	BTYPE="shared"
+    fi
+
+    NAME="$VER-NC4$NC4-DAP$DAP-$BTYPE"
+    BDIR="build_$NAME"
+    echo "Building $NAME"
+
+    rm -rf $BDIR
+    mkdir $BDIR
+    cd $BDIR
+    CMAKE_PREFIX_PATH=$RESOURCE_DIR cmake .. -G"$CMAKEGEN" -DCPACK_PACKAGE_FILE_NAME=NetCDF-$NAME -DENABLE_TESTS=OFF -DBUILD_SHARED_LIBS=$SHARED -DENABLE_DAP=$DAP -DENABLE_NETCDF_4=$NC4
+    CHECKERR
+
+    cmake --build .
+    CHECKERR
+
+    cmake --build . --target package
+    CHECKERR
+
+    mv -f *.zip *.dmg *.exe ../$INSTALLDIRS
+    cd ..
+    echo "Finished building $NAME"
+
+}
+
+
+#####
+# Set up platform-specific variables 
 #####
 
-RDIR=/c/share/w32/static
+unamestr=`uname`
+VER="4.2.x-snapshot"
+INSTALLDIRS="packages"
+
+case $unamestr in
+    Darwin) echo "Configuring Darwin"
+    CMAKEGEN="Unix Makefiles"
+    ;;
+    *) echo "Unknown platform: $unamestr"
+    exit 1
+    ;;
+esac
+
+rm -rf $INSTALLDIRS
+mkdir -p $INSTALLDIRS
+
+
+#####
+# 32-bit
+#####
+RESOURCE_DIR=/c/share/w32/static
+PLTFRM="32"
+
 
 ###
 # Static
@@ -16,20 +81,51 @@ SHARED=OFF
 # Minimum
 #
 
-BDIR="build_32_min_static"
-NAME="4.2.x-No_DAP-No_HDF-static-snapshot"
-echo "Building $NAME"
-mkdir $BDIR
-cd $BDIR
-CMAKE_PREFIX_PATH=$RDIR cmake .. -DENABLE_NETCDF_4=OFF -DENABLE_DAP=OFF -DCPACK_PACKAGE_FILE_NAME=NetCDF-$NAME -DENABLE_TESTS=OFF -DBUILD_SHARED_LIBS=$SHARED
-cmake --build .
-cmake --build . --target package
-echo "Finished building $NAME"
+NC4="OFF"
+DAP="OFF"
+DOBUILD
 
 #
 # NetCDF-4
 #
 
+NC4="ON"
+DAP="OFF"
+DOBUILD
+
 #
 # NetCDF-4, DAP
 # 
+
+NC4="ON"
+DAP="ON"
+DOBUILD
+
+###
+# Shared
+###
+
+SHARED=ON
+#
+# Minimum
+#
+
+NC4="OFF"
+DAP="OFF"
+DOBUILD
+
+#
+# NetCDF-4
+#
+
+NC4="ON"
+DAP="OFF"
+DOBUILD
+
+#
+# NetCDF-4, DAP
+# 
+
+NC4="ON"
+DAP="ON"
+DOBUILD
