7d36493cd514ed9bee192dafb047516ed56ba2ef
Ward Fisher <wfisher@unidata.ucar.edu>
2012-12-07 21:09:11 +0000

    Updated script test.

--- build_all_win_cmake.sh
+++ build_all_win_cmake.sh
@@ -26,12 +26,20 @@ DOBUILD () {
 	CMAKEGEN=$CMAKEGEN64
     fi
 
+
     # The packages are built 32 & 64 bit on OSX.
-    if [ $unamestr == "Darwin" ]; then
-	NAME="$VER-NC4_$NC4-DAP_$DAP-$BTYPE"
+    NAME="$VER"
+    if [ "x$NC4" = "xON" ]; then
+	NAME="$NAME-NC4"
     else
-	NAME="$VER-NC4_$NC4-DAP_$DAP-$PLTFRM-$BTYPE"
+	NAME="$NAME-NC3"
     fi
+    
+    if [ x$DAP = "xON" ]; then
+	NAME="$NAME-DAP"
+    fi		
+
+    NAME="$NAME-$PLTFRM-$BTYPE"
     BDIR=$ALLBUILDS"build_$NAME"
     echo "Building $NAME"
 
@@ -59,7 +67,7 @@ DOBUILD () {
 #####
 
 unamestr=`uname | cut -d " " -f 1`
-VER="4.2.x-snapshot"
+VER="4.2.dev"
 CURDIR=`pwd`
 ALLBUILDS=$CURDIR/"all_builds/"
 INSTALLDIRS=$ALLBUILDS"packages"
@@ -71,11 +79,12 @@ case $unamestr in
     Darwin) echo "Configuring Darwin"
     CMAKEGEN32="Unix Makefiles"
     CMAKEGEN64="Unix Makefiles"
-    POSTFLAGS=-D'CMAKE_OSX_ARCHITECTURES=i386;x86_64'
+    #POSTFLAGS=-D'CMAKE_OSX_ARCHITECTURES=i386;x86_64'
     ;;
     MINGW32_NT-6.1) echo "Configuring MSYS/MinGW"
     CMAKEGEN32="Visual Studio 10"
     CMAKEGEN64="Visual Studio 10 Win64"
+    POSTFLAGS="--config Release"
     ;;
     *) echo "Unknown platform: $unamestr"
     exit 1
@@ -95,23 +104,23 @@ PLTFRM="32"
 ###
 # Static
 ###
-RESOURCE_DIR=/c/share/w32/static
+#RESOURCE_DIR=/c/share/w32/static
 
 
-SHARED=OFF
+#SHARED=OFF
 
-for NC4_STATES in OFF ON; do
-    for DAP_STATES in OFF ON; do
-	NC4=$NC4_STATES
-	DAP=$DAP_STATES
-	DOBUILD
-    done
-done
+#for NC4_STATES in OFF ON; do
+#    for DAP_STATES in OFF ON; do
+#	NC4=$NC4_STATES
+#	DAP=$DAP_STATES
+#	DOBUILD
+ #   done
+#done
 
 ###
 # Shared
 ###
-
+RESOURCE_DIR=/c/share/w32/shared
 SHARED=ON
 
 for NC4_STATES in OFF ON; do
@@ -136,8 +145,6 @@ PLTFRM="64"
 # Shared
 ###
 RESOURCE_DIR=/c/share/x64/shared
-
-
 SHARED=ON
 
 for NC4_STATES in OFF ON; do
