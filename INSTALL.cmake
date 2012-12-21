1d1dc4e9f0f4bca062e96b7e9d286961209994c8
Ward Fisher <wfisher@unidata.ucar.edu>
2012-12-21 21:08:50 +0000

    Working on CMake documentaiton.

--- INSTALL.cmake
+++ INSTALL.cmake
@@ -1,5 +1,5 @@
-Documentation for getting and building netCDF
-*********************************************
+Documentation for getting and building netCDF with CMake
+********************************************************
 
 This document is for getting and building the netCDF C library and
 utilities, version 4.3.  Other libraries that depend on the netCDF C
@@ -44,8 +44,22 @@ portability bugs.
 Once you have downloaded and unpacked the distribution, see the
 following section on building.
 
-Building NetCDF
-***************
+For additional instructions on getting and building the netCDF 
+dependencies, please see the 'INSTALL' text file.
+
+Getting CMake
+*************
+
+CMake, a software configuration and testing tool, is maintained by
+Kitware.  CMake is available in many linux package management
+systems, as well as the 'macports' package management system for 
+OSX. 
+
+CMake may also be downloaded for these platforms, as well as Windows,
+from the CMake website at http://www.cmake.org.
+
+Building NetCDF with CMake
+**************************
 
 The netCDF-C library and utilities requires 3rd party libraries for
 full functionality.
@@ -55,90 +69,12 @@ full functionality.
   *  Building with HDF4 Support
   *  Building with Parallel I/O Support
 
-Building with NetCDF-4 and the Remote Data Client
-*************************************************
-
-The usual way of building netCDF requires the HDF5, zlib, and curl
-libraries. (And, optionally, the szlib library). Versions required are
-at least HDF5 1.8.8, zlib 1.2.5, and curl 7.18.0 or later.
-(Optionally, if building with szlib, get szip 2.0 or later.)
-
-HDF5 1.8.9 and zlib 1.2.7 packages are available from the netCDF-4 ftp
-site:
-
-  ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4
-
-If you wish to use the remote data client code, then you will also
-need libcurl, which can be obtained from the curl website:
-
-  http://curl.haxx.se/download.html
-
-Make sure you run ``make check'' for the HDF5 and zlib
-distributions. They are very well-behaved distributions, but sometimes
-the build doesn't work (perhaps because of something subtly
-misconfigured on the target machine). If one of these libraries is not
-working, netCDF will have serious problems.
-
-Note that for building netCDF, it is not necessary to build the HDF5
-Fortran, C++, or Java API's.  Only the HDF5 C library is used.
-
-Optionally, you can also build netCDF-4 with the szip library
-(a.k.a. szlib). NetCDF cannot create szipped data files, but can read
-HDF5 data files that have used szip.
-
-There are license restrictions on the use of szip, see the section on
-licensing terms in the web page on szip compression in HDF products:
-
-  http://www.hdfgroup.org/doc_resource/SZIP/
-
-These license restrictions seem to apply to commercial users who are
-writing data. (Data readers are not restricted.) But here at NetCDF
-World Headquarters, in Sunny Boulder, Colorado, there are no lawyers,
-only programmers, so please read the szip documents for the license
-agreement to see how it applies to your situation.
-
-If ``make check'' fails for either zlib or HDF5, the problem must be
-resolved before the netCDF-4 installation can continue. For HDF5
-problems, see the HDF5 help services:
-
-  http://www.hdfgroup.org/services/support.html
-
-Build zlib like this:
-
-  ./configure --prefix=/home/ed/local
-  make check install
-
-Then you build HDF5, specifying the location of the zlib library:
-
-  ./configure --with-zlib=/home/ed/local --prefix=/home/ed/local 
-  make check install
-
-Note that for shared libraries, you may need to add the install
-directory to the LD_LIBRARY_PATH environment variable. See the netCDF
-FAQ:
-
-  http://www.unidata.ucar.edu/netcdf/docs/faq.html#Shared%20Libraries
-
-for more details on using shared libraries.
-
-If you are building HDF5 with szip, then include the --with-szlib=
-option, with the directory holding the szip library.
-
-After HDF5 is done, build netcdf, specifying the location of the HDF5,
-zlib, and (if built into HDF5) the szip header files and libraries in
-the CPPFLAGS and LDFLAGS environment variables.  For example:
-
-  CPPFLAGS=-I/home/ed/local/include LDFLAGS=-L/home/ed/local/lib ./configure --prefix=/home/ed/local
-  make check install
-
-The configure script will try to find necessary tools in your
-path. When you run configure you may optionally use the --prefix
-argument to change the default installation directory. The above
-examples install the zlib, HDF5, and netCDF-4 libraries in
-/home/ed/local/lib, the header file in /home/ed/local/include, and the
-utilities in /home/ed/local/bin.  If you don't provide a --prefix
-option, installation will be in /usr/local/, in subdirectories lib/,
-include/, and bin/.
+Note that CMake encourages 'out-of-source-tree' builds, i.e. the
+directory used to build netCDF is not the root of the netCDF
+file structure.  For example, it is fairly common practice to
+create a 'build' directory inside the source directory. The examples
+in this file will use syntax which assumes the user is currently
+located in c:\netcdf-src-dir\build\.
 
 Building NetCDF with Classic Library Only
 *****************************************
@@ -160,8 +96,15 @@ servers.)
 To build without support for the netCDF-4 formats or the additional
 netCDF-4 functions, but with remote access, use:
 
-  ./configure --prefix=/home/ed/local --disable-netcdf-4
-  make check install
+Windows:
+	> cmake -DCMAKE_INSTALL_PREFIX=C:\Home\Ed\Local -DENABLE_NETCDF_4=OFF
+	> cmake --build .
+	> cmake --build . --target RUN_TESTS
+	> cmake --build . --target INSTALL
+
+Linux/Unix:	
+  	> ./cmake -DCMAKE_INSTALL_PREFIX=/home/ed/local -DENABLE_NETCDF_4=OFF
+   	> make test install
 
 (Replace ``/home/ed/local'' with the name of the directory where
 netCDF is to be installed.)
@@ -171,16 +114,22 @@ supported remote data access, using the OPeNDAP protocols.  To build
 with full support for netCDF-4 APIs and format but without remote
 client access, use:
 
-  ./configure --prefix=/home/ed/local --disable-dap
-  make check install
+Windows:
+	> cmake -DCMAKE_INSTALL_PREFIX=C:\Home\Ed\Local -DENABLE_DAP=OFF
+	> cmake --build .
+	> cmake --build . --target RUN_TESTS
+	> cmake --build . --target INSTALL
 
-To build without netCDF-4 support or remote client access, use:
-
-  ./configure --prefix=/home/ed/local --disable-netcdf-4 --disable-dap
-  make check install
+Linux/Unix
+	> ./cmake -DCMAKE_INSTALL_PREFIX=/home/ed/local -DENABLE_DAP=OFF
+	> make test install
+	
 
 If you get the message that netCDF installed correctly, then you are
 done!
+>>>> Pick Up Here <<<<
+
+
 
 Building with HDF4 Support
 **************************
