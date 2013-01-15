4a6b93f3ab364094e6c6e161bed3c71165416469
Ward Fisher <wfisher@unidata.ucar.edu>
2013-01-15 19:48:12 +0000

    A test from netcdf-cmake branch made its way into the trunk; corrected.

--- libsrc4/nc4file.c
+++ libsrc4/nc4file.c
@@ -1245,7 +1245,7 @@ read_type(NC_GRP_INFO_T *grp, char *type_name)
          if ((retval = nc4_enum_member_add(&type->enum_member, type->size, 
                                            member_name, value)))
             return retval;
-         H5MM_xfree(member_name); /* Where is this defined? */
+         free(member_name); /* Where is this defined? */
      }
       
       /* Free the tempory memory for one value, and the member name
