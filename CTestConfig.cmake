ef2cead1445a21438c6db425e33455a51e30e491
Ward Fisher <wfisher@unidata.ucar.edu>
2013-07-24 15:41:43 +0000

    Updating subproject files

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,6 @@
 # CTest/CDash configuration
 
-SET(CTEST_PROJECT_SUBPROJECTS netcdf ncgen ncdump nccopy)
+SET(CTEST_PROJECT_SUBPROJECTS netcdf ncgen ncdump nccopy nc_test)
 
 IF(USE_REMOTE_CDASH)
 	set(CTEST_PROJECT_NAME "NetCDF-C")
