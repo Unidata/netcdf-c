4c0e15605dfc1cb547f1290d90012768b78279aa
Ward Fisher <wfisher@unidata.ucar.edu>
2013-08-07 15:33:20 -0600

    Replaced the debugging CTestConfig.cmake file with one configured for the hosted CDash instance.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,27 +1,15 @@
-# CTest/CDash configuration
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
 
-IF(USE_REMOTE_CDASH)
-	set(CTEST_PROJECT_NAME "netcdf-c")
-	set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
-	set(CTEST_SITE "Unidata OSX")
-	set(CTEST_DROP_METHOD "http")
-	set(CTEST_DROP_SITE "my.cdash.org")
-	set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-c")
-	set(CTEST_DROP_SITE_CDASH TRUE)
-ELSEIF(BUILDING_TRUNK)
-	SET (CTEST_DROP_METHOD "http")
-	SET (CTEST_DROP_SITE "yakov.unidata.ucar.edu/")
-	SET (CTEST_DROP_LOCATION "CDash/submit.php?project=netcdf-c")
-	SET (CTEST_DROP_SITE_CDASH TRUE)
-ELSEIF(WORKING_FROM_HOME)
-	SET (CTEST_DROP_METHOD "http")
-	SET (CTEST_DROP_SITE "localhost:4111/")
-	SET (CTEST_DROP_LOCATION "CDash/submit.php?project=netcdf-cmake")
-	SET (CTEST_DROP_SITE_CDASH TRUE)
-ELSE()
-	SET (CTEST_PROJECT_NAME "netcdf-cmake")
-	SET (CTEST_DROP_METHOD "http")
-	SET (CTEST_DROP_SITE "yakov.unidata.ucar.edu/")
-	SET (CTEST_DROP_LOCATION "CDash/submit.php?project=netcdf-cmake")
-	SET (CTEST_DROP_SITE_CDASH TRUE)
-ENDIF()
+set(CTEST_PROJECT_NAME "netcdf-c")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-c")
+set(CTEST_DROP_SITE_CDASH TRUE)
