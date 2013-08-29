c132d94608139e188aa02df738d73c6164f1cdc9
Ward Fisher <wfisher@unidata.ucar.edu>
2013-08-29 13:57:44 -0600

    Increased timeout to 3600, to prevent test timeouts with large file tests when submitting to an online dashboard.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -6,10 +6,13 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
 
+set(CTEST_TEST_TIMEOUT  "1")
+set(DART_TESTING_TIMEOUT "3600")
+
 set(CTEST_PROJECT_NAME "netcdf-c")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_SITE "my.cdash.org") 
 set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-c")
 set(CTEST_DROP_SITE_CDASH TRUE)
