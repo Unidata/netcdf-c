189eb4b2702fa7c5e90ee337a3d32d2007ae0119
dmh <dmh@unidata.ucar.edu>
2013-12-20 13:57:15 -0700

    add formatx test cases

---
+++ nc_test4/tst_formatx_hdf4.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This shell script tests the output several previous tests.
+# $Id: tst_output.sh,v 1.17 2010/05/14 16:21:15 ed Exp $
+
+PATH=tst_interops2.h4
+
+echo ""
+echo "*** Testing extended file format output."
+set -e
+
+echo "Test extended format output for a HDF4 file"
+rm -f tmp
+../ncdump/ncdump -K $PATH >tmp
+if ! fgrep 'HDF4' <tmp ; then
+echo "*** Fail: extended format for an HDF4 file"
+fi
