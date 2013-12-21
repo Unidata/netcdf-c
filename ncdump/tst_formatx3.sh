3423bb46d64d7b9634242f002a02c193c38b00f1
dmh <dmh@unidata.ucar.edu>
2013-12-21 14:47:50 -0700

    add  to formatx?.sh tests

--- ncdump/tst_formatx3.sh
+++ ncdump/tst_formatx3.sh
@@ -3,12 +3,16 @@
 # $Id: tst_output.sh,v 1.17 2010/05/14 16:21:15 ed Exp $
 
 
+if test "x$srcdir" = x ; then
+srcdir="."
+fi
+
 echo ""
 echo "*** Testing extended file format output."
 set -e
 echo "Test extended format output for a netcdf-3 file"
 rm -f tmp
-../ncgen/ncgen -k1 -b -o ./test.nc ref_tst_small.cdl
+../ncgen/ncgen -k1 -b -o ./test.nc $srcdir/ref_tst_small.cdl
 ./ncdump -K test.nc >tmp
 if ! fgrep 'classic mode=00000000' <tmp ; then
 echo "*** Fail: extended format for a classic file"
@@ -16,7 +20,7 @@ fi
 
 echo "Test extended format output for a 64-bit netcdf-3 file"
 rm -f tmp
-../ncgen/ncgen -k2 -b -o ./test.nc ref_tst_small.cdl
+../ncgen/ncgen -k2 -b -o ./test.nc $srcdir/ref_tst_small.cdl
 ./ncdump -K test.nc >tmp
 if ! fgrep '64-bit offset mode=00000200' <tmp ; then
 echo "*** Fail: extended format for a 64-bit classic file"
