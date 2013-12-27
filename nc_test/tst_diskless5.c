13138fb2fb3318ce1634e913713408386c27b6df
Fedor Baart <fedor.baart@deltares.nl>
2013-12-27 13:33:23 +0100

    test for dimension and ncids

--- nc_test/tst_diskless5.c
+++ nc_test/tst_diskless5.c
@@ -19,22 +19,54 @@ int main(int argc, char *argv[])
   int status;
   const char *path = "diskless.nc";
   int mode = NC_DISKLESS;
-  int ncid;
+  int ncid1;
+  int ncid2;
+  int dimid1;
+  int dimid2;
+  char name[NC_MAX_NAME];
+  size_t len;
 
   /* Create an in memory file */
-  status = nc_create(path, mode, &ncid);
+  status = nc_create(path, mode, &ncid1);
   check_err(status, __LINE__, __FILE__);
+
+  /* Add a dimension */
+  status = nc_def_dim(ncid1, "dimension1", 10, &dimid1);
+  check_err(status, __LINE__, __FILE__);
+
+  status = nc_enddef(ncid1);
+  check_err(status, __LINE__, __FILE__);
+
   /* Open the same memory file somewhere else. */
-  /* This now fails because the file is not found */
-  status = nc_open(path, mode, &ncid);
+  status = nc_open(path, mode, &ncid2);
   check_err(status, __LINE__, __FILE__);
-  /* Maybe keep a hashtable of path->ncid and check when a diskless file is opened */
+
+  /* Should be the same file */
+  assert(ncid1 == ncid2);
+
+  /* Read the id of  dimension1 */
+  status = nc_inq_dimid(ncid2, "dimension1", &dimid2);
+  check_err(status, __LINE__, __FILE__);
+
+  /* Should be the same dimension */
+  assert(dimid1 == dimid2);
+
+  /* Let's check the name and the length */
+  status = nc_inq_dim(ncid2, dimid2, name, &len);
+  check_err(status, __LINE__, __FILE__);
+
+  assert(len == 10);
+  assert(strcmp(name, "dimension1") == 0);
 
   /* Close the file */
-  status = nc_close(ncid);
+  status = nc_close(ncid1);
   check_err(status, __LINE__, __FILE__);
 
-  /* TODO check a persistent file also */
+  /* File 2 should also be closed now. */
+  status = nc_close(ncid2);
+  /* so status should be a bad id error, if we close it twice */
+  assert(status == NC_EBADID);
+
   SUMMARIZE_ERR;
   FINAL_RESULTS;
 
