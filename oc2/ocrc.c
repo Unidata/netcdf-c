29576459850f1643c1189efddf8ac9b029918245
Ward Fisher <wfisher@unidata.ucar.edu>
2014-01-15 12:48:21 -0700

    Addressed a coverity issue of minor importance.

--- oc2/ocrc.c
+++ oc2/ocrc.c
@@ -341,8 +341,9 @@ ocdodsrc_read(char* basename, char* path)
 		oclog(OCLOGERR, "%s entry too long: %s",basename,line);
 		continue;
 	    }
-	    strcat(line,"=1");
+	    strncat(line,"=1",2);
 	    value = strchr(line,'=');
+	    if(value == NULL) continue; /* Shouldn't happen, but we should address this case just to be safe. */
 	}
         *value = '\0';
 	value++;
