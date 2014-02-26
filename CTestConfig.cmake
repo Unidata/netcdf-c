74feb63e099881085da3e803127eb7761370e515
Ward Fisher <wfisher@unidata.ucar.edu>
2014-02-26 11:09:18 -0700

    Reset the CDashboard location to netcdf-c from netcdf-staging.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -13,5 +13,5 @@ set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org") 
-set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-staging")
+set(CTEST_DROP_LOCATION "/submit.php?project=netcdf-c")
 set(CTEST_DROP_SITE_CDASH TRUE)
