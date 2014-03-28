76e5e0f1fa73cd4f2e5b354054a3bf460242267f
Ward Fisher <ward.fisher@gmail.com>
2014-03-28 17:11:26 -0600

    Addressed several minor issues revealed by static analysis (coverity, clang).  Mostly to do with extreme corner cases in which a null pointer would be dereferenced, or dereferencing a non-null (but non-initialized) pointer would result in garbage.

--- libsrc4/nc4var.c
+++ libsrc4/nc4var.c
@@ -761,11 +761,14 @@ NC4_inq_var_all(int ncid, int varid, char *name, nc_type *xtypep,
          {
             if (!(fill_valuep = calloc(1, sizeof(char *))))
                return NC_ENOMEM;
+	    
             if ((retval = nc4_get_default_fill_value(var->type_info, (char **)fill_valuep)))
             {
                free(fill_valuep);
                return retval;
-            }
+            } else {
+	      free(fill_valuep);
+	    }
          }
          else
          {
