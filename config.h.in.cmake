a9c2796dffe7794ddd0c8b2ddfa21009258d43b7
dmh <dmh@unidata.ucar.edu>
2014-04-09 19:55:20 -0600

    jna fix

--- config.h.in.cmake
+++ config.h.in.cmake
@@ -71,6 +71,7 @@ are set when opening a binary file on Windows. */
 #cmakedefine BUILD_V2 1
 #cmakedefine NO_NETCDF_2 1
 #cmakedefine USE_FSYNC 1
+#cmakedefine JNA 1
 #cmakedefine ENABLE_DOXYGEN 1
 #cmakedefine ENABLE_INTERNAL_DOCS 1
 #cmakedefine VALGRIND_TESTS 1
