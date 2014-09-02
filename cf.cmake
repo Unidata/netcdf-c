19011bf8d12969e127af704c9407c3c80acf77cf
dmh <dmh@unidata.ucar.edu>
2014-09-02 10:48:30 -0600

    switch

--- cf.cmake
+++ cf.cmake
@@ -1,7 +1,8 @@
 #!/bin/bash
+PREFIX="/usr/local"
+#PREFIX="t:\\cygwin\\usr\\local"
 rm -fr ./build
 mkdir ./build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="t:\\cygwin\\usr\\local"
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 cmake --build .
-
