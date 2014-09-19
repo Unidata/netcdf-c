2e20b700af3871fe87865dff2efc1784905450d3
dmh <dmh@unidata.ucar.edu>
2014-09-19 12:43:39 -0600

    Forgot to update cmake wrt to directory name changes

--- cf.cmake
+++ cf.cmake
@@ -5,5 +5,5 @@ UL=/usr/local
 PPATH="$UL"
 HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
 cmake -DCMAKE_INSTALL_PREFIX=${UL} -DCMAKE_PREFIX_PATH="$PPATH" ${HDF5} ..
-cmake --build ..
+cmake --build .
 #cmake --build . --target RUN_TESTS
