5a08ce0f3a769f230c99782c86464e60764f7bdf
Ward Fisher <ward.fisher@gmail.com>
2014-12-04 12:42:55 -0700

    Removed stray Doxygen tags, added some more explicit information to the binary installer created for Windows.

--- CMakeInstallation.cmake
+++ CMakeInstallation.cmake
@@ -12,7 +12,11 @@
 
 IF(WIN32)
   SET(CPACK_NSIS_MODIFY_PATH ON)
+  SET(CPACK_NSIS_DISPLAY_NAME "NetCDF ${netCDF_VERSION}")
+  SET(CPACK_NSIS_PACKAGE_NAME "NetCDF ${netCDF_VERSION}")
   SET(CPACK_NSIS_HELP_LINK "http://www.unidata.ucar.edu/netcdf")
+  SET(CPACK_NSIS_URL_INFO_ABOUT "http://www.unidata.ucar.edu/netcdf")
+  SET(CPACK_NSIS_CONTACT "support-netcdf@unidata.ucar.edu")
   SET(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
 ENDIF()
 
