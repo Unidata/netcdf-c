f9edce92edbc0ace2009e303a1d5e08c92d018b2
dmh <dmh@unidata.ucar.edu>
2015-03-03 21:01:43 -0700

    make testauth be under the enable-auth-tests

--- ncdap_test/testauth.sh
+++ ncdap_test/testauth.sh
@@ -1,49 +1,33 @@
 #!/bin/sh
-#set -x
 
 #NOEMBED=1
 #NOLOCAL=1
 #NOHOME=1
+#NOENV=1
 
-SHOW=1
 #DBG=1
-#GDB=1
+#SHOW=1
 
-CMP=1
+# Choose at most 1
+#GDB=1
+#VG=1
+
+NFL=1
 
 WD=`pwd`
-
-# if this is part of a distcheck action, then this script
-# will be executed in a different directory
-# than the one containing it; so capture the path to this script
-# as the location of the source directory.
-
-# capture the build directory
-builddir=`pwd`/..
-if test "x$TOPSRCDIR" != x ; then
-srcdir="$TOPSRCDIR/ncdap_test"
-else
-srcdir=`dirname $0`
-fi
-# canonical
-cd $srcdir
-srcdir=`pwd`
-
-# Hack for CYGWIN
-if [ `uname | cut -d "_" -f 1` = "MINGW32" ]; then
-    srcdir=`echo $srcdir | sed 's/\/c\//c:\//g'`
-    builddir=`echo $builddir | sed 's/\/c\//c:\//g'`
-fi
-cd ${builddir}/ncdap_test
 
 NETRCFILE=$WD/test_auth_netrc
 # This is the control variable
 NETRC=$NETRCFILE
-COOKIES="${WD}/test_auth_cookies"
-RC=.ocrc
-EXPECT=$srcdir/expected3/testData.nc.dmp
-OCLOGFILE=stderr
 
+COOKIES="${WD}/test_auth_cookies"
+
+RC=.daprc
+
+NCLOGFILE=stderr
+if test "x$DBG" = x1 ; then
+SHOW=1
+fi
 
 # Major parameters
 
@@ -59,112 +43,119 @@ URLPATH="opendap/data/nc/fnoc1.nc"
 BASICCOMBO="$URS"
 NOEMBED=1
 NETRC=$NETRCFILE
-CMP=
 else
 NETRC=
 fi
 
-if test "x$CMP" = x1 ; then SHOW= ; else SHOW=1; fi
-if test "x$DBG" = x1 ; then SHOW=1; fi
+if test "x$DBG" = x1 ; then
+URLPATH="${URLPATH}#log&show=fetch"
+fi
 
 # Split the combo
 BASICUSER=`echo $BASICCOMBO | cut -d: -f1`
 BASICPWD=`echo $BASICCOMBO | cut -d: -f2`
 
-NCDUMP=$builddir/ncdump/ncdump
+NCDUMP=
+for o in ./.libs/ncdump.exe ./.libs/ncdump ./ncdump.exe ./ncdump ; do
+  if test -f $o ; then
+  NCDUMP=$o
+  break;
+  fi
+done
+if test "x$NCDUMP" = x ; then
+echo "no ncdump"
+exit 1
+fi
+
+if test "x$SHOW" = x ; then
+OUTPUT="> /dev/null"
+else
+OUTPUT=
+fi
+
+if test "x$TEMP" = x ; then
+  TEMP="/tmp"
+fi
+TEMP=`echo "$TEMP" | sed -e "s|/$||"`
 
 LOCALRC=./$RC
 HOMERC=${HOME}/$RC
 HOMERC=`echo "$HOMERC" | sed -e "s|//|/|g"`
-SPECRC=$TEMP
-if test "x$TEMP" = x ; then
-  SPECRC="/tmp"
-fi
-SPECRC="$SPECRC/temprc"
+ENVRC="$TEMP/$RC"
+
+cd `pwd`
+builddir=`pwd`
+# Hack for CYGWIN
+cd $srcdir
+srcdir=`pwd`
+cd ${builddir}
 
 function createrc {
-  if test "x$1" = x ; then
-    echo "createrc: no rc file specified"
-  else
-  rm -f $1
-  RCP=$1
-  NRC=$2
-  echo "Creating rc file $RCP"
-  if test "x${DBG}" != x ; then
-    echo "HTTP.VERBOSE=1" >>$RCP
-  fi	
-  echo "HTTP.COOKIEJAR=${COOKIES}" >>$RCP
-  if test "x${URS}" = x ; then
-    echo "HTTP.CREDENTIALS.USERPASSWORD=${BASICCOMBO}" >>$RCP
-  fi
-  if test "x${NRC}" != x ; then
-    echo "HTTP.NETRC=${NRC}" >>$RCP
-  fi
-  fi
+if test "x$1" != x ; then
+RCP=$1
+
+rm -f $RCP
+echo "Creating rc file $RCP"
+if test "x${DBG}" != x ; then
+echo "HTTP.VERBOSE=1" >>$RCP
+fi	
+echo "HTTP.COOKIEJAR=${COOKIES}" >>$RCP
+if test "x${URS}" = x ; then
+echo "HTTP.CREDENTIALS.USERPASSWORD=${BASICCOMBO}" >>$RCP
+fi
+if test "x${NETRC}" != x && test "x$NFL" = x ; then
+echo "HTTP.NETRC=${NETRC}" >>$RCP
+fi
+fi
 }
 
 function createnetrc {
-  if test "x$1" != x ; then
-    rm -f $1
-    echo "Creating netrc file $1"
-    echo "machine uat.urs.earthdata.nasa.gov login $BASICUSER password $BASICPWD" >>$1
-#   echo "machine 54.86.135.31 login $BASICUSER password $BASICPWD" >>$1
-  fi
+if test "x$1" != x ; then
+rm -f $1
+echo "Creating netrc file $1"
+echo "machine uat.urs.earthdata.nasa.gov login $BASICUSER password $BASICPWD" >>$1
+#echo "machine 54.86.135.31 login $BASICUSER password $BASICPWD" >>$1
+fi
 }
 
+# Forcibly remove all and do not restore with save
 function reset {
-  for f in ./$RC $HOME/$RC $SPECRC $COOKIES $NETRC ; do
-    if test "x$DBG" = x1 ; then echo "Deleting $f"; fi
+    for f in ./$RC $HOMERC $ENVRC $COOKIES $NETRC ; do
+      rm -f ${f}
+    done      
+}
+
+# Restore from .save files
+function restore {
+  for f in ./$RC $HOMERC $ENVRC $COOKIES $NETRC ; do
     rm -f ${f}
     if test -f ${f}.save ; then
-        if test "x$DBG" = x1 ; then echo "restoring old ${f}" ; fi
+      echo "restoring old ${f}"
       cp ${f}.save ${f}
     fi      
   done      
-  # unconditional
-  rm -f ./tmp
 }
 
 function save {
-  for f in ./$RC $HOME/$RC $SPECRC $COOKIES $NETRC ; do
+  for f in ./$RC $HOMERC $ENVRC $COOKIES $NETRC ; do
     if test -f $f ; then
       if test -f ${f}.save ; then
         ignore=1
       else
-        if test "x$DBG" = x1 ; then echo "saving $f"; fi
+        echo "saving $f"
         cp ${f} ${f}.save
       fi
     fi      
   done      
 }
 
-function compare {
-  if test "x$CMP" = x1 ; then
-    if diff -w ./tmp $EXPECT ; then
-      echo "***Pass"
-    else
-      echo "***FAIL"
-      pass=1
-    fi
-  fi
-}
+export LD_LIBRARY_PATH="../liblib/.libs:/usr/local/lib:/usr/lib64:$LD_LIBRARY_PATH"
 
-# Invoke ncdump to extract the URL
-function doit {
-  reset
-  createnetrc $2
-  createrc $1 $2
-  echo "command: ${NCDUMP} -h $URL"
-  ${NCDUMP} -h "$URL" >./tmp
-  if test "x$CMP" = x1 ; then compare ; fi
-  if test "x${SHOW}" = x1 ; then cat ./tmp ; fi
-}
-
-pass=0
-
-# Assemble the ncdump command
 if test "x$GDB" = x1 ; then
-  NCDUMP="gdb --args $NCDUMP"
+NCDUMP="gdb --args $NCDUMP"
+fi
+if test "x$VG" = x1 ; then
+NCDUMP="valgrind --leak-check=full $NCDUMP"
 fi
 
 # Initialize
@@ -174,24 +165,49 @@ reset
 if test "x$NOEMBED" != x1 ; then
 echo "***Testing rc file with embedded user:pwd"
 URL="https://${BASICCOMBO}@${URLSERVER}/$URLPATH"
-# Invoke ncdump to extract the URL
-doit
+# Invoke ncdump to extract a file from the URL
+echo "command: ${NCDUMP} -h $URL ${OUTPUT}"
+${NCDUMP} -h "$URL" ${OUTPUT}
 fi
 
 URL="https://${URLSERVER}/$URLPATH"
 if test "x$NOLOCAL" != x1 ; then
-  URL="https://${URLSERVER}/$URLPATH"
-  echo "***Testing rc file in local directory"
-  doit $LOCALRC $NETRC
+echo "***Testing rc file in local directory"
+# Create the rc file and (optional) netrc file in ./
+reset
+createnetrc $NETRC
+createrc $LOCALRC
+
+# Invoke ncdump to extract a file the URL
+echo "command: ${NCDUMP} -h $URL ${OUTPUT}"
+${NCDUMP} -h "$URL" ${OUTPUT}
 fi
 
 if test "x$NOHOME" != x1 ; then
-  URL="https://${URLSERVER}/$URLPATH"
-  echo "***Testing rc file in home directory"
-  doit $HOMERC $NETRC
+echo "***Testing rc file in home directory"
+# Create the rc file and (optional) netrc fil in ./
+reset
+createnetrc $NETRC
+createrc $HOMERC
+
+# Invoke ncdump to extract a file the URL
+echo "command: ${NCDUMP} -h $URL ${OUTPUT}"
+${NCDUMP} -h "$URL" ${OUTPUT}
 fi
 
-#cleanup
+if test "x$NOENV" != x1 ; then
+echo "*** Testing rc file from env variable"
+# Create the rc file and (optional) netrc file
 reset
+createnetrc $NETRC
+export NCRCFILE=$ENVRC
+createrc $NCRCFILE
 
-exit $pass
+# Invoke ncdump to extract a file the URL
+echo "command: ${NCDUMP} -h $URL ${OUTPUT}"
+${NCDUMP} -h "$URL" ${OUTPUT} 
+fi
+
+set +x
+#restore
+
