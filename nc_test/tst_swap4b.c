b2bdd83a7fdfcc68da27e794a76919ba6b0247b9
Ward Fisher <wfisher@unidata.ucar.edu>
2015-07-27 11:20:44 -0600

    Added skeleton of new test for [NCF338].

---
+++ nc_test/tst_swap4b.c
@@ -0,0 +1,23 @@
+/*! Test swap4b function.
+ *
+ * Testa added July 27, 2015.
+ *
+ * This test confirms that the function ncx.c:swap4b() is
+ * working properly. For more information regarding this test
+ * and the circumstances prompting it, see
+ * https://bugtracking.unidata.ucar.edu/browse/NCF-338
+ */
+
+#include "netcdf.h"
+
+#include <stdio.h>
+
+int main() {
+
+
+
+
+  printf("Finished.\n");
+  return 0;
+
+}
