dccbc87d2455d4241fb1409ef57b3d860764daf4
Dennis Heimbigner <dmh@ucar.edu>
2015-09-15 15:18:59 -0600

    1. Added temporary work-around for cygwin where    AC_CHECK_SIZEOF is not working because anti-virus    will not allow very rapid creation/deletion of a    file with same name. 2. modified some test baselines to attempt to fix    Ward's issue

--- cf.cmake
+++ cf.cmake
@@ -3,12 +3,14 @@ mkdir build
 cd build
 UL=/usr/local
 PPATH="$UL"
+ZLIB="-DZLIB_LIBRARY=${UL}/lib/libz.so  -DZLIB_INCLUDE_DIR=${UL}/include"
 HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
+CURL="-DCURL_LIBRARY=${UL}/lib/libcurl.so  -DCURL_INCLUDE_DIR=${UL}/include"
 FLAGS="-DCMAKE_PREFIX_PATH=$PPATH"
 FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=${UL}"
 FLAGS="$FLAGS -DCMAKE_PREFIX_PATH=$PPATH"
 FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true"
 FLAGS="$FLAGS -DENABLE_DAP_AUTH_TESTS=true"
-cmake $FLAGS ${HDF5} ..
-cmake --build .
-cmake --build . --target test
+cmake $FLAGS ${ZLIB} ${HDF5} ${CURL} ..
+#cmake --build .
+#cmake --build . --target test
