c825d612c2fbfae970f5ebfb838c8b9e8faef6c0
Ward Fisher <wfisher@unidata.ucar.edu>
2015-10-22 14:09:19 -0600

    Tweaked some tests so that everything would pass when netcdf4 was disabled.

---
+++ ncdump/run_utf8_nc4_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Moving some netcdf-4 only tests here, out of tst_nccopy and run_utf8_tests.
+# Without this, the tests fail when netcdf-4 is disabled.
+
+set -e
+if test "x$srcdir" = "x"; then
+    srcdir=`dirname $0`;
+fi
+export srcdir
+echo ""
+
+rm -f utf8.nc utf8.cdl
+echo "*** creating enhanced file with utf8 characters..."
+../ncgen/ncgen -4 -b -o utf8.nc ${srcdir}/ref_tst_utf8_4.cdl
+echo "*** dump and compare utf8 output..."
+./ncdump utf8.nc > utf8.cdl
+diff -b -w utf8.cdl ${srcdir}/ref_tst_utf8_4.cdl
