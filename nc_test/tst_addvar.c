117708f464568fba5622b259af0ce37ae5e0821d
Ward Fisher <wfisher@unidata.ucar.edu>
2015-10-28 11:26:23 -0600

    More work on the parallel bug.

--- nc_test/tst_addvar.c
+++ nc_test/tst_addvar.c
@@ -23,60 +23,21 @@
 #define NX 5
 
 
-//#define ERR {if(err!=NC_NOERR){printf("Error at line=%d: %s\n", __LINE__, nc_strerror(err));}}
-
-int createfile() {
-  int i, j, rank, nprocs, ncid, cmode, varid[NVARS], dimid[2], *buf;
-  int err = 0;
-  size_t start[2], count[2];
-  char str[32];
-
-  cmode = NC_CLOBBER;
-  if (nc_create(FILENAME, cmode, &ncid)) ERR_RET;
-
-  /* define dimension */
-  if (nc_def_dim(ncid, "Y", NC_UNLIMITED, &dimid[0])) ERR;
-  if (nc_def_dim(ncid, "X", NX,           &dimid[1])) ERR;
-
-  /* Odd numbers are fixed variables, even numbers are record variables */
-  for (i=0; i<NVARS; i++) {
-    if (i%2) {
-      sprintf(str,"fixed_var_%d",i);
-      if (nc_def_var(ncid, str, NC_INT, 1, dimid+1, &varid[i])) ERR;
-    }
-    else {
-      sprintf(str,"record_var_%d",i);
-      if (nc_def_var(ncid, str, NC_INT, 2, dimid, &varid[i])) ERR;
-    }
-  }
-  if (nc_enddef(ncid)) ERR;
-
-  /* write all variables */
-  buf = (int*) malloc(NX * sizeof(int));
-  for (i=0; i<NVARS; i++) {
-    for (j=0; j<NX; j++) buf[j] = i*10 + j;
-    if (i%2) {
-      start[0] = 0; count[0] = NX;
-      if (nc_put_vara_int(ncid, varid[i], start, count, buf)) ERR;
-    }
-    else {
-      start[0] = 0; start[1] = 0;
-      count[0] = 1; count[1] = NX;
-      if (nc_put_vara_int(ncid, varid[i], start, count, buf)) ERR;
-    }
-  }
-
-  if (nc_close(ncid)) ERR;
-}
-
 int main(int argc, char** argv) {
-    int err, ncid, varid[2], dimid[2];
+  int err, ncid, varid[2], dimid[2], rank, nprocs;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     printf("\n*** Testing bug fix for adding a new variable to an existing file.\n");
 
     printf("***** Reopening file...\t");
 
-    err = nc_open(FILENAME, NC_WRITE, &ncid); ERR;
+    err = nc_open(FILENAME, NC_WRITE|NC_PNETCDF, comm, info &ncid); ERR;
+    if(err) {
+
+    }
     printf("success.\n");
 
     printf("***** Entering redefinition mode...\t");
@@ -92,9 +53,18 @@ int main(int argc, char** argv) {
     err = nc_def_var(ncid, "new_var1", NC_INT,   2, dimid, &varid[0]); ERR;
     err = nc_def_var(ncid, "new_var2", NC_FLOAT, 2, dimid, &varid[1]); ERR;
     err = nc_enddef(ncid); ERR;
+
     err = nc_close(ncid); ERR;
 
     printf("success.\n");
 
+ fn_exit:
+
+    MPI_Finalize();
+    SUMMARIZE_ERR;
+    FINAL_RESULTS;
+
     return 0;
+
+
 }
