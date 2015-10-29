4e60400275834cc853cfb6cc8ea6a272f7aa7002
Ward Fisher <wfisher@ucar.edu>
2015-10-29 19:50:48 +0000

    Changing some cmake infrastructure re: how tests are built.

--- nc_test/run_pnetcdf_test.sh
+++ nc_test/run_pnetcdf_test.sh
@@ -8,7 +8,7 @@ echo "Testing file created with pnetcdf is modifiable with netCDF..."
 ./tst_pnetcdf
 
 echo "Testing file created with pnetcdf works when adding variables..."
-./tst_addvar
+./tst_addvar tst_pnetcdf.nc
 
 set -x
 # We assume a min of at least 2 processors is available
