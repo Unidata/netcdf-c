413e49d758c491dd6c5471069fcf0ec98c75b100
dmh <dmh@unidata.ucar.edu>
2015-11-06 17:03:28 -0700

    1. There were several bugs in ncdump/dumplib.c    that were not taking the CDF-5 format into account.
    2. Had to update ncgen.1 man page to define the new
       k-flag rules to deal with cdf-5.
    
    3. Had to fix some tests that use 'cmp' for comparison;
       this really should be deprecated.
    
    3. There was a bug in configure.ac with respect
       to using the enable-netcdf-4 flag vs
       using disable-netcdf-4.

--- nctest/compare_test_files.sh
+++ nctest/compare_test_files.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-cmp nctest_classic.nc $srcdir/ref_nctest_classic.nc && cmp nctest_64bit_offset.nc $srcdir/ref_nctest_64bit_offset.nc
+set -e
+if test "x$srcdir" = x ; then
+srcdir=`pwd`
+fi
+cmp nctest_classic.nc $srcdir/ref_nctest_classic.nc
+cmp nctest_64bit_offset.nc $srcdir/ref_nctest_64bit_offset.nc
 
