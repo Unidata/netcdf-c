5a1ca24fd69cda2259c5be672a634ec2007a73b2
Dennis Heimbigner <dmh@ucar.edu>
2016-03-17 22:05:46 -0600

    Fix github issue: https://github.com/Unidata/netcdf-c/issues/236
    If a char valued attribute contains embedded nul characters,
    then illegal xml will be generated when using the -x (ncml) flag.
    
    There is not good solution since we have a char value '\0' that
    is legal in a netcdf-c char valued attribute, but is completely
    illegal in ncml (i.e. xml).
    
    So, implemented hack is to go ahead and generate '&#0;' entities
    and then complain that we are generating illegal ncml.

--- ncdump/ncdump.c
+++ ncdump/ncdump.c
@@ -49,6 +49,7 @@ typedef int ssize_t;
 #include "isnan.h"
 #include "cdl.h"
 
+#define XML_VERSION "1.0"
 
 #define int64_t long long
 #define uint64_t unsigned long long
@@ -393,8 +394,8 @@ done:
 static void
 pr_initx(int ncid, const char *path)
 {
-    printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<netcdf xmlns=\"http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2\" location=\"%s\">\n",
-	   path);
+    printf("<?xml version=\"%s\" encoding=\"UTF-8\"?>\n<netcdf xmlns=\"http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2\" location=\"%s\">\n",
+	   XML_VERSION, path);
 }
 
 /*
@@ -472,6 +473,7 @@ pr_att_string(
  */
 static void
 pr_attx_string(
+     const char* attname,
      size_t len,
      const char *string
      )
@@ -480,6 +482,7 @@ pr_attx_string(
     const char *cp;
     const char *sp;
     unsigned char uc;
+    int nulcount = 0;
 
     cp = string;
     printf ("\"");
@@ -509,6 +512,11 @@ pr_attx_string(
 	    break;
 	case '\t':
 	    printf ("&#x9;");
+	    break;
+	case '\0':
+	    printf ("&#0;");
+	    if(nulcount++ == 0)
+		fprintf(stderr,"Attribute: '%s'; value contains nul characters; producing illegal xml\n",attname);
 	    break;
 	default:
 	    if (iscntrl(uc))
@@ -1120,7 +1128,7 @@ pr_attx(
 		varid != NC_GLOBAL ? "  " : "",
 		att.name);
 	/* print attvals as a string with XML escapes */
-	pr_attx_string(attvalslen, attvals);
+	pr_attx_string(att.name, attvalslen, attvals);
     } else {			/* non-string attribute */
 	char att_type_name[NC_MAX_NAME + 1];
 	get_type_name(ncid, att.type, att_type_name);
