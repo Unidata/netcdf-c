98b0f1c9a35c31c24e8f1e37339ac9ac97cdceb5
Ward Fisher <wfisher@ucar.edu>
2016-05-13 12:30:11 -0600

    Removed stray file.

--- update_hdf5_compat_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Temporary utility script.
-
-cp ./tst_vars.nc ../../nc_test4/ref_hdf5_compat1.nc
-cp ./tst_vars4.nc ../../nc_test4/ref_hdf5_compat2.nc
-cp ./tst_compounds.nc ../../nc_test4/ref_hdf5_compat3.nc
