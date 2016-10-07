09eeb798c73a9a243180e27fcce48d58adb6ff2d
Dennis Heimbigner <dmh@ucar.edu>
2016-10-07 11:48:11 -0600

    ckp

---
+++ dap4_test/t_dmrparse.c
@@ -0,0 +1,27 @@
+/*********************************************************************
+ *   Copyright 2016, UCAR/Unidata
+ *   See netcdf/COPYRIGHT file for copying and redistribution conditions.
+ *********************************************************************/
+
+/**
+This provides a simple dap4  metadata -> xml printer.
+Used to test the parser
+*/
+
+#include "t_dmr.h"
+
+int
+main(int argc, char** argv)
+{
+    int ret = NC_NOERR;
+
+    setup(1,argc,argv);
+
+    if((ret = NCD4_parse(metadata))) goto done;
+    ret = NCD4_print(metadata,output);
+    ncbytesnull(output);
+    if(ret == NC_NOERR)
+        fprintf(stdout,"%s\n",ncbytescontents(output));
+done:
+    return cleanup(ret);
+}
