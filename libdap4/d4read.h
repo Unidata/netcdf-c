6e829529ffe78ae41aa3dc477e104269f5df7778
Dennis Heimbigner <dmh@ucar.edu>
2016-10-07 11:47:44 -0600

    ckp

---
+++ libdap4/d4read.h
@@ -0,0 +1,12 @@
+/*********************************************************************
+  *   Copyright 2016, UCAR/Unidata
+  *   See netcdf/COPYRIGHT file for copying and redistribution conditions.
+  *********************************************************************/
+
+#ifndef D4READ_H
+#define D4READ_H
+
+extern int ncd4_readDMR(NCD4INFO*);
+extern int ncd4_readDAP(NCD4INFO**, int inmemory);
+
+#endif /*READ_H*/
