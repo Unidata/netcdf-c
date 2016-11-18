637f649555633d0f01505ef56106e9bbcf548b63
Ward Fisher <wfisher@ucar.edu>
2016-11-18 11:11:53 -0700

    Migrated test validation to run_tests.sh

--- ncgen/run_tests.sh
+++ ncgen/run_tests.sh
@@ -2,7 +2,6 @@
 # This shell script runs the ncgen tests.
 # $Id: run_tests.sh,v 1.10 2010/04/04 22:06:03 dmh Exp $
 
-set -x
 if test "x$srcdir" = x ; then
 srcdir=`pwd`
 fi
@@ -10,28 +9,42 @@ fi
 echo "*** Testing ncgen."
 set -e
 
+##
+# Function to test a netCDF CDL file.
+# 1. Generate binary nc.
+# Use ncdump to compare against original CDL file.
+# Input: CDL file name, minus the suffix.
+# Other input: arguments.
+#
+# Example:
+#     $ validateNC compound_datasize_test -k nc4
+##
+validateNC() {
+    BASENAME=$1
+    INFILE=$srcdir/$1.cdl
+    TMPFILE="tst_$1".cdl
+    shift
+    ARGS=$@
+
+    echo "*** generating $BASENAME.nc ***"
+    ./ncgen $ARGS $INFILE
+    ../ncdump/ncdump $BASENAME.nc > $TMPFILE
+    echo "*** comparing binary against source CDL file *** "
+    diff -b -w $INFILE $TMPFILE
+
+}
+
+
 echo "*** creating classic file c0.nc from c0.cdl..."
-ls -l "$srcdir/c0.cdl"
-ls -l ./ncgen*
-./ncgen -b -o c0.nc $srcdir/c0.cdl
-if [ ! -f c0.nc ]; then
-    echo "Failure."
-    exit 1
-fi
+
+validateNC c0
 
 echo "*** creating 64-bit offset file c0_64.nc from c0.cdl..."
-./ncgen -k 64-bit-offset -b -o c0_64.nc $srcdir/c0.cdl
-if [ ! -f c0_64.nc ]; then
-    echo "Failure."
-    exit 1
-fi
+
+validate c0 -k 64-bit-offset -b
 
 echo "*** creating 64-bit offset file c5.nc from c5.cdl..."
-./ncgen -k 64-bit-data -b -o c5.nc $srcdir/c5.cdl
-if [ ! -f c5.nc ]; then
-    echo "Failure."
-    exit 1
-fi
+validate c5 -k 64-bit-data -b
 
 echo "*** Test successful!"
 exit 0
