a07728946443dc3174f33fc74b5986202fe90fef
Ward Fisher <wfisher@ucar.edu>
2017-06-01 09:11:52 -0600

    Refactored hdf4 chunking test a little bit.

--- nc_test4/run_chunk_hdf4.sh
+++ nc_test4/run_chunk_hdf4.sh
@@ -4,9 +4,6 @@
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 . ../test_common.sh
 
-CHUNKED=chunked.hdf4
-CONTIG=contiguous.hdf4
-
 echo ""
 echo "*** Testing hdf4 chunking..."
 
@@ -14,24 +11,10 @@ if test "x${srcdir}" = "x" ; then
 srcdir="."
 fi
 
-# Move the data sets into place
-ISDISTCHECK=0
-if test -f ./${CHUNKED} ; then
-ISDISTCHECK=0
-else
-ISDISTCHECK=1
-cp ${srcdir}/${CHUNKED} .
-cp ${srcdir}/${CONTIG} .
-fi
-
 if ./tst_chunk_hdf4 ; then
   echo "***SUCCESS!! tst_chunk_hdf4"
 else
   echo "***FAIL: tst_chunk_hdf4"
-fi
-
-if test "x${ISDISTCHECK}" = "x1" ; then
-echo rm -f ./${CHUNKED} ./${CONTIG}
 fi
 
 exit 0
