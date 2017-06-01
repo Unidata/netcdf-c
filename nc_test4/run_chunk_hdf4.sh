f1f7224f93abe0d037d98248633d84341440ca44
Ward Fisher <wfisher@ucar.edu>
2017-06-01 09:36:54 -0600

    Corrected a false positive.

--- nc_test4/run_chunk_hdf4.sh
+++ nc_test4/run_chunk_hdf4.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # Run test_chunk_hdf4 passing ${srcdir}
 
+
+
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 . ../test_common.sh
+
+set -e
 
 echo ""
 echo "*** Testing hdf4 chunking..."
@@ -14,7 +18,9 @@ fi
 if ./tst_chunk_hdf4 ; then
   echo "***SUCCESS!! tst_chunk_hdf4"
 else
+  RES=$?
   echo "***FAIL: tst_chunk_hdf4"
+  exit $RES
 fi
 
 exit 0
