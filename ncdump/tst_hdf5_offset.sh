0102d79816c618bf520897915ae8615c9bfde051
Dennis Heimbigner <dmh@ucar.edu>
2017-07-08 14:06:29 -0600

    Modified the offset test case so that it used a pre-constructed binary file rather than using echo to create it on the fly. This should ensure that there are no extraneous newlines or such.

--- ncdump/tst_hdf5_offset.sh
+++ ncdump/tst_hdf5_offset.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi 
 . ../test_common.sh
@@ -6,18 +6,16 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 set -e
 
 # This tests that we can detect an HDF5 file with an offset
-L64="                                                                "
-L128="${L64}${L64}"
 
 rm -f ./offset.cdl ./offset.nc
 cp ${srcdir}/small.cdl ./offset.cdl
 ${NCGEN} -4 ./offset.cdl
 
 # Test a 512 byte offset
-rm -f L512.txt L512.hdf5
-L512="${L128}${L128}${L128}${L128}"
-echo -n "$L512" > L512.txt
-cat L512.txt offset.nc > L512.hdf5
+rm -f L512.hdf5
+# verify size of L512.bin
+#LSIZE=`wc -c ${srcdir}/L512.bin | cut -d ' ' -f 1`
+cat ${srcdir}/L512.bin offset.nc > L512.hdf5
 K=`${NCDUMP} -k L512.hdf5`
 if test "x$K" = "xnetCDF-4" ; then
 echo "***Pass: 512 offset"
@@ -27,10 +25,8 @@ FAILURES=1
 fi
 
 # Test a 1024 byte offset
-rm -f L1024.txt L1024.hdf5
-L1024="${L512}${L512}"
-echo -n "$L1024" > L1024.txt
-cat L1024.txt offset.nc > L1024.hdf5
+rm -f L1024.hdf5
+cat ${srcdir}/L512.bin ${srcdir}/L512.bin offset.nc > L1024.hdf5
 K=`${NCDUMP} -k L1024.hdf5`
 if test "x$K" = "xnetCDF-4" ; then
 echo "***Pass: 1024 offset"
@@ -40,8 +36,7 @@ FAILURES=1
 fi
 
 #cleanup
-rm -f L512.txt L512.hdf5
-rm -f L1024.txt L1024.hdf5
+rm -f L512.hdf5 L1024.hdf5
 rm -f ./offset.cdl ./offset.nc
 
 if test "x$FAILURES" = x1 ; then
