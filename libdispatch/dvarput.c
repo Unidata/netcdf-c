a07938194e32fcbe74e31958f8a68f0cb1721f67
Greg Sjaardema <gsjaardema@gmail.com>
2017-08-02 11:12:04 -0600

    Move nels==0 check instead of eliminating
    The previous change where the `nels==0` check was removed caused problems with nc_test.  
    If the check is moved after the `NC_put_vara` call, then it will avoid the parallel hang problem for stride=1, but will still pass all tests in `nc_test`.  Note that this is somewhat of a kluge since there will still be a parallel hang if nels==0 and stride is > 1 and the code falls into the odometer section.

--- libdispatch/dvarput.c
+++ libdispatch/dvarput.c
@@ -246,6 +246,14 @@ NCDEFAULT_put_vars(int ncid, int varid, const size_t * start,
       return NC_put_vara(ncid, varid, mystart, myedges, value, memtype);
    }
 
+   if(nels == 0) {
+      /* This should be here instead of before NC_put_vara call to 
+       * avoid hang in parallel write for single stride.
+       * Still issue with parallel hang if stride > 1
+       */
+      return NC_NOERR; /* cannot write anything */
+   }
+	   
    /* Initial version uses and odometer to walk the variable
       and read each value one at a time. This can later be optimized
       to read larger chunks at a time.
