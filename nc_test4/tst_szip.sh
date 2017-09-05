3cc3b6a4a1173385e4d57272f165de1a49d3ec2b
Dennis Heimbigner <dmh@ucar.edu>
2017-09-04 18:37:13 -0600

    add known szip file against which to test

--- nc_test4/tst_szip.sh
+++ nc_test4/tst_szip.sh
@@ -5,7 +5,11 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 
 set -e
 
-rm -f testnc.h5 testszip.nc
+rm -f testnc.h5 testszip.nc szip_dump.cdl
+
+echo "*** Test read of known szip file"
+${NCDUMP} ${srcdir}/ref_szip.h5 >szip_dump.cdl
+diff -w ${srcdir}/ref_szip.cdl ./szip_dump.cdl
 
 echo "*** Testing tst_szip "
 ${execdir}/test_szip
@@ -20,5 +24,6 @@ ${execdir}/h5testszip ./testszip.nc
 echo "***Passed"
 
 rm -f testnc.h5 testszip.nc
+rm -f szip_dump.cdl
 
 exit
