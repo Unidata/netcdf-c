b7f6941da801499d08330d84b4ee1a29e0cda5bf
Dennis Heimbigner <dmh@ucar.edu>
2017-09-08 13:54:02 -0600

    It is imperative that this fix goes into v4.5-release-candidate branch and master branch ASAP.
    The bug occurs in d4rc.c where strcmp is being applied to NULL.
    Also, the code in which it occurs is debugging code, so it needs
    to be #ifdef'd.  This fix may cause minor conflicts with other
    outstanding pull requests that fix the same bug. But the
    conflicts should be minor and easy to resolve.

--- oc2/ocinternal.c
+++ oc2/ocinternal.c
@@ -606,7 +606,6 @@ ocset_curlproperties(OCstate* state)
         if(path == NULL) return OC_ENOMEM;
         occopycat(path,len,3,ocglobalstate.tempdir,"/","occookies");
         stat = ocmktmp(path,&name);
-fprintf(stderr,"%s => %s\n",state->uri->uri,name); fflush(stderr);
         free(path);
 	state->curlflags.cookiejar = name;
 	state->curlflags.createdflags |= COOKIECREATED;
