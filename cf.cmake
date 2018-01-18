2e7ab58e08ec24f5beef62ada896c05ee403dc60
Dennis Heimbigner <dmh@ucar.edu>
2018-01-17 19:47:54 -0700

    Add some temporary debugging output

--- cf.cmake
+++ cf.cmake
@@ -3,6 +3,8 @@
 # Is netcdf-4 and/or DAP enabled?
 NC4=1
 #DAP=1
+CDF5=1
+#HDF4=1
 
 case "$1" in
 vs|VS) VS=1 ;;
@@ -31,19 +33,14 @@ FLAGS="-DCMAKE_PREFIX_PATH=c:/tools/nccmake"
 fi
 FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=/tmp/netcdf"
 
-if test "x$DAP" = x ; then
-FLAGS="$FLAGS -DENABLE_DAP=false"
-else
-FLAGS="$FLAGS -DENABLE_DAP=true"
-fi
-if test "x$NC4" = x ; then
-FLAGS="$FLAGS -DENABLE_NETCDF_4=false"
-fi
+if test "x$DAP" = x ; then FLAGS="$FLAGS -DENABLE_DAP=false"; fi
+if test "x$NC4" = x ; then FLAGS="$FLAGS -DENABLE_NETCDF_4=false"; fi
+if test "x$CDF5" != x ; then FLAGS="$FLAGS -DENABLE_CDF5=true"; fi
+if test "x$HDF4" != x ; then FLAGS="$FLAGS -DENABLE_HDF4=true"; fi
 FLAGS="$FLAGS -DENABLE_CONVERSION_WARNINGS=false"
 FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true"
 FLAGS="$FLAGS -DENABLE_TESTS=true"
 FLAGS="$FLAGS -DENABLE_EXAMPLES=false"
-#FLAGS="$FLAGS -DENABLE_HDF4=true"
 FLAGS="$FLAGS -DENABLE_DYNAMIC_LOADING=false"
 FLAGS="$FLAGS -DENABLE_WINSOCK2=false"
 #FLAGS="$FLAGS -DENABLE_LARGE_FILE_TESTS=true"
--- nc_test4/tst_filter.sh
+++ nc_test4/tst_filter.sh
@@ -3,6 +3,7 @@
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 . ../test_common.sh
 
+set -x
 set -e
 
 # Which test cases to exercise
@@ -101,6 +102,7 @@ echo "*** Testing dynamic filters using nccopy"
 rm -f ./unfiltered.nc ./filtered.nc ./filtered.dump ./tmp
 ${NCGEN} -4 -lb -o unfiltered.nc ${srcdir}/unfiltered.cdl
 ${NCCOPY} -F "/g/var,307,9,4" unfiltered.nc filtered.nc
+${NCDUMP} -sh filtered.nc
 ${NCDUMP} -s filtered.nc > ./tmp
 # Remove irrelevant -s output
 sclean ./tmp ./filtered.dump
