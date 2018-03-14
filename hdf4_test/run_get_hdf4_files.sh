d805389f63cdc7894cc5f8e1ee45706b9262572f
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-03-14 14:15:40 -0600

    changed hdf4 script to try getting files multiple times

--- hdf4_test/run_get_hdf4_files.sh
+++ hdf4_test/run_get_hdf4_files.sh
@@ -18,11 +18,31 @@ file_list="AMSR_E_L2_Rain_V10_200905312326_A.hdf AMSR_E_L3_DailyLand_V06_2002061
     MOD29.A2000055.0005.005.2006267200024.hdf"
 echo "Getting HDF4 test files $file_list"
 
+# Try to get files 3 times, with a random delay between attempts.
 for f1 in $file_list
 do
     if ! test -f $f1; then
-	wget "ftp://ftp.unidata.ucar.edu/pub/netcdf/sample_data/hdf4/$f1.gz"
-	gunzip $f1.gz
+        failed=1
+        num_tries=0
+        delay=3
+        while [ $failed -eq 1 -a $num_tries -lt 3 ]
+        do
+	    if wget "ftp://ftp.unidata.ucar.edu/pub/netcdf/sample_data/hdf4/$f1.gz"; then
+                failed=0
+                num_tries=0
+                delay=3
+	        gunzip -f $f1.gz
+            else
+                sleep $delay
+                num_tries=`expr $num_tries + 1`
+                delay=$(( RANDOM % (10 - 3 + 1 ) + 5 ))
+            fi
+        done
+        # If we can't get a file, we can't proceed.
+        if test $failed -eq 1; then
+            echo "Could not get HDF4 files from Unidata FTP site!"
+            exit 1
+        fi
     fi
 done
 
