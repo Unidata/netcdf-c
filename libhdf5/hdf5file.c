44e73bca15837ea3b2cc50f84a2bb8680c178387
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-04-11 10:05:04 -0600

    working on dim

--- libhdf5/hdf5file.c
+++ libhdf5/hdf5file.c
@@ -1104,6 +1104,7 @@ read_scale(NC_GRP_INFO_T *grp, hid_t datasetid, const char *obj_name,
 
          /* Hold open the dataset, since the dimension doesn't have a coordinate variable */
          new_dim->hdf_dimscaleid = datasetid;
+         hdf5_dim->hdf_dimscaleid = datasetid;
          H5Iinc_ref(new_dim->hdf_dimscaleid);        /* Increment number of objects using ID */
       }
    }
