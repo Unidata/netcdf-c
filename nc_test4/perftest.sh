0584789bfbbc351f29034e7e50591985bba4fe0d
Dennis Heimbigner <dmh@ucar.edu>
2018-04-14 12:01:52 -0600

    missing file

--- nc_test4/perftest.sh
+++ nc_test4/perftest.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-BUILD=1
 #PROF=1
 #DEBUG=1
 #MEM=1
@@ -8,6 +7,7 @@ BUILD=1
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 . ../test_common.sh
 
+set -x
 set -e
 
 echo "Testing performance of nc_create and nc_open on file with large metadata"
@@ -27,28 +27,10 @@ elif test "x$DEBUG" = x1 ; then
 CMD="gdb --args "
 fi
 
-if test "x$BUILD" = x1 ; then
-CFLAGS="-Wall -Wno-unused-variable -Wno-unused-function -I.. -I../include"
-LDFLAGS="../liblib/.libs/libnetcdf.a -L/usr/local/lib -lhdf5_hl -lhdf5 -lz -ldl -lcurl -lm -lmfhdf -ldf"
-CC=gcc
-if test "x$PROF" = x1 ; then
-CFLAGS="-pg $CFLAGS"
-LDFLAGS="-pg $LDFLAGS"
-fi
-if test "x$DEBUG" = x1 ; then
-CFLAGS="-g -O0 $CFLAGS"
-LDFLAGS="-g -O0 $LDFLAGS"
-fi
-LLP="/usr/local/lib:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH=${LLP}; export CFLAGS; export LDFLAGS
-${CC} -o bigmeta ${CFLAGS} bigmeta.c  ${LDFLAGS}
-${CC} -o openbigmeta ${CFLAGS} openbigmeta.c  ${LDFLAGS}
-fi
-
 echo "timing bigmeta:"
-${CMD} ./bigmeta $ARGS
+${execdir}/bigmeta $ARGS
 echo "timing openbigmeta:"
-${CMD} ./openbigmeta
+${execdir}/openbigmeta
 if test "x$PROF" = x1 ; then
 rm -f perftest.txt
 gprof openbigmeta gmon.out >perftest.txt
