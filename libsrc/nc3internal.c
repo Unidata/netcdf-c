6de055057ad72e77d9ee233a039ff86f3aa258cd
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-04-19 07:40:22 -0600

    more work on configure

--- libsrc/nc3internal.c
+++ libsrc/nc3internal.c
@@ -1774,19 +1774,14 @@ int
 nc_delete_mp(const char * path, int basepe)
 {
 	NC *nc;
-	NC3_INFO* nc3;
 	int status;
 	int ncid;
-	size_t chunk = 512;
 
 	status = nc_open(path,NC_NOWRITE,&ncid);
         if(status) return status;
 
 	status = NC_check_id(ncid,&nc);
         if(status) return status;
-	/* nc3 = NC3_DATA(nc); */
-
-	/* nc3->chunk = chunk; */
 
 #if defined(LOCKNUMREC) /* && _CRAYMPP */
 	if (status = NC_init_pe(nc3, basepe)) {
