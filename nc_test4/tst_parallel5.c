941875e3f06a2c34ca20c4093da1f1def6e585ba
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-05-14 14:25:56 -0600

    test development

--- nc_test4/tst_parallel5.c
+++ nc_test4/tst_parallel5.c
@@ -12,12 +12,14 @@
 #include <mpi.h>
 
 #define FILE "tst_parallel5.nc"
-#define VAR_NAME "Odysseus"
-#define DIM_NAME "height"
+#define VAR_NAME "TheIrishRover"
+#define DIM_NAME "number_of_masts"
+#define MASTS 27
 #define NDIMS1 1
 #define DIMSIZE 4
 #define NUM_PROC 4
 #define NUM_SLABS 10
+#define NUM_ACCESS_TESTS 2
 
 int
 main(int argc, char **argv)
@@ -27,11 +29,12 @@ main(int argc, char **argv)
    MPI_Info info = MPI_INFO_NULL;
    int ncid, v1id, dimid;
    size_t start[NDIMS1] = {0}, count[NDIMS1] = {0};
-   int data = TEST_VAL_42;
-   int data_in = TEST_VAL_42 + 1;
+   int data = MASTS;
+   int data_in = TEST_VAL_42;
+   int acc;
 
    /* Initialize MPI. */
-   MPI_Init(&argc,&argv);
+   MPI_Init(&argc, &argv);
    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
@@ -44,40 +47,42 @@ main(int argc, char **argv)
       printf("*** tst_parallel testing whether we can write 0 elements from some tasks...");
    }
 
-   /* Create a parallel netcdf-4 file. */
-   /*nc_set_log_level(3);*/
-   if (nc_create_par(FILE, NC_NETCDF4|NC_MPIIO, comm, info, &ncid)) ERR;
+   for (acc = 0; acc < NUM_ACCESS_TESTS; acc++)
+   {
+      /* Create a parallel netcdf-4 file. */
+      /*nc_set_log_level(3);*/
+      if (nc_create_par(FILE, NC_NETCDF4|NC_MPIIO, comm, info, &ncid)) ERR;
 
-   /* Create a dimension. */
-   if (nc_def_dim(ncid, DIM_NAME, DIMSIZE, &dimid)) ERR;
+      /* Create a dimension. */
+      if (nc_def_dim(ncid, DIM_NAME, DIMSIZE, &dimid)) ERR;
 
-   /* Create one var. */
-   if (nc_def_var(ncid, VAR_NAME, NC_INT, NDIMS1, &dimid, &v1id)) ERR;
+      /* Create one var. */
+      if (nc_def_var(ncid, VAR_NAME, NC_INT, NDIMS1, &dimid, &v1id)) ERR;
 
-   /* Write metadata to file. */
-   if (nc_enddef(ncid)) ERR;
+      /* Write metadata to file. */
+      if (nc_enddef(ncid)) ERR;
 
-   /* Set up slab for this process. */
-   if (!mpi_rank)
-      count[0] = 1;
+      /* Set up slab for this process. */
+      if (!mpi_rank)
+         count[0] = 1;
 
-   if (nc_var_par_access(ncid, v1id, NC_COLLECTIVE)) ERR;
-/*    if (nc_var_par_access(ncid, v1id, NC_INDEPENDENT)) ERR;*/
+      if (nc_var_par_access(ncid, v1id, acc ? NC_COLLECTIVE : NC_INDEPENDENT)) ERR;
 
-   /* Write phoney data. */
-   if (nc_put_vara_int(ncid, v1id, start, count, &data)) ERR;
+      /* Write phoney data. */
+      if (nc_put_vara_int(ncid, v1id, start, count, &data)) ERR;
 
-   if (nc_sync(ncid)) ERR;
+      if (nc_sync(ncid)) ERR;
 
-   /* Read phoney data. */
-   if (nc_get_vara_int(ncid, v1id, start, count, &data_in)) ERR;
+      /* Read phoney data. */
+      if (nc_get_vara_int(ncid, v1id, start, count, &data_in)) ERR;
 
-   /* Task 0 has TEST_VAL_42, the others have data_in remaining, as
-    * initialized, at TEST_VAL_42 + 1. */
-   if (data_in != (mpi_rank ? (TEST_VAL_42 + 1) : TEST_VAL_42)) ERR;
+      /* Task 0 has MASTS, the others have data_in remaining, as
+       * initialized, at TEST_VAL_42. */
+      if (data_in != (mpi_rank ? TEST_VAL_42 : MASTS)) ERR;
 
-   /* Close the netcdf file. */
-   if (nc_close(ncid)) ERR;
+      /* Close the netcdf file. */
+      if (nc_close(ncid)) ERR;
+   }
 
    /* Shut down MPI. */
    MPI_Finalize();
