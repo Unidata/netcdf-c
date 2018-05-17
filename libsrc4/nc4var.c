9252ae53dfec1d6f0fae030323ab2d1e8d352437
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-05-17 06:49:49 -0600

    removed dead code associated with old handling of NC_LONG

--- libsrc4/nc4var.c
+++ libsrc4/nc4var.c
@@ -1673,7 +1673,7 @@ NC4_put_vara(int ncid, int varid, const size_t *startp,
    if (!(nc = nc4_find_nc_file(ncid, NULL)))
       return NC_EBADID;
 
-   return nc4_put_vara(nc, ncid, varid, startp, countp, memtype, 0, (void *)op);
+   return nc4_put_vara(nc, ncid, varid, startp, countp, memtype, (void *)op);
 }
 
 /**
@@ -1705,6 +1705,5 @@ NC4_get_vara(int ncid, int varid, const size_t *startp,
       return NC_EBADID;
 
    /* Get the data. */
-   return nc4_get_vara(nc, ncid, varid, startp, countp, memtype,
-                       0, (void *)ip);
+   return nc4_get_vara(nc, ncid, varid, startp, countp, memtype, (void *)ip);
 }
