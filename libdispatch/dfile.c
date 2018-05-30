dbacd00c9c10ccb8ff80aa2dbd5000ea73ae2907
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-05-30 03:29:19 -0600

    fixing windows build problem, plus documentation

--- libdispatch/dfile.c
+++ libdispatch/dfile.c
@@ -68,39 +68,8 @@ static char HDF5_SIGNATURE[MAGIC_NUMBER_LEN] = "\211HDF\r\n\032\n";
 /* User-defined formats. */
 NC_Dispatch* UF0_dispatch_table = NULL;
 NC_Dispatch* UF1_dispatch_table = NULL;
-
-/**
- * Add handling of user-defined format.
- *
- * @param mode_flag NC_UF0 or NC_UF1
- * @param dispatch_table Pointer to dispatch table to use for this user format.
- * @param magic_numer Magic number used to identify file. Ignored if NULL.
- *
- * @return ::NC_NOERR No error.
- * @return ::NC_EINVAL Invalid input.
- * @author Ed Hartnett
- */
-int
-nc_def_user_format(int mode_flag, NC_Dispatch *dispatch_table, char *magic_number)
-{
-   if (mode_flag != NC_UF0 && mode_flag != NC_UF1)
-      return NC_EINVAL;
-   if (!dispatch_table)
-      return NC_EINVAL;
-
-   switch(mode_flag)
-   {
-   case NC_UF0:
-      UF0_dispatch_table = dispatch_table;
-      break;
-   case NC_UF1:
-      UF1_dispatch_table = dispatch_table;
-      break;
-   }
-   
-   return NC_NOERR;
-}
 #endif /* USE_NETCDF4 */
+
 
 /** \defgroup datasets NetCDF File and Data I/O
 
@@ -137,6 +106,44 @@ any.
 interfaces, the rest of this chapter presents a detailed description
 of the interfaces for these operations.
 */
+
+#ifdef USE_NETCDF4
+/**
+ * Add handling of user-defined format.
+ *
+ * @param mode_flag NC_UF0 or NC_UF1
+ * @param dispatch_table Pointer to dispatch table to use for this user format.
+ * @param magic_number Magic number used to identify file. Ignored if
+ * NULL.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EINVAL Invalid input.
+ * @author Ed Hartnett
+ * @ingroup datasets
+ */
+int
+nc_def_user_format(int mode_flag, NC_Dispatch *dispatch_table, char *magic_number)
+{
+   /* Check inputs. */
+   if (mode_flag != NC_UF0 && mode_flag != NC_UF1)
+      return NC_EINVAL;
+   if (!dispatch_table)
+      return NC_EINVAL;
+
+   /* Retain a pointer to the dispatch_table. */
+   switch(mode_flag)
+   {
+   case NC_UF0:
+      UF0_dispatch_table = dispatch_table;
+      break;
+   case NC_UF1:
+      UF1_dispatch_table = dispatch_table;
+      break;
+   }
+   
+   return NC_NOERR;
+}
+#endif /* USE_NETCDF4 */
 
 /*!
   Interpret the magic number found in the header of a netCDF file.
