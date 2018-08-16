c7c1f82c1cc6a7d1ada00b00dfb6ab7684ae9d6d
Ward Fisher <wfisher@ucar.edu>
2018-08-16 11:31:35 -0600

    Fixed syntax error.  Also seeing new issues in relationship to https://github.com/Unidata/netcdf-c/issues/1113

--- nc_test/run_inmemory.sh
+++ nc_test/run_inmemory.sh
@@ -23,7 +23,7 @@ CHECKERR() {
 
     RES=$?
 
-        if [[ $RES -ne 0 ]]; then
+    if [[ $RES -ne 0 ]]; then
         echo "Error Caught: $RES"
         exit $RES
     fi
@@ -37,24 +37,25 @@ HASNC4=`${top_builddir}/nc-config --has-nc4`
 
 # Execute the core of the inmemory tests
 if ! ${execdir}/tst_inmemory; then
-   echo "FAIL: tst.inmemory"
+    echo "FAIL: tst.inmemory"
 fi
 if ! ${execdir}/tst_open_mem ${srcdir}/${OMEMFILE}; then
-   echo "FAIL: tst_open_mem"
+    echo "FAIL: tst_open_mem"
 fi
 
 echo "**** Test ncdump of the resulting inmemory data"
 ${NCDUMP} -n "${FILE3}" ${FILE3}.nc > ${FILE3}.cdl ; CHECKERR
 ${NCDUMP} -n "${FILE3}" ${CREATE3}.nc > ${CREATE3}.cdl ; CHECKERR
 if ! diff -wb ${FILE3}.cdl ${CREATE3}.cdl ; then
-   echo "FAIL: ${FILE3}.cdl vs ${CREATE3}.cdl"
+    echo "FAIL: ${FILE3}.cdl vs ${CREATE3}.cdl"
 fi
 
 if test "x$HASNC4" = "xyes" ; then
-${NCDUMP} ${FILE4}.nc > ${FILE4}.cdl ; CHECKERR
-${NCDUMP} ${CREATE4}.nc > ${CREATE4}.cdl ; CHECKERR
-if diff -wb ${FILE4}.cdl ${CREATE4}.cdl ; then
-   echo "FAIL: ${FILE4}.cdl vs ${CREATE4}.cdl"
+    ${NCDUMP} ${FILE4}.nc > ${FILE4}.cdl ; CHECKERR
+    ${NCDUMP} ${CREATE4}.nc > ${CREATE4}.cdl ; CHECKERR
+    if diff -wb ${FILE4}.cdl ${CREATE4}.cdl ; then
+        echo "FAIL: ${FILE4}.cdl vs ${CREATE4}.cdl"
+    fi
 fi
 
 # cleanup
