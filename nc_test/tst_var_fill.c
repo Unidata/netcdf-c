a64ab9aeb7e97b9d8e690a8a49787903053578b6
Wei-keng Liao <wkliao@users.noreply.github.com>
2018-08-16 15:39:57 -0500

    For NetCDF4 files, change all variables fill mode when nc_set_fill is called.  For classic files, this is already done.

---
+++ nc_test/tst_var_fill.c
@@ -0,0 +1,104 @@
+/* This is part of the netCDF package.
+ * Copyright 2005 University Corporation for Atmospheric Research/Unidata
+ * See COPYRIGHT file for conditions of use.
+ *
+ * Test whether nc_inq_var_fill() reports fill mode correctly after calling
+ * nc_set_fill().
+ *
+ * Author: Wei-keng Liao.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netcdf.h>
+
+#include "tests.h"
+
+#define CHECK_ERR { \
+    if (err != NC_NOERR) { \
+        nerrs++; \
+        printf("Error at line %d in %s: (%s)\n", \
+        __LINE__,__FILE__,nc_strerror(err)); \
+    } \
+}
+
+int main(int argc, char** argv) {
+    char filename[256];
+    int j, k, err, nerrs=0, ncid, varid0, varid1, no_fill;
+    char *fmats[5]={"NC_FORMAT_CLASSIC",
+                    "NC_FORMAT_64BIT_OFFSET",
+                    "NC_FORMAT_CDF5",
+                    "NC_FORMAT_NETCDF4",
+                    "NC_FORMAT_NETCDF4_CLASSIC"};
+    int formats[5]={NC_FORMAT_CLASSIC,
+                    NC_FORMAT_64BIT_OFFSET,
+                    NC_FORMAT_CDF5,
+                    NC_FORMAT_NETCDF4,
+                    NC_FORMAT_NETCDF4_CLASSIC};
+    int fill_mode[2]={NC_FILL, NC_NOFILL};
+
+    if (argc > 2) {
+        printf("Usage: %s [filename]\n",argv[0]);
+        return 1;
+    }
+    if (argc == 2) snprintf(filename, 256, "%s", argv[1]);
+    else           strcpy(filename, "tst_def_var_fill.nc");
+
+    char *cmd_str = (char*)malloc(strlen(argv[0]) + 256);
+    sprintf(cmd_str, "*** TESTING C   %s for variable fill mode ", argv[0]);
+    printf("%-66s ------ ", cmd_str); fflush(stdout);
+    free(cmd_str);
+
+    for (k=0; k<5; k++) {
+#ifndef ENABLE_CDF5
+        if (formats[k] == NC_FORMAT_CDF5) continue;
+#endif
+#ifndef USE_NETCDF4
+        if (formats[k] == NC_FORMAT_NETCDF4 ||
+            formats[k] == NC_FORMAT_NETCDF4_CLASSIC)
+            continue;
+#endif
+        nc_set_default_format(formats[k], NULL);
+
+        for (j=0; j<2; j++) {
+            /* create a new file for writing --------------------------------*/
+            err = nc_create(filename, NC_CLOBBER, &ncid); CHECK_ERR
+            err = nc_def_var(ncid, "var0", NC_INT, 0, NULL, &varid0); CHECK_ERR
+
+            /* set fill mode for the entire file */
+            err = nc_set_fill(ncid, fill_mode[j], NULL); CHECK_ERR
+
+            /* all variables defined after nc_set_fill() should inherit the
+             * fill mode */
+            err = nc_def_var(ncid, "var1", NC_INT, 0, NULL, &varid1); CHECK_ERR
+            err = nc_enddef(ncid); CHECK_ERR
+
+            err = nc_inq_var_fill(ncid, varid0, &no_fill, NULL); CHECK_ERR
+            if (no_fill && fill_mode[j] == NC_FILL) {
+                printf("\nFAIL: %s var0 expect NOFILL but got FILL\n",fmats[k]);
+                nerrs++;
+            }
+            else if (!no_fill && fill_mode[j] == NC_NOFILL) {
+                printf("\nFAIL: %s var0 expect FILL but got NOFILL\n",fmats[k]);
+                nerrs++;
+            }
+            err = nc_inq_var_fill(ncid, varid1, &no_fill, NULL); CHECK_ERR
+            if (no_fill && fill_mode[j] == NC_FILL) {
+                printf("\nFAIL: %s var1 expect NOFILL but got FILL\n",fmats[k]);
+                nerrs++;
+            }
+            else if (!no_fill && fill_mode[j] == NC_NOFILL) {
+                printf("\nFAIL: %s var1 expect FILL but got NOFILL\n",fmats[k]);
+                nerrs++;
+            }
+            err = nc_close(ncid); CHECK_ERR
+        }
+    }
+
+    if (nerrs) printf("fail with %d mismatches\n",nerrs);
+    else       printf("pass\n");
+
+    return (nerrs > 0);
+}
+
