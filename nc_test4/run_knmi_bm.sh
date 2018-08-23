58e8dc4ac58499755ec44528937cfe32972ef173
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-08-23 06:32:04 -0600

    got knmi benchmark test working with parallel builds

--- nc_test4/run_knmi_bm.sh
+++ nc_test4/run_knmi_bm.sh
@@ -2,6 +2,9 @@
 
 # This shell gets files from the netCDF ftp site for testing.
 
+if test "x$srcdir" = x ; then srcdir=`pwd`; fi
+. ../test_common.sh
+
 set -e
 echo ""
 file_list="MSGCPP_CWP_NC3.nc  MSGCPP_CWP_NC4.nc"
@@ -14,6 +17,9 @@ do
 	gunzip $f1.gz
     fi
 done
+
+# Run the C program on these files.
+${execdir}/tst_knmi
 
 echo "SUCCESS!!!"
 
