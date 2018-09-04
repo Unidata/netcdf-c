8aa2fedbbf71b4d314c0e256f745b471f96fd6c0
Dennis Heimbigner <dmh@ucar.edu>
2018-09-04 11:27:47 -0600

    Fix the NC_INMEMORY code to work in all cases with HDF5 1.10.
    re: github issue https://github.com/Unidata/netcdf-c/issues/1111
    
    One of the less common use cases for the in-memory feature is
    apparently failing with HDF5-1.10.x.  The fix is complicated and
    requires significant changes to libhdf5/nc4memcb.c. The current
    setup is detailed in the file docs/inmeminternal.dox.
    
    Additionally, it was discovered that the program
    nc_test/tst_inmemory.c, which is invoked by
    nc_test/run_inmemory.sh, actually was failing because of the
    above problem. But the failure is not detected since the script
    does not return non-zero value.
    
    Other Changes:
    1. Fix nc_test_tst_inmemory to return errors correctly.
    2. Make ncdap_tests/findtestserver.c and dap4_tests/findtestserver4.c
       be generated from ncdap_test/findtestserver.c.in.
    3. Make LOG() print output to stderr instead of stdout to
       avoid contaminating e.g. ncdump output.
    4. Modify the handling of NC_INMEMORY and NC_DISKLESS flags
       to properly handle that NC_DISKLESS => NC_INMEMORY. This
       affects a number of code pieces, especially memio.c.

--- include/hdf5internal.h
+++ include/hdf5internal.h
@@ -60,6 +60,9 @@ typedef struct  NC_HDF5_FILE_INFO
 int rec_detach_scales(NC_GRP_INFO_T *grp, int dimid, hid_t dimscaleid);
 int rec_reattach_scales(NC_GRP_INFO_T *grp, int dimid, hid_t dimscaleid);
 
+/* In-memory functions */
+extern hid_t NC4_image_init(NC_FILE_INFO_T* h5);
+extern void NC4_image_finalize(void*);
 /* These functions are internal to the libhdf5 directory. */
 int nc4_detect_preserve_dimids(NC_GRP_INFO_T *grp, nc_bool_t *bad_coord_orderp);
 int hdf5_set_log_level();
