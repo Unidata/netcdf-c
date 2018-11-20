9e970562cfd31343842150fb45237a4397e12cf9
Ed Hartnett <edwardjameshartnett@gmail.com>
2018-11-20 10:01:39 -0700

    more type work

--- libhdf5/hdf5internal.c
+++ libhdf5/hdf5internal.c
@@ -633,9 +633,11 @@ nc4_rec_grp_HDF5_del(NC_GRP_INFO_T *grp)
       if (type->hdf_typeid && H5Tclose(type->hdf_typeid) < 0)
          return NC_EHDFERR;
       type->hdf_typeid = 0;
+      hdf5_type->hdf_typeid = 0;
       if (type->native_hdf_typeid && H5Tclose(type->native_hdf_typeid) < 0)
          return NC_EHDFERR;
       type->native_hdf_typeid = 0;
+      hdf5_type->native_hdf_typeid = 0;
    }
 
    /* Close the HDF5 group. */
