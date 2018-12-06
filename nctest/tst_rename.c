921a217621fb39e6f99fc28636d8de894fd4d209
Ward Fisher <wfisher@ucar.edu>
2018-12-06 15:47:47 -0700

    nctest, oc2 files updated.

--- nctest/tst_rename.c
+++ nctest/tst_rename.c
@@ -1,5 +1,5 @@
 /* This is part of the netCDF package.
-   Copyright 2010 University Corporation for Atmospheric Research/Unidata
+   Copyright 2018 University Corporation for Atmospheric Research/Unidata
    See COPYRIGHT file for conditions of use.
 
    Test dim rename that is causing problems with v2 API.
