660bda1be342a3a072f5a263e274b988d83b2ac8
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-01-27 11:14:29 -0700

    made function give_var_secret_name() static again

--- libhdf5/hdf5var.c
+++ libhdf5/hdf5var.c
@@ -233,8 +233,8 @@ nc4_find_default_chunksizes2(NC_GRP_INFO_T *grp, NC_VAR_INFO_T *var)
  * @returns ::NC_ENOMEM Out of memory.
  * @author Ed Hartnett
  */
-int
-nc4_give_var_secret_name(NC_VAR_INFO_T *var)
+static int
+give_var_secret_name(NC_VAR_INFO_T *var)
 {
    /* Set a different hdf5 name for this variable to avoid name
     * clash. */
@@ -531,7 +531,7 @@ NC4_def_var(int ncid, const char *name, nc_type xtype,
     * and this var has the same name. */
    dim = (NC_DIM_INFO_T*)ncindexlookup(grp->dim,norm_name);
    if (dim && (!var->ndims || dimidsp[0] != dim->hdr.id))
-      if ((retval = nc4_give_var_secret_name(var)))
+      if ((retval = give_var_secret_name(var)))
          BAIL(retval);
 
    /* If this is a coordinate var, it is marked as a HDF5 dimension
@@ -1160,6 +1160,8 @@ NC4_rename_var(int ncid, int varid, const char *name)
       /* Break up the coordinate variable */
       if ((retval = nc4_break_coord_var(grp, var, var->dim[0])))
          return retval;
+      /* if ((retval = give_var_secret_name(var))) */
+      /*    return retval; */
    }
 
    /* Check if this should become a coordinate variable. */
