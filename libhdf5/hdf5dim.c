7f7110bf3e4a9b54e6bdc18c8f771e26c0216bfb
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-02-03 07:43:56 -0700

    comment cleanup

--- libhdf5/hdf5dim.c
+++ libhdf5/hdf5dim.c
@@ -1,4 +1,4 @@
-/* Copyright 2003-2018, University Corporation for Atmospheric
+/* Copyright 2003-2019, University Corporation for Atmospheric
  * Research. See the COPYRIGHT file for copying and redistribution
  * conditions. */
 /**
