efe26a4627c2618f46f5a8acd850ebc777d1488c
Dennis Heimbigner <dmh@ucar.edu>
2019-02-07 13:08:16 -0700

    ckp

--- libdispatch/dmutex.c
+++ libdispatch/dmutex.c
@@ -1,20 +1,19 @@
 /** \file
 Global data
 
-This file stores all known global data for the netcdf-c library.
-It provides two structure types and instances.
-1. Mutable data
-2. Constant data 
+This file wraps the handling of mutual exclusion structures (mutexes).
 
-Both must be locked for initialization, but after that the constant data
-can be accessed without locking.  Access to the mutable data must always
-occur in a critical section.
+There exists a single, global mutex to control access to data
+that is shared across more than one dispatch table.
+There also is support for a per-dispatch table mutex.
+The later allows for individual dispatchers to implement
+finer-grain locking as opposed to locking the whole API.
 */
 
-/*
-Copyright 2010 University Corporation for Atmospheric
-Research/Unidata. See COPYRIGHT file for more info.
-*/
+/*********************************************************************
+*    Copyright 2019, UCAR/Unidata
+*    See netcdf/COPYRIGHT file for copying and redistribution conditions.
+* ********************************************************************/
 
 #include "config.h"
 
@@ -22,102 +21,94 @@ Research/Unidata. See COPYRIGHT file for more info.
 #include <stdlib.h>
 #include <stdio.h>
 #endif
-#ifdef ENABLE_THREADSAFE
+
 #ifdef HAVE_LIBPTHREAD
 #include <pthread.h>
 #endif
+#include <ncmutex.h>
+
+#if !defined(HAVE_LIBPTHREAD) && !defined(_WIN32)
+#error "Cannot find thread support library"
 #endif
 
-#ifdef ENABLE_THREADSAFE
+typedef struct NCmutex {
+#ifdef HAVE_LIBPTHREAD
+    pthread_mutex_t mutex;
+#endif
+#ifdef _WIN32
+    CRITICAL_SECTION mutex;
+#endif
+} NCmutex;
+
+static volatile NCmutex NC_globalmutex; /* globally accessible */
+
+static volatile int global_mutex_initialized = 0; /* initialize once */
 
 #ifdef HAVE_LIBPTHREAD
-
-static pthread_mutex_t NC_globalmutex; /* globally accessible */
 static pthread_once_t once_control = PTHREAD_ONCE_INIT;  /* for pthread_once */
-
-#elif defined(_MSC_VER)
-
-static CRITICAL_SECTION NC_globalmutex;
-
 #endif
 
-static int mutex_initialized = 1; /* set to zero if we failed to initialize */
-
 #ifdef HAVE_LIBPTHREAD
-static void ncmutexinit(void)
+static void
+ncmutexinit(NCmutex* mutex)
 {
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
     pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL);
-    if(pthread_mutex_init(&NC_globalmutex, &attr))
+#if 0
+    if(pthread_mutex_init(&mutex->mutex, &attr))
 	mutex_initialized = 0;
     else
 	mutex_initialized = 1;
+#endif
     pthread_mutexattr_destroy(&attr);
 }
 
-#endif
-
-int
-NC_mutex_initialize(void)
+/* Global mutex init; for use with once */
+static void
+globalncmutexinit(void)
 {
-    if(mutex_initialized) return 1;
+    ncmutextinit(&NC_globalmutex);
+}
+
+#endif /*HAVE_LIBPTHREAD*/
+
+/* Thread module init */
+int
+NC_global_mutex_initialize(void)
+{
+    if(global_mutex_initialized) return 1;
 
 #ifdef HAVE_LIBPTHREAD
     /* We use pthread_once to guarantee that the mutex is initialized */
-    pthread_once(&once_control,ncmutexinit);
+    pthread_once(&once_control,globalncmutexinit);
 #elif defined(_MSC_VER)
     InitializeCriticalSectionAndSpinCount(&NC_globalmutex, 4000); /* is this itself thread-safe?*/
-    mutex_initialized = 1;
 #endif
-    if(!mutex_initialized) {
-	fprintf(stderr,"nc_global_init: NC_globalmutex not initialized\n");
-	return 0;
-    }
-    return 1;
+    mutex_initialized = 1;
+    return mutex_initialized;
 }    
 
 void
-NC_lock(void)
+NC_lock(void* mutex0)
 {
+    NCmutext* mutex = mutex0;
 #ifdef HAVE_LIBPTHREAD
-    pthread_mutex_lock(&NC_globalmutex);
-#elif defined(_MSC_VER)
-    EnterCriticalSection(&NC_globalmutex);
-#else
-#error "no mutex available"
+    pthread_mutex_lock(&mutex->mutex);
+#endif
+#ifdef _WIN32
+    EnterCriticalSection(&mutex->mutex);
 #endif
 }
 
 void
-NC_unlock(void)
+NC_unlock(void* mutex0)
 {
+    NCmutext* mutex = mutex0;
 #ifdef HAVE_LIBPTHREAD
-    pthread_mutex_unlock(&NC_globalmutex);
-#elif defined(_MSC_VER)
-    LeaveCriticalSection(&NC_globalmutex);
-#else
-#error "no mutex available"
+    pthread_mutex_unlock(&mutex->mutex);
+#endif
+#ifdef _WIN32
+    LeaveCriticalSection(&mutex->mutex);
 #endif
 }
-
-#else /*!ENABLE_THREADSAFE*/
-
-int
-NC_mutex_initialize(void)
-{
-    return 1;
-}
-
-void
-NC_lock(void)
-{
-}
-
-void
-NC_unlock(void)
-{
-}
-
-#endif /*ENABLE_THREADSAFE*/
-
