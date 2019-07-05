39ba4aae892ffe212f4dd7412650e89c3099ca44
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-07-05 04:50:36 -0600

    added netcdf_dispatch.h file

---
+++ include/netcdf_dispatch.h
@@ -0,0 +1,15 @@
+/* Copyright 2018-2018 University Corporation for Atmospheric
+   Research/Unidata. */
+/**
+ * @file
+ * @internal Includes prototypes for core dispatch functionality.
+ *
+ * @author Dennis Heimbigner
+ */
+
+#ifndef NETCDF_DISPATCH_H
+#define NETCDF_DISPATCH_H
+
+#include "config.h"
+
+#endif /* NETCDF_DISPATCH_H */
