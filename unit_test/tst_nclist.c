592ac51a636270fd14840bcef7521e5117ad6e1f
edwardhartnett <edwardjameshartnett@gmail.com>
2019-08-09 09:31:24 -0600

    starting to add unit tests

---
+++ unit_test/tst_nclist.c
@@ -0,0 +1,26 @@
+/* This is part of the netCDF package. Copyright 2005-2019 University
+   Corporation for Atmospheric Research/Unidata. See COPYRIGHT file
+   for conditions of use.
+
+   Test list functions in nclistmgr.c.
+
+   Ed Hartnett, 8/10/19
+*/
+
+#include "config.h"
+#include <nc_tests.h>
+#include "err_macros.h"
+
+int
+main(int argc, char **argv)
+{
+   printf("\n*** Testing netcdf internal NC list functions.\n");
+   {
+
+       printf("Testing NC list functions...");
+       {
+       }
+       SUMMARIZE_ERR;
+   }
+   FINAL_RESULTS;
+}
