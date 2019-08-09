3426b5338683a6af7db2f5b0ae077c52ae9a31ed
edwardhartnett <edwardjameshartnett@gmail.com>
2019-08-09 11:13:55 -0600

    more documentation

--- unit_test/tst_nclist.c
+++ unit_test/tst_nclist.c
@@ -15,13 +15,28 @@
 int
 main(int argc, char **argv)
 {
-   printf("\n*** Testing netcdf internal NC list functions.\n");
-   {
+    printf("\n*** Testing netcdf internal NC list functions.\n");
+    printf("Testing NC list functions with no open files...");
+    {
+        /* These are cases where there are no files, or NULL
+         * params. */
+        if (count_NCList()) ERR;
+        free_NCList();
+        if (find_in_NCList_by_name("nope")) ERR;
+        if (iterate_NCList(0, NULL)) ERR;
+    }
+    SUMMARIZE_ERR;
+    printf("Testing adding to NC list...");
+    {
+        /* NC *ncp; */
+    /* int ret; */
 
-       printf("Testing NC list functions...");
-       {
-       }
-       SUMMARIZE_ERR;
-   }
-   FINAL_RESULTS;
+        /* /\* Create the NC* instance and insert its dispatcher and model. *\/ */
+        /* if ((ret = new_NC(dispatcher, path, cmode, &model, &ncp))) ERR; */
+
+        /* /\* Add to list of known open files and define ext_ncid. *\/ */
+        /* add_to_NCList(ncp); */
+    }
+    SUMMARIZE_ERR;
+    FINAL_RESULTS;
 }
