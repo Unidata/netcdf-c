8b8ece4f4b69a0c042af9ffe479f4e713f81189a
edwardhartnett <edwardjameshartnett@gmail.com>
2019-08-09 13:49:52 -0600

    more testing of nclistmgr.c

--- unit_test/tst_nclist.c
+++ unit_test/tst_nclist.c
@@ -13,6 +13,9 @@
 #include "ncdispatch.h"
 #include "err_macros.h"
 
+/* An integer value to use in testing. */
+#define TEST_VAL_42 42
+
 int
 main(int argc, char **argv)
 {
@@ -29,7 +32,8 @@ main(int argc, char **argv)
     SUMMARIZE_ERR;
     printf("Testing adding to NC list...");
     {
-        NC *ncp;
+        /* int ncid; */
+        NC *ncp, *ncp2;
         int mode = 0;
         NCmodel model;
         char path[] = {"file.nc"};
@@ -40,6 +44,23 @@ main(int argc, char **argv)
 
         /* Add to list of known open files and define ext_ncid. */
         add_to_NCList(ncp);
+
+        /* These won't work! */
+        if (find_in_NCList(TEST_VAL_42)) ERR;
+        if (find_in_NCList_by_name("nope")) ERR;
+        if ((ret = iterate_NCList(2, &ncp2))) ERR;
+
+        /* Find it in the list. */
+        if (!(ncp2 = find_in_NCList(ncp->ext_ncid))) ERR;
+        if (!(ncp2 = find_in_NCList_by_name(path))) ERR;
+        if ((ret = iterate_NCList(1, &ncp2))) ERR;
+        if (count_NCList() != 1) ERR;
+
+        /* Delete it. */
+        /* ncid = ncp->ext_ncid; */
+        /* free_NC(ncp); */
+        /* del_from_NCList(ncp); */
+        /* if (find_in_NCList(ncid)) ERR; */
 
         /* Where is the allocated memory being freed? */
     }
