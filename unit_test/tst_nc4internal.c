2acdd8e828e5cae4462acbbf71c3d4b65101f818
edwardhartnett <edwardjameshartnett@gmail.com>
2019-08-21 10:33:28 -0600

    more unit testing

--- unit_test/tst_nc4internal.c
+++ unit_test/tst_nc4internal.c
@@ -29,7 +29,11 @@ main(int argc, char **argv)
         NC *ncp;
         NCmodel model;
         char *path;
-        void *dispatchdata, *disspatchdata_in;
+        /* The NC3_dispatch_table is defined in nc3dispatch.c and
+         * externed in ncdispatch.h. But it will be 0 because we have
+         * not yet called NC3_initialize(). */
+        const void *dispatcher = NC3_dispatch_table;
+        void *dispatchdata_in;
         int mode = 0, mode_in;
 
         /* This won't work because there is no NC in the NC list which
@@ -37,9 +41,8 @@ main(int argc, char **argv)
         if (nc4_file_list_add(TEST_VAL_42, FILE_NAME, 0, NULL) != NC_EBADID) ERR;
 
         /* Create the NC* instance and insert its dispatcher and
-         * model. The NC3_dispatch_table is defined in
-         * ncdispatch.h. */
-        if(new_NC(NC3_dispatch_table, FILE_NAME, mode, &model, &ncp)) ERR;
+         * model. */
+        if (new_NC(dispatcher, FILE_NAME, mode, &model, &ncp)) ERR;
 
         /* Add to array of open files nc_filelist and define
          * ext_ncid by left-shifting the index 16 bits. */
@@ -55,7 +58,6 @@ main(int argc, char **argv)
         if (strcmp(path, FILE_NAME)) ERR;
         free(path);
         if (mode_in != mode) ERR;
-        if (dispatchdata_in != dispatchdata) ERR;
 
         /* This won't work. */
         if (nc4_file_list_del(TEST_VAL_42) != NC_EBADID) ERR;
@@ -74,6 +76,43 @@ main(int argc, char **argv)
     printf("Testing adding new file to nc4internal file lists with "
            "nc4_nc4f_list_add()...");
     {
+        NC *ncp;
+        NCmodel model;
+        char *path;
+        void *dispatchdata_in;
+        int mode = 0, mode_in;
+
+        /* Create the NC* instance and insert its dispatcher and
+         * model. */
+        if (new_NC(NC3_dispatch_table, FILE_NAME, mode, &model, &ncp)) ERR;
+
+        /* Add to array of open files nc_filelist and define
+         * ext_ncid by left-shifting the index 16 bits. */
+        add_to_NCList(ncp);
+
+        /* Create the NC_FILE_INFO_T instance associated empty lists
+         * to hold dims, types, groups, and the root group. */
+        if (nc4_nc4f_list_add(ncp, FILE_NAME, mode)) ERR;
+
+        /* Find the file in the list. */
+        if (!(path = malloc(NC_MAX_NAME + 1))) ERR;
+        if (nc4_file_list_get(ncp->ext_ncid, &path, &mode_in, &dispatchdata_in)) ERR;
+        if (strcmp(path, FILE_NAME)) ERR;
+        free(path);
+        if (mode_in != mode) ERR;
+
+        /* This won't work. */
+        if (nc4_file_list_del(TEST_VAL_42) != NC_EBADID) ERR;
+
+        /* Delete the NC_FILE_INFO_T and related storage. */
+        if (nc4_file_list_del(ncp->ext_ncid)) ERR;
+
+        /* Delete the ncp from the list. (In fact, just null out its
+         * entry in the array of file slots.) */
+        del_from_NCList(ncp); /* Will free empty list. */
+
+        /* Now free the NC struct. */
+        free_NC(ncp);
     }
     SUMMARIZE_ERR;
     /* printf("Testing changing ncid..."); */
