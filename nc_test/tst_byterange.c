6d778f0f71bd3792daeb3d09eb0d636dad4a84a1
Dennis Heimbigner <dmh@ucar.edu>
2020-01-08 15:18:31 -0700

    Make the dap4 code resistant to various server errors.
    Some versions of some servers are returning malformed responses.
    Make the library either handle them or gracefully fail.
    The three server errors "fixed" here are as follows.
    1. The attribute _NCProperties sometimes has a trailing nul character
       in its value. Soln is to elide the nul(s).
    2. Sometimes a DAP response has no data part, only a DMR.
       Soln is to detect and return an error code instead of crashing.
    3. Sometimes a server returns a redirection, but our current
       openmagic() function was not following the redirect. Soln
       is to follow redirects.
    Also because of #2, I am temporarily making --disable-dap-remote-tests
    be the default.

--- nc_test/tst_byterange.c
+++ nc_test/tst_byterange.c
@@ -33,7 +33,7 @@ struct TESTURLS {
     int format; /* instance of NC_FORMATX_XXX */
     const char* url;
 } testurls[] = {
-{NC_FORMAT_CLASSIC,"http://149.165.169.123:8080/thredds/fileServer/testdata/2004050300_eta_211.nc#bytes"},
+{NC_FORMAT_CLASSIC,"http://remotetest.unidata.ucar.edu/thredds/fileServer/testdata/2004050300_eta_211.nc#bytes"},
 #ifdef USE_NETCDF4
 {NC_FORMAT_NETCDF4,"http://noaa-goes16.s3.amazonaws.com/ABI-L1b-RadC/2017/059/03/OR_ABI-L1b-RadC-M3C13_G16_s20170590337505_e20170590340289_c20170590340316.nc#mode=bytes"},
 #endif
@@ -59,6 +59,7 @@ dotest(struct TESTURLS* test)
     int ncid;
     int format = -1;
 
+    fprintf(stderr,"Test: url=%s\n",test->url);
     /* First, try to open the url */
     if((ret = nc_open(test->url,0,&ncid))) return fail(ret);
 
