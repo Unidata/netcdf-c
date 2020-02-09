e957ebccfa0030f96d6f7feb2aac7ab93e76c3e0
Edward Hartnett <edward.hartnett@noaa.gov>
2020-02-09 13:22:35 -0700

    now test that bad version number in dispatch table causes rejection

--- nc_test4/tst_udf.c
+++ nc_test4/tst_udf.c
@@ -63,11 +63,103 @@ tst_get_vara(int ncid, int varid, const size_t *start, const size_t *count,
 }
 
 /* This is the dispatch object that holds pointers to all the
- * functions that make up the HDF4 dispatch interface. */
+ * functions that make up the test dispatch interface. */
 static NC_Dispatch tst_dispatcher = {
 
     NC_FORMATX_UDF0,
     NC_DISPATCH_VERSION,
+
+    NC_RO_create,
+    tst_open,
+
+    NC_RO_redef,
+    NC_RO__enddef,
+    NC_RO_sync,
+    tst_abort,
+    tst_close,
+    NC_RO_set_fill,
+    tst_inq_format,
+    tst_inq_format_extended,
+
+    NC4_inq,
+    NC4_inq_type,
+
+    NC_RO_def_dim,
+    NC4_inq_dimid,
+    NC4_inq_dim,
+    NC4_inq_unlimdim,
+    NC_RO_rename_dim,
+
+    NC4_inq_att,
+    NC4_inq_attid,
+    NC4_inq_attname,
+    NC_RO_rename_att,
+    NC_RO_del_att,
+    NC4_get_att,
+    NC_RO_put_att,
+
+    NC_RO_def_var,
+    NC4_inq_varid,
+    NC_RO_rename_var,
+    tst_get_vara,
+    NC_RO_put_vara,
+    NCDEFAULT_get_vars,
+    NCDEFAULT_put_vars,
+    NCDEFAULT_get_varm,
+    NCDEFAULT_put_varm,
+
+    NC4_inq_var_all,
+
+    NC_NOTNC4_var_par_access,
+    NC_RO_def_var_fill,
+
+    NC4_show_metadata,
+    NC4_inq_unlimdims,
+
+    NC4_inq_ncid,
+    NC4_inq_grps,
+    NC4_inq_grpname,
+    NC4_inq_grpname_full,
+    NC4_inq_grp_parent,
+    NC4_inq_grp_full_ncid,
+    NC4_inq_varids,
+    NC4_inq_dimids,
+    NC4_inq_typeids,
+    NC4_inq_type_equal,
+    NC_NOTNC4_def_grp,
+    NC_NOTNC4_rename_grp,
+    NC4_inq_user_type,
+    NC4_inq_typeid,
+
+    NC_NOTNC4_def_compound,
+    NC_NOTNC4_insert_compound,
+    NC_NOTNC4_insert_array_compound,
+    NC_NOTNC4_inq_compound_field,
+    NC_NOTNC4_inq_compound_fieldindex,
+    NC_NOTNC4_def_vlen,
+    NC_NOTNC4_put_vlen_element,
+    NC_NOTNC4_get_vlen_element,
+    NC_NOTNC4_def_enum,
+    NC_NOTNC4_insert_enum,
+    NC_NOTNC4_inq_enum_member,
+    NC_NOTNC4_inq_enum_ident,
+    NC_NOTNC4_def_opaque,
+    NC_NOTNC4_def_var_deflate,
+    NC_NOTNC4_def_var_fletcher32,
+    NC_NOTNC4_def_var_chunking,
+    NC_NOTNC4_def_var_endian,
+    NC_NOTNC4_def_var_filter,
+    NC_NOTNC4_set_var_chunk_cache,
+    NC_NOTNC4_get_var_chunk_cache
+};
+
+/* This is the dispatch object that holds pointers to all the
+ * functions that make up the test dispatch interface with a bad
+ * version number. */
+static NC_Dispatch tst_dispatcher_bad_version = {
+
+    NC_FORMATX_UDF0,
+    NC_DISPATCH_VERSION - 1,
 
     NC_RO_create,
     tst_open,
@@ -237,6 +329,18 @@ main(int argc, char **argv)
             if (nc_inq_format(ncid, NULL) != TEST_VAL_42) ERR;
             if (nc_inq_format_extended(ncid, NULL, NULL) != TEST_VAL_42) ERR;
             if (nc_abort(ncid) != TEST_VAL_42) ERR;
+        }
+    }
+    SUMMARIZE_ERR;
+    printf("*** testing bad version causes dispatch table to be rejected...");
+    {
+        int i;
+
+        /* Test all available user-defined format slots. */
+        for (i = 0; i < NUM_UDFS; i++)
+        {
+            /* Make sure our bad version format is rejected. */
+            if (nc_def_user_format(mode[i], &tst_dispatcher_bad_version, NULL) != NC_EINVAL) ERR;
         }
     }
     SUMMARIZE_ERR;
