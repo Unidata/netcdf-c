5d7fff9fde06f329f2e7ae0be98eca565fda1eb0
Dennis Heimbigner <dmh@ucar.edu>
2020-05-11 10:35:56 -0600

    uintptr for VS

--- include/ncconfigure.h
+++ include/ncconfigure.h
@@ -104,11 +104,13 @@ typedef unsigned short ushort;
 typedef unsigned int uint;
 #endif
 
+#ifndef _WIN32
 #ifndef HAVE_UINTPTR_T
 #if SIZEOF_VOIDP == 8
 typedef unsigned long uintptr_t;
 #else
 typedef unsigned int uintptr_t;
+#endif
 #endif
 #endif
 
