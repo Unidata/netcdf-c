d21c0170d79ec02da4b7a05bfe2749ee88bd3a98
Dennis Heimbigner <dmh@ucar.edu>
2020-06-29 09:14:47 -0600

    Semi-disable S3 tests. They rely on access to the unidata S3 bucket on stratus, so only Unidata NetCDF personnel can execute them, To enable them, one needs to do this at command level: export NETCDF_S3_TESTS=1

--- nczarr_test/run_it_test2.sh
+++ nczarr_test/run_it_test2.sh
@@ -60,6 +60,8 @@ if test "x$HAVE_SETTING" = x1 ; then HAVES3=1; fi
 main nz4
 main nzf
 
-if test "x$HAVENCZARR" = x1 -a "x$HAVES3" = x1 ; then
-main s3 'https://stratus.ucar.edu/unidata-netcdf-zarr-testing'
+if test "x$NETCDF_S3_TESTS" != x ; then
+    if test "x$HAVENCZARR" = x1 -a "x$HAVES3" = x1 ; then
+      main s3 'https://stratus.ucar.edu/unidata-netcdf-zarr-testing'
+    fi
 fi
