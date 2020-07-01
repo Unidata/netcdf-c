529c746c2e3c25b116e8334dfce67cada452bdd6
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-07-01 13:30:24 -0600

    now create metadata in a function with many parameters

--- nc_perf/tst_gfs_data_1.c
+++ nc_perf/tst_gfs_data_1.c
@@ -17,6 +17,7 @@
 
 #define FILE_NAME "tst_gfs_data_1.nc"
 #define NUM_META_VARS 7
+#define NDIM2 2
 #define NDIM4 4
 #define NDIM5 5
 #define NUM_PROC 4
@@ -32,8 +33,143 @@
 #define NUM_DATA_VARS 1
 
 int
-write_metadata(int ncid, int *dim_len, size_t phalf_loc_size, size_t phalf_start, float *value_phalf_loc)
+write_metadata(int ncid, int *data_varid, int s, int f, int *dim_len, size_t phalf_loc_size, size_t phalf_start, float *value_phalf_loc, size_t *data_start, size_t *data_count, float *value_pfull_loc,
+	       size_t grid_xt_start, size_t grid_xt_loc_size, double *value_grid_xt_loc, size_t grid_yt_start, size_t grid_yt_loc_size, double *value_grid_yt_loc,
+	       size_t *lat_start, size_t *lat_count, double *value_lat_loc, size_t *lon_start, size_t *lon_count, double *value_lon_loc, int my_rank)
 {
+    char dim_name[NDIM5][NC_MAX_NAME + 1] = {"grid_xt", "grid_yt", "pfull",
+					     "phalf", "time"};
+    int dimid[NDIM5];
+    int dimid_data[NDIM4];
+    char var_name[NUM_META_VARS][NC_MAX_NAME + 1] = {"grid_xt", "lon", "grid_yt",
+						     "lat", "pfull", "phalf", "time"};
+    int var_type[NUM_META_VARS] = {NC_DOUBLE, NC_DOUBLE, NC_DOUBLE, NC_DOUBLE,
+				   NC_FLOAT, NC_FLOAT, NC_DOUBLE};
+    int varid[NUM_META_VARS];
+    double value_time = 2.0;
+    int dv;
+    int res;
+    		    
+    /* Turn off fill mode. */
+    if (nc_set_fill(ncid, NC_NOFILL, NULL)) ERR;
+
+    /* Define dimension grid_xt. */
+    if (nc_def_dim(ncid, dim_name[0], dim_len[0], &dimid[0])) ERR;
+
+    /* Define dimension grid_yt. */
+    if (nc_def_dim(ncid, dim_name[1], dim_len[1], &dimid[1])) ERR;
+
+    /* Define variable grid_xt. */
+    if (nc_def_var(ncid, var_name[0], var_type[0], 1, &dimid[0], &varid[0])) ERR;
+    if (nc_var_par_access(ncid, varid[0], NC_INDEPENDENT)) ERR;
+
+    /* Define variable lon. */
+    if (nc_def_var(ncid, var_name[1], var_type[1], 2, dimid, &varid[1])) ERR;
+    if (nc_var_par_access(ncid, varid[1], NC_INDEPENDENT));
+
+    /* Define variable grid_yt. */
+    if (nc_def_var(ncid, var_name[2], var_type[2], 1, &dimid[1], &varid[2])) ERR;
+    if (nc_var_par_access(ncid, varid[2], NC_INDEPENDENT)) ERR;
+
+    /* Define variable lat. */
+    if (nc_def_var(ncid, var_name[3], var_type[3], 2, dimid, &varid[3])) ERR;
+    if (nc_var_par_access(ncid, varid[3], NC_INDEPENDENT)) ERR;
+
+    /* Define dimension pfull. */
+    if (nc_def_dim(ncid, dim_name[2], dim_len[2], &dimid[2])) ERR;
+
+    /* Define variable pfull and write data. */
+    if (nc_def_var(ncid, var_name[4], var_type[4], 1, &dimid[2], &varid[4])) ERR;
+    if (nc_var_par_access(ncid, varid[4], NC_INDEPENDENT)) ERR;
+    if (nc_enddef(ncid)) ERR;
+    if (nc_put_vara_float(ncid, varid[4], &data_start[1], &data_count[1], value_pfull_loc)) ERR;
+    if (nc_redef(ncid)) ERR;
+
+    /* Define dimension phalf. This dim is only used by the phalf coord var. */
+    if (nc_def_dim(ncid, dim_name[3], dim_len[3], &dimid[3])) ERR;
+
+    /* Define coord variable phalf and write data. */
+    if (nc_def_var(ncid, var_name[5], var_type[5], 1, &dimid[3], &varid[5])) ERR;
+    if (nc_var_par_access(ncid, varid[5], NC_INDEPENDENT)) ERR;
+    if (nc_enddef(ncid)) ERR;
+    if (nc_put_vara_float(ncid, varid[5], &phalf_start, &phalf_loc_size, value_phalf_loc)) ERR;
+    if (nc_redef(ncid)) ERR;
+
+    /* Define dimension time. */
+    if (nc_def_dim(ncid, dim_name[4], dim_len[4], &dimid[4])) ERR;
+
+    /* Define variable time and write data. */
+    if (nc_def_var(ncid, var_name[6], var_type[6], 1, &dimid[4], &varid[6])) ERR;
+    if (nc_var_par_access(ncid, varid[6], NC_INDEPENDENT)) ERR;
+    if (nc_enddef(ncid)) ERR;
+
+    /* In NOAA code, do all processors write the single time value? */
+    if (my_rank == 0)
+	if (nc_put_var_double(ncid, varid[6], &value_time)) ERR;;
+    if (nc_redef(ncid)) ERR;
+
+    /* Write variable grid_xt data. */
+    if (nc_enddef(ncid)) ERR;
+    if (nc_put_vara_double(ncid, varid[0], &grid_xt_start, &grid_xt_loc_size, value_grid_xt_loc)) ERR;
+    if (nc_redef(ncid)) ERR;
+
+    /* Write lon data. */
+    if (nc_enddef(ncid)) ERR;
+    /* start[0] = lon_xt_start; */
+    /* start[1] = lon_yt_start; */
+    /* count[0] = lon_xt_loc_size; */
+    /* count[1] = lon_yt_loc_size; */
+    if (nc_put_vara_double(ncid, varid[1], lon_start, lon_count, value_lon_loc)) ERR;
+    if (nc_redef(ncid)) ERR;
+
+    /* Write grid_yt data. */
+    if (nc_enddef(ncid)) ERR;
+    if (nc_put_vara_double(ncid, varid[2], &grid_yt_start, &grid_yt_loc_size, value_grid_yt_loc)) ERR;
+    if (nc_redef(ncid)) ERR;
+
+    /* Write lat data. */
+    if (nc_enddef(ncid)) ERR;
+    /* start[0] = lat_xt_start; */
+    /* start[1] = lat_yt_start; */
+    /* count[0] = lat_xt_loc_size; */
+    /* count[1] = lat_yt_loc_size; */
+    if (nc_put_vara_double(ncid, varid[3], lat_start, lat_count, value_lat_loc)) ERR;
+    if (nc_redef(ncid)) ERR;
+
+    /* Specify dimensions for our data vars. */
+    dimid_data[0] = dimid[4];
+    dimid_data[1] = dimid[2];
+    dimid_data[2] = dimid[1];
+    dimid_data[3] = dimid[0];
+
+    /* Define data variables. */
+    for (dv = 0; dv < NUM_DATA_VARS; dv++)
+    {
+	char data_var_name[NC_MAX_NAME + 1];
+
+	sprintf(data_var_name, "var_%d", dv);
+	if (nc_def_var(ncid, data_var_name, NC_FLOAT, NDIM4, dimid_data, &data_varid[dv])) ERR;
+
+	/* Setting any filter only will work for HDF5-1.10.3 and later */
+	/* versions. */
+	if (!f)
+	    res = nc_def_var_deflate(ncid, data_varid[dv], s, 1, 4);
+	else
+	{
+	    res = nc_def_var_deflate(ncid, data_varid[dv], s, 0, 0);
+	    if (!res)
+		res = nc_def_var_szip(ncid, data_varid[dv], 32, 32);
+	}
+#ifdef HDF5_SUPPORTS_PAR_FILTERS
+	if (res) ERR;
+#else
+	if (res != NC_EINVAL) ERR;
+#endif
+		    
+	if (nc_var_par_access(ncid, data_varid[dv], NC_COLLECTIVE)) ERR;
+	if (nc_enddef(ncid)) ERR;
+    }
+
     return 0;
 }
 
@@ -81,21 +217,14 @@ main(int argc, char **argv)
     double data_start_time, data_stop_time;
     
     int ncid;
-    size_t start[NDIM4], count[NDIM4];
+    size_t lat_start[NDIM2], lat_count[NDIM2];
+    size_t lon_start[NDIM2], lon_count[NDIM2];
     size_t data_start[NDIM4], data_count[NDIM4];
 
     int dim_len[NDIM5] = {3072, 1536, 127, 128, 1};
-    int dimid[NDIM5];
-    int dimid_data[NDIM4];
 
     /* Variables. */
-    char var_name[NUM_META_VARS][NC_MAX_NAME + 1] = {"grid_xt", "lon", "grid_yt",
-						     "lat", "pfull", "phalf", "time"};
-    int varid[NUM_META_VARS];
     int data_varid[NUM_DATA_VARS];
-    int var_type[NUM_META_VARS] = {NC_DOUBLE, NC_DOUBLE, NC_DOUBLE, NC_DOUBLE,
-			      NC_FLOAT, NC_FLOAT, NC_DOUBLE};
-    double value_time = 2.0;
     float *value_pfull_loc;
     size_t phalf_loc_size, phalf_start;
     float *value_phalf_loc;
@@ -103,15 +232,12 @@ main(int argc, char **argv)
     double *value_grid_xt_loc;
     size_t grid_yt_loc_size, grid_yt_start;
     double *value_grid_yt_loc;
-    size_t lon_xt_loc_size, lon_xt_start, lon_yt_loc_size, lon_yt_start;
     double *value_lon_loc;
-    size_t lat_xt_loc_size, lat_xt_start, lat_yt_loc_size, lat_yt_start;
     double *value_lat_loc;
     float *value_data;
 
     int f;
     int i, j, k, dv;
-    int res;
 
     /* Initialize MPI. */
     MPI_Init(&argc, &argv);
@@ -141,29 +267,29 @@ main(int argc, char **argv)
 
     /* Size of local arrays (i.e. for this pe) lon and lat data. This is */
     /* specific to 4 pes. */
-    lon_xt_loc_size = 1536;
-    lat_xt_loc_size = 1536;
+    lon_count[0] = 1536;
+    lat_count[0] = 1536;
     if (my_rank == 0 || my_rank == 2)
     {
-	lon_xt_start = 0;
-	lat_xt_start = 0;
+	lon_start[0] = 0;
+	lat_start[0] = 0;
     }
     else
     {
-	lon_xt_start = 1536;
-	lat_xt_start = 1536;
+	lon_start[0] = 1536;
+	lat_start[0] = 1536;
     }
-    lon_yt_loc_size = 768;
-    lat_yt_loc_size = 768;
+    lon_count[1] = 768;
+    lat_count[1] = 768;
     if (my_rank == 0 || my_rank == 1)
     {
-	lon_yt_start = 0;
-	lat_yt_start = 0;
+	lon_start[1] = 0;
+	lat_start[1] = 0;
     }
     else
     {
-	lon_yt_start = 768;
-	lat_yt_start = 768;
+	lon_start[1] = 768;
+	lat_start[1] = 768;
     }
 
     /* Allocate space on this pe to hold the coordinate var data for this pe. */
@@ -171,8 +297,8 @@ main(int argc, char **argv)
     if (!(value_phalf_loc = malloc(phalf_loc_size * sizeof(float)))) ERR;
     if (!(value_grid_xt_loc = malloc(grid_xt_loc_size * sizeof(double)))) ERR;
     if (!(value_grid_yt_loc = malloc(grid_yt_loc_size * sizeof(double)))) ERR;
-    if (!(value_lon_loc = malloc(lon_xt_loc_size * lon_yt_loc_size * sizeof(double)))) ERR;
-    if (!(value_lat_loc = malloc(lat_xt_loc_size * lat_yt_loc_size * sizeof(double)))) ERR;
+    if (!(value_lon_loc = malloc(lon_count[0] * lon_count[1] * sizeof(double)))) ERR;
+    if (!(value_lat_loc = malloc(lat_count[0] * lat_count[1] * sizeof(double)))) ERR;
 
     /* Allocate space to hold the data. */
     if (!(value_data = malloc(data_count[3] * data_count[2] * data_count[1] * sizeof(float)))) ERR;
@@ -186,14 +312,14 @@ main(int argc, char **argv)
 	value_grid_xt_loc[i] = my_rank * 100 + i;
     for (i = 0; i < grid_yt_loc_size; i++)
 	value_grid_yt_loc[i] = my_rank * 100 + i;
-    for (j = 0; j < lon_yt_loc_size; j++)
+    for (j = 0; j < lon_count[1]; j++)
     {
-	for(i = 0; i < lon_xt_loc_size; i++)
+	for(i = 0; i < lon_count[0]; i++)
 	{
-	    value_lon_loc[j * lon_xt_loc_size + i] = my_rank * 100 + i + j;
-	    value_lat_loc[j * lon_xt_loc_size + i] = my_rank * 100 + i + j;
+	    value_lon_loc[j * lon_count[0] + i] = my_rank * 100 + i + j;
+	    value_lat_loc[j * lon_count[0] + i] = my_rank * 100 + i + j;
 	    for (k = 0; k < data_count[1]; k++)
-		value_data[j * lon_xt_loc_size + i] = my_rank * 100 + i + j + k;
+		value_data[j * lon_count[0] + i] = my_rank * 100 + i + j + k;
 	}
     }
 
@@ -213,134 +339,10 @@ main(int argc, char **argv)
 		meta_start_time = MPI_Wtime();		
                 if (nc_create_par(FILE_NAME, NC_NETCDF4, comm, info, &ncid)) ERR;
 
-		if (write_metadata(ncid, dim_len, phalf_loc_size, phalf_start, value_phalf_loc)) ERR;
+		if (write_metadata(ncid, data_varid, s, f, dim_len, phalf_loc_size, phalf_start, value_phalf_loc, data_start, data_count, value_pfull_loc,
+				   grid_xt_start, grid_xt_loc_size, value_grid_xt_loc, grid_yt_start, grid_yt_loc_size, value_grid_yt_loc,
+				   lat_start, lat_count, value_lat_loc, lon_start, lon_count, value_lon_loc, my_rank)) ERR;
 		
-		{
-
-		    /* Dimensions. */
-		    char dim_name[NDIM5][NC_MAX_NAME + 1] = {"grid_xt", "grid_yt", "pfull",
-							     "phalf", "time"};
-		    
-		    /* Turn off fill mode. */
-		    if (nc_set_fill(ncid, NC_NOFILL, NULL)) ERR;
-
-		    /* Define dimension grid_xt. */
-		    if (nc_def_dim(ncid, dim_name[0], dim_len[0], &dimid[0])) ERR;
-
-		    /* Define dimension grid_yt. */
-		    if (nc_def_dim(ncid, dim_name[1], dim_len[1], &dimid[1])) ERR;
-
-		    /* Define variable grid_xt. */
-		    if (nc_def_var(ncid, var_name[0], var_type[0], 1, &dimid[0], &varid[0])) ERR;
-		    if (nc_var_par_access(ncid, varid[0], NC_INDEPENDENT)) ERR;
-
-		    /* Define variable lon. */
-		    if (nc_def_var(ncid, var_name[1], var_type[1], 2, dimid, &varid[1])) ERR;
-		    if (nc_var_par_access(ncid, varid[1], NC_INDEPENDENT));
-
-		    /* Define variable grid_yt. */
-		    if (nc_def_var(ncid, var_name[2], var_type[2], 1, &dimid[1], &varid[2])) ERR;
-		    if (nc_var_par_access(ncid, varid[2], NC_INDEPENDENT)) ERR;
-
-		    /* Define variable lat. */
-		    if (nc_def_var(ncid, var_name[3], var_type[3], 2, dimid, &varid[3])) ERR;
-		    if (nc_var_par_access(ncid, varid[3], NC_INDEPENDENT)) ERR;
-
-		    /* Define dimension pfull. */
-		    if (nc_def_dim(ncid, dim_name[2], dim_len[2], &dimid[2])) ERR;
-
-		    /* Define variable pfull and write data. */
-		    if (nc_def_var(ncid, var_name[4], var_type[4], 1, &dimid[2], &varid[4])) ERR;
-		    if (nc_var_par_access(ncid, varid[4], NC_INDEPENDENT)) ERR;
-		    if (nc_enddef(ncid)) ERR;
-		    if (nc_put_vara_float(ncid, varid[4], &data_start[1], &data_count[1], value_pfull_loc)) ERR;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Define dimension phalf. This dim is only used by the phalf coord var. */
-		    if (nc_def_dim(ncid, dim_name[3], dim_len[3], &dimid[3])) ERR;
-
-		    /* Define coord variable phalf and write data. */
-		    if (nc_def_var(ncid, var_name[5], var_type[5], 1, &dimid[3], &varid[5])) ERR;
-		    if (nc_var_par_access(ncid, varid[5], NC_INDEPENDENT)) ERR;
-		    if (nc_enddef(ncid)) ERR;
-		    if (nc_put_vara_float(ncid, varid[5], &phalf_start, &phalf_loc_size, value_phalf_loc)) ERR;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Define dimension time. */
-		    if (nc_def_dim(ncid, dim_name[4], dim_len[4], &dimid[4])) ERR;
-
-		    /* Define variable time and write data. */
-		    if (nc_def_var(ncid, var_name[6], var_type[6], 1, &dimid[4], &varid[6])) ERR;
-		    if (nc_var_par_access(ncid, varid[6], NC_INDEPENDENT)) ERR;
-		    if (nc_enddef(ncid)) ERR;
-
-		    /* In NOAA code, do all processors write the single time value? */
-		    if (my_rank == 0)
-			if (nc_put_var_double(ncid, varid[6], &value_time)) ERR;;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Write variable grid_xt data. */
-		    if (nc_enddef(ncid)) ERR;
-		    if (nc_put_vara_double(ncid, varid[0], &grid_xt_start, &grid_xt_loc_size, value_grid_xt_loc)) ERR;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Write lon data. */
-		    if (nc_enddef(ncid)) ERR;
-		    start[0] = lon_xt_start;
-		    start[1] = lon_yt_start;
-		    count[0] = lon_xt_loc_size;
-		    count[1] = lon_yt_loc_size;
-		    if (nc_put_vara_double(ncid, varid[1], start, count, value_lon_loc)) ERR;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Write grid_yt data. */
-		    if (nc_enddef(ncid)) ERR;
-		    if (nc_put_vara_double(ncid, varid[2], &grid_yt_start, &grid_yt_loc_size, value_grid_yt_loc)) ERR;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Write lat data. */
-		    if (nc_enddef(ncid)) ERR;
-		    start[0] = lat_xt_start;
-		    start[1] = lat_yt_start;
-		    count[0] = lat_xt_loc_size;
-		    count[1] = lat_yt_loc_size;
-		    if (nc_put_vara_double(ncid, varid[3], start, count, value_lat_loc)) ERR;
-		    if (nc_redef(ncid)) ERR;
-
-		    /* Specify dimensions for our data vars. */
-		    dimid_data[0] = dimid[4];
-		    dimid_data[1] = dimid[2];
-		    dimid_data[2] = dimid[1];
-		    dimid_data[3] = dimid[0];
-
-		    /* Define data variables. */
-		    for (dv = 0; dv < NUM_DATA_VARS; dv++)
-		    {
-			char data_var_name[NC_MAX_NAME + 1];
-
-			sprintf(data_var_name, "var_%d", dv);
-			if (nc_def_var(ncid, data_var_name, NC_FLOAT, NDIM4, dimid_data, &data_varid[dv])) ERR;
-
-			/* Setting any filter only will work for HDF5-1.10.3 and later */
-			/* versions. */
-			if (!f)
-			    res = nc_def_var_deflate(ncid, data_varid[dv], s, 1, 4);
-			else
-			{
-			    res = nc_def_var_deflate(ncid, data_varid[dv], s, 0, 0);
-			    if (!res)
-				res = nc_def_var_szip(ncid, data_varid[dv], 32, 32);
-			}
-#ifdef HDF5_SUPPORTS_PAR_FILTERS
-			if (res) ERR;
-#else
-			if (res != NC_EINVAL) ERR;
-#endif
-		    
-			if (nc_var_par_access(ncid, data_varid[dv], NC_COLLECTIVE)) ERR;
-			if (nc_enddef(ncid)) ERR;
-		    }
-		}
 		
 		MPI_Barrier(MPI_COMM_WORLD);
 		meta_stop_time = MPI_Wtime();
