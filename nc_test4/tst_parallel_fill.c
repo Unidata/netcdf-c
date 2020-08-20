4ecc1fbe8d695383a47e99afc4a90dd87c0849cc
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-08-20 12:49:51 -0600

    test development

--- nc_test4/tst_parallel_fill.c
+++ nc_test4/tst_parallel_fill.c
@@ -12,8 +12,10 @@
 #include <mpi.h>
 
 #define FILE_NAME "tst_parallel_fill.nc"
-#define DIM_NAME "d1"
+#define DIM_NAME "Elephants"
 #define DIM_LEN 4
+#define VAR_NAME "Hannible"
+#define NDIM1 1
 
 int
 main(int argc, char **argv)
@@ -24,7 +26,7 @@ main(int argc, char **argv)
     MPI_Info info = MPI_INFO_NULL;
 
     /* Netcdf-4 stuff. */
-    int ncid, dimid;
+    int ncid, dimid, varid;
 
     /* Initialize MPI. */
     MPI_Init(&argc, &argv);
@@ -34,6 +36,7 @@ main(int argc, char **argv)
     /* Create a test data file. */
     if (nc_create_par(FILE_NAME, NC_NETCDF4, comm, info, &ncid)) ERR;
     if (nc_def_dim(ncid, DIM_NAME, DIM_LEN, &dimid)) ERR;
+    if (nc_def_var(ncid, VAR_NAME, NC_INT, NDIM1, &dimid, &varid)) ERR;
     if (nc_close(ncid)) ERR;
 
     /* Open the file and check it. */
@@ -42,7 +45,7 @@ main(int argc, char **argv)
 	
 	if (nc_open_par(FILE_NAME, NC_NOWRITE, comm, info, &ncid)) ERR;
 	if (nc_inq(ncid, &ndims, &nvars, &natts, &unlimdimid)) ERR;
-	if (nvars != 0 || ndims != 1 || natts != 0 && unlimdimid != -1) ERR;
+	if (nvars != 0 || ndims != 1 || natts != 0 || unlimdimid != -1) ERR;
 	if (nc_close(ncid)) ERR;
     }
 
