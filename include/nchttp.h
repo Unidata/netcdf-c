7a44ae91847826cdf357e1176e4292744b9593a5
Dennis Heimbigner <dmh@ucar.edu>
2021-01-31 21:40:08 -0700

    Unify definition of NC_DISPATCH_VERSION
    re: Issue
    
    The netcdf dispatch table version was defined in several places.
    Modify to only require defining it in CMakeLists.txt and configure.ac.
    
    Fix entailed the following changes:
    * Up the NC_DISPATCH_VERSION from 2 to 3 in configure.ac and CMakeLists.txt
    * Create include/netcdf_dispatch.h.in and use it to configure include/netcdf_dispatch.h
    * For CMAKE, make it search CMAKE_CURRENT_BINARY_DIR so code can locate the configured netcdf_dispatch.h
    * Add entry to config.h.cmake.in for NC_DISPATCH_VERSION
    * Move NCerror from include/ncdispatch.h to libdap2/nccomon.h
    * Fix an API problem re nchttp.h
    * Fix a conversion warning in libdispatch/dinfermodel.c

--- include/nchttp.h
+++ include/nchttp.h
@@ -22,7 +22,7 @@ typedef struct NC_HTTP_STATE {
     NCbytes* buf;
 } NC_HTTP_STATE;
 
-extern int nc_http_open(const char* objecturl, NC_HTTP_STATE** state, size64_t* lenp);
+extern int nc_http_open(const char* objecturl, NC_HTTP_STATE** state, long long* lenp);
 extern int nc_http_size(NC_HTTP_STATE* state, const char* url, size64_t* sizep);
 extern int nc_http_read(NC_HTTP_STATE* state, const char* url, size64_t start, size64_t count, NCbytes* buf);
 extern int nc_http_close(NC_HTTP_STATE* state);
