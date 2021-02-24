2afbdbd18fbf9fd8550215e7eff1d2b311b6626d
Dennis Heimbigner <dmh@ucar.edu>
2021-02-24 13:46:11 -0700

    Add support for the XArray Zarr _ARRAY_DIMENSIONS attribute
    The XArray implementation that uses Zarr for storage
    provides a mechanism to simulate named dimensions.
    It does this by adding a per-variable attribute called
    _ARRAY_DIMENSIONS. This attribute contains a list of names
    to be matched against the shape values of the variable.
    In effect a named dimension is created with the name
    _ARRAY_DIMENSIONS(i) and length shape(i) for all i
    in range 0..rank(variable).
    Both read and write support is provided.
    
    This XArray support is only invoked if the mode value
    of "xarray" is defined. So for example, as in this URL.
    ````
    https://s3.us-west-1.amazonaws.com/bucket/dataset#mode=nczarr,xarray,s3
    ````
    Note that the "xarray" mode flag also implies mode flag "zarr", so the above
    is equivalent to this URL.
    ````
    https://s3.us-west-1.amazonaws.com/bucket/dataset#mode=nczarr,zarr,xarray,s3
    ````
    
    The primary change to implement this was to unify the handling
    of dimension references in libnczarr/zsync.
    
    A test for this and other pure-zarr features was added as
    nczarr_test/run_purezarr.sh
    
    Other changes:
    * Make sure distcheck leaves no files around.
    * Change the special attribute flag DIMSCALEFLAG to HIDDENATTRFLAG
      to support the xarray attribute.
    * Annotate the zmap implementations with feature flags such as
      WRITEONCE (for zip files).

--- libnczarr/zmap.c
+++ libnczarr/zmap.c
@@ -23,16 +23,16 @@ extern NCZMAP_DS_API zmap_s3sdk;
 
 /**************************************************/
 
-NCZM_PROPERTIES
-nczmap_properties(NCZM_IMPL impl)
+NCZM_FEATURES
+nczmap_features(NCZM_IMPL impl)
 {
     switch (impl) {
-    case NCZM_FILE: return zmap_file.properties;
+    case NCZM_FILE: return zmap_file.features;
 #ifdef ENABLE_NCZARR_ZIP
-    case NCZM_ZIP: return zmap_zip.properties;
+    case NCZM_ZIP: return zmap_zip.features;
 #endif
 #ifdef ENABLE_S3_SDK
-    case NCZM_S3: return zmap_s3sdk.properties;
+    case NCZM_S3: return zmap_s3sdk.features;
 #endif
     default: break;
     }
@@ -437,6 +437,28 @@ done:
 }
 
 /*
+Extract the last segment from path.
+*/
+
+int
+nczm_lastsegment(const char* path, char** lastp)
+{
+    int ret = NC_NOERR;
+    const char* last = NULL;
+
+    if(path == NULL)
+	{if(lastp) *lastp = NULL; goto done;}
+
+    last = strrchr(path,'/');
+    if(last == NULL) last = path; else last++;
+
+    if(lastp) *lastp = strdup(last);
+
+done:
+    return THROW(ret);    
+}
+
+/*
 Extract the basename from a path.
 Basename is last segment minus one extension.
 */
@@ -444,30 +466,27 @@ Basename is last segment minus one extension.
 int
 nczm_basename(const char* path, char** basep)
 {
-    int ret = NC_NOERR;
+    int stat = NC_NOERR;
     char* base = NULL;
+    char* last = NULL;
     const char* p = NULL;
-    const char* q = NULL;
     ptrdiff_t delta;
 
-    if(path == NULL) 
-	{base = NULL; goto done;}
+    if((stat=nczm_lastsegment(path,&last))) goto done;
 
-    p = strrchr(path,'/');
-    if(p == NULL) p = path; else p++;
-    q = strrchr(p,'.');
-    if(q == NULL) q = p + strlen(p);
-    
-    delta = (q-p);
+    if(last == NULL) goto done;
+    p = strrchr(last,'.');
+    if(p == NULL) p = last+strlen(last);
+    delta = (p - last);
     if((base = (char*)malloc(delta+1))==NULL)
-        {ret = NC_ENOMEM; goto done;}
-    memcpy(base,p,delta);
+        {stat = NC_ENOMEM; goto done;}
+    memcpy(base,last,delta);
     base[delta] = '\0';
-
     if(basep) {*basep = base; base = NULL;}
 done:
+    nullfree(last);
     nullfree(base);
-    return THROW(ret);    
+    return THROW(stat);    
 }
 
 /* bubble sort a list of strings */
