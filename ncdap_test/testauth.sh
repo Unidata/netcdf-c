f720c5edbe2545335f55c95ba82831c040329048
Dennis Heimbigner <dmh@ucar.edu>
2021-04-14 15:14:14 -0600

    try18

--- ncdap_test/testauth.sh
+++ ncdap_test/testauth.sh
@@ -6,16 +6,27 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 #SHOW=1
 #DBG=1
 
-if test -d "/home/dmh" ; then
+if test "x$NCAUTH_HOMETEST" != x ; then
     RCHOME=1
 fi
+RCHOME=1
 
 WD=`pwd`
 
 COOKIES="${WD}/.cookies_test"
 
 RC=.daprc
+
+if test "x$FPISMSVC" = x ; then
 NETRC=.netrc_test
+NETRCIMP=.netrc
+else
+NETRC=_netrc_test
+NETRCIMP=_netrc
+fi
+
+LOCALRCFILES="$WD/.dodsrc $WD/.daprc $WD/.ncrc $WD/$NETRC $WD/$NETRCIMP"
+HOMERCFILES="$HOME/.dodsrc $HOME/.daprc $HOME/.ncrc $HOME/$NETRC $HOME/$NETRCIMP"
 
 NETRCFILE=$WD/$NETRC
 DAPRCFILE=$WD/$RC
@@ -57,35 +68,38 @@ HOMENETRC=`echo "$HOMENETRC" | sed -e "s|//|/|g"`
 
 createrc() {
     local RCP
-    local NETRC
+    local NETRCFILE
   RCP="$1"
   if test "x$RCP" = x ; then
     echo "createrc: no rc specified"
     exit 1
   fi
   shift
-  NETRC="$1"
+  NETRCPATH="$1"
   echo "Creating rc file $RCP"
   if test "x${DBG}" = x1 ; then
     echo "HTTP.VERBOSE=1" >>$RCP
   fi	
-  if test "x$NETRC" = x ; then
+  if test "x$NETRCPATH" = x ; then
     echo "HTTP.CREDENTIALS.USERPASSWORD=${BASICCOMBO}" >>$RCP
-  elif test "x$NETRC" != ximplicit ; then
-    echo "HTTP.NETRC=${NETRC}" >>$RCP
+  elif test "x$NETRCPATH" != ximplicit ; then
+    echo "HTTP.NETRC=${NETRCPATH}" >>$RCP
+  elif test "x$NETRCPATH" = ximplicit ; then
+    echo "HTTP.NETRC=" >>$RCP
   fi
   echo "HTTP.COOKIEJAR=${COOKIES}" >>$RCP
 }
 
 createnetrc() {
-    local NETRC
-  NETRC="$1"; # netrc file path
-  if test "x$NETRC" = x ; then return; fi
-  echo "Creating netrc file $NETRC"
-  echo -n "${PROTO}://$URLSERVER/$URLPATH" >>$NETRC
-  echo  -n "  login $BASICUSER password $BASICPWD" >>$NETRC
-  echo "" >>$NETRC
-  chmod go-rwx $NETRC
+    local NETRCPATH
+  NETRCPATH="$1"; # netrc file path
+  if test "x$NETRCPATH" = x ; then return; fi
+  echo "Creating netrc file $NETRCPATH"
+#  echo -n "${PROTO}://$URLSERVER/$URLPATH" >>$NETRCPATH
+  echo -n "machine $URLSERVER" >>$NETRCPATH
+  echo  -n "  login $BASICUSER password $BASICPWD" >>$NETRCPATH
+  echo "" >>$NETRCPATH
+  chmod go-rwx $NETRCPATH
 }
 
 # Test cases
@@ -100,72 +114,96 @@ rcembed() {
 }
 
 # Case: local daprc no netrc no embed
-rclocal() {
+rclocal1() {
   echo "***Testing rc file in local directory"
   reset
-  # Create the rc file and (optional) netrc fil in ./
-  createrc $LOCALRC $LOCALNETRC
-
+  # Create the rc file in ./
+  createrc $LOCALRC
   # Invoke ncdump to extract a file using the URL
   ${NCDUMP} -h "$URL" > testauthoutput
 }
 
 # Case: local daprc local netrc no embed
-rclocalnetrc() {
+rclocal2() {
   echo "***Testing rc file in local directory"
   reset
   # Create the rc file and (optional) netrc fil in ./
   createnetrc $LOCALNETRC
   createrc $LOCALRC $LOCALNETRC
-
   # Invoke ncdump to extract a file using the URL
   ${NCDUMP} -h "$URL" > testauthoutput
 }
 
-rchome() {
-  echo "***Testing rc file and netrc in home directory"
+# Case: home rc no netrc no embed
+rchome1() {
+  echo "***Testing home rc file no netrc in home directory"
   reset
-  # Create the rc file and (optional) netrc file in ./
-  createnetrc $HOMENETRC
-  createrc $HOMERC $HOMENETRC
-
+  # Create the rc file file in ./
+  createrc $HOMERC
   # Invoke ncdump to extract a file the URL
   ${NCDUMP} -h "$URL" > testauthoutput
 }
 
+# Case: home daprc implicit home netrc no embed
 rchome2() {
+  echo "***Testing .netrc file in home directory"
+  reset
+  createnetrc $HOME/$NETRCIMP
+  createrc $HOMERC implicit
+  # Invoke ncdump to extract a file using the URL
+  ${NCDUMP} -h "$URL" > testauthoutput
+}
+
+# Case: local rc explicit netrc no embed
+rchome3() {
+  echo "***Testing local rc file and .netrc explicit in home directory"
+  reset
+  # Create the rc file and (optional) netrc file in ./
+  createnetrc $HOME/$NETRC
+  createrc $LOCALRC $HOME/$NETRC
+  # Invoke ncdump to extract a file the URL
+  ${NCDUMP} -h "$URL" > testauthoutput
+}
+
+# Case: local rc implicit netrc no embed
+rchome4() {
   echo "***Testing local rc file and .netrc implicit in home directory"
   reset
   # Create the rc file and (optional) netrc file in ./
-  createnetrc $HOME/.netrc
-  createrc $HOMERC implicit
-
+  createnetrc $HOME/$NETRCIMP
+  createrc $LOCALRC implicit
   # Invoke ncdump to extract a file the URL
   ${NCDUMP} -h "$URL" > testauthoutput
 }
 
 reset() {
-if test "x$RCHOME" = x1 ; then rm -f $HOMENETRCFILE $HOMEDAPRCFILE; fi
-rm -f $NETRCFILE $DAPRCFILE $LOCALRC $LOCALNETRC
-unset DAPRCFILE
-rm -f testauthoutput
+  if test "x$RCHOME" = x1 ; then
+      rm -f $HOMERCFILES
+  fi
+  rm -f $LOCALRCFILES
+  unset DAPRCFILE
+  rm -f ./testauthoutput
 }
 
-rcembed
+set -x
+
+#rcembed
 
 # Next set tests assume these defaults
 URL="${PROTO}://${URLSERVER}/$URLPATH"
 
-rclocal
-rclocalnetrc
+#rclocal1
+#rclocal2
 
 # Do not do this unless you know what you are doing
 if test "x$RCHOME" = x1 ; then
-    rchome
-    rchome2
+#    rchome1
+#    rchome2
+#    rchome3
+    rchome4
 fi
 
-reset
+#reset
 
 exit
 
