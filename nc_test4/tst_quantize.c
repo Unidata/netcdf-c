bf71ee25e24d3fdfc4638265e0970468d9db4f75
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-08-26 07:21:07 -0600

    more quantize testing

--- nc_test4/tst_quantize.c
+++ nc_test4/tst_quantize.c
@@ -17,11 +17,12 @@
 #define FILE_NAME "tst_quantize.nc"
 #define NDIMS1 1
 #define DIM_NAME_1 "meters_along_canal"
-#define DIM_LEN_1 3
+#define DIM_LEN_3 3
+#define DIM_LEN_1 1
 #define VAR_NAME_1 "Amsterdam_houseboat_location"
 #define VAR_NAME_2 "Amsterdam_street_noise_decibels"
-#define NSD_1 3
-#define NSD_2 9
+#define NSD_3 3
+#define NSD_9 9
 
 int
 main(int argc, char **argv)
@@ -35,24 +36,24 @@ main(int argc, char **argv)
 	/* Create a netcdf classic file with one var. Attempt
 	 * quantization. It will not work. */
 	if (nc_create(FILE_NAME, NC_CLOBBER, &ncid)) ERR;
-	if (nc_def_dim(ncid, DIM_NAME_1, DIM_LEN_1, &dimid)) ERR;
+	if (nc_def_dim(ncid, DIM_NAME_1, DIM_LEN_3, &dimid)) ERR;
 	if (nc_def_var(ncid, VAR_NAME_1, NC_FLOAT, NDIMS1, &dimid, &varid1)) ERR;
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_1) != NC_ENOTNC4) ERR;
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_3) != NC_ENOTNC4) ERR;
 	if (nc_inq_var_quantize(ncid, varid1, &quantize_mode_in, &nsd_in) != NC_ENOTNC4) ERR;
 	if (nc_close(ncid)) ERR;
 
 	/* Create a netcdf-4 file with two vars. Attempt
 	 * quantization. It will work, eventually... */
 	if (nc_create(FILE_NAME, NC_NETCDF4|NC_CLOBBER, &ncid)) ERR;
-	if (nc_def_dim(ncid, DIM_NAME_1, DIM_LEN_1, &dimid)) ERR;
+	if (nc_def_dim(ncid, DIM_NAME_1, DIM_LEN_3, &dimid)) ERR;
 	if (nc_def_var(ncid, VAR_NAME_1, NC_FLOAT, NDIMS1, &dimid, &varid1)) ERR;
 	if (nc_def_var(ncid, VAR_NAME_2, NC_DOUBLE, NDIMS1, &dimid, &varid2)) ERR;
 
 	/* Bad varid. */
-	if (nc_def_var_quantize(ncid, NC_GLOBAL, NC_QUANTIZE_BITGROOM, NSD_1) != NC_EGLOBAL) ERR;
-	if (nc_def_var_quantize(ncid, varid2 + 1, NC_QUANTIZE_BITGROOM, NSD_1) != NC_ENOTVAR) ERR;
+	if (nc_def_var_quantize(ncid, NC_GLOBAL, NC_QUANTIZE_BITGROOM, NSD_3) != NC_EGLOBAL) ERR;
+	if (nc_def_var_quantize(ncid, varid2 + 1, NC_QUANTIZE_BITGROOM, NSD_3) != NC_ENOTVAR) ERR;
 	/* Invalid values. */
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM + 1, NSD_1) != NC_EINVAL) ERR;
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM + 1, NSD_3) != NC_EINVAL) ERR;
 	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, -1) != NC_EINVAL) ERR;
 	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NC_QUANTIZE_MAX_FLOAT_NSD + 1) != NC_EINVAL) ERR;
 	if (nc_def_var_quantize(ncid, varid2, NC_QUANTIZE_BITGROOM + 1, 3) != NC_EINVAL) ERR;
@@ -60,10 +61,10 @@ main(int argc, char **argv)
 	if (nc_def_var_quantize(ncid, varid2, NC_QUANTIZE_BITGROOM, NC_QUANTIZE_MAX_DOUBLE_NSD + 1) != NC_EINVAL) ERR;
 
 	/* This will work. */
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_1)) ERR;
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_3)) ERR;
 	if (nc_inq_var_quantize(ncid, varid1, &quantize_mode_in, &nsd_in)) ERR;
 	if (quantize_mode_in != NC_QUANTIZE_BITGROOM) ERR;
-	if (nsd_in != NSD_1) ERR;
+	if (nsd_in != NSD_3) ERR;
 
 	/* Wait, I changed my mind! Let's turn off quantization. */
 	if (nc_def_var_quantize(ncid, varid1, NC_NOQUANTIZE, 0)) ERR;
@@ -72,7 +73,7 @@ main(int argc, char **argv)
 	if (nsd_in != 0) ERR;
 
 	/* Changed my mind again, turn it on. */
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_1)) ERR;
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_3)) ERR;
 
 	/* I changed my mind again! Turn it off! */
 	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, 0)) ERR;
@@ -81,19 +82,19 @@ main(int argc, char **argv)
 	if (nsd_in != 0) ERR;
 
 	/* Changed my mind again, turn it on. */
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_1)) ERR;
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_3)) ERR;
 
 	/* This also will work for double. */
-	if (nc_def_var_quantize(ncid, varid2, NC_QUANTIZE_BITGROOM, NSD_2)) ERR;
+	if (nc_def_var_quantize(ncid, varid2, NC_QUANTIZE_BITGROOM, NSD_9)) ERR;
 	if (nc_inq_var_quantize(ncid, varid2, &quantize_mode_in, &nsd_in)) ERR;
 	if (quantize_mode_in != NC_QUANTIZE_BITGROOM) ERR;
-	if (nsd_in != NSD_2) ERR;
+	if (nsd_in != NSD_9) ERR;
 
 	/* End define mode. */
 	if (nc_enddef(ncid)) ERR;
 
 	/* This will not work, it's too late! */
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_1) != NC_ELATEDEF) ERR;
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_3) != NC_ELATEDEF) ERR;
 
 	/* Close the file. */
 	if (nc_close(ncid)) ERR;
@@ -102,45 +103,10 @@ main(int argc, char **argv)
 	if (nc_open(FILE_NAME, NC_WRITE, &ncid)) ERR;
 	if (nc_inq_var_quantize(ncid, 0, &quantize_mode_in, &nsd_in)) ERR;
 	if (quantize_mode_in != NC_QUANTIZE_BITGROOM) ERR;
-	if (nsd_in != NSD_1) ERR;
+	if (nsd_in != NSD_3) ERR;
 	if (nc_inq_var_quantize(ncid, 1, &quantize_mode_in, &nsd_in)) ERR;
 	if (quantize_mode_in != NC_QUANTIZE_BITGROOM) ERR;
-	if (nsd_in != NSD_2) ERR;
-	if (nc_close(ncid)) ERR;
-    }
-    SUMMARIZE_ERR;
-
-    printf("**** testing quantization values...");
-    {
-	int ncid, dimid, varid1, varid2;
-	int quantize_mode_in, nsd_in;
-	float float_data[DIM_LEN_1] = {1.0, 1.11111, 1.1111111};
-	double double_data[DIM_LEN_1] = {1.0, 1.111111111, 1.111111111111};
-
-	/* Create a netcdf-4 file with two vars. Attempt
-	 * quantization. */
-	if (nc_create(FILE_NAME, NC_NETCDF4|NC_CLOBBER, &ncid)) ERR;
-	if (nc_def_dim(ncid, DIM_NAME_1, DIM_LEN_1, &dimid)) ERR;
-	if (nc_def_var(ncid, VAR_NAME_1, NC_FLOAT, NDIMS1, &dimid, &varid1)) ERR;
-	if (nc_def_var(ncid, VAR_NAME_2, NC_DOUBLE, NDIMS1, &dimid, &varid2)) ERR;
-
-	/* This will work. */
-	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_1)) ERR;
-	if (nc_def_var_quantize(ncid, varid2, NC_QUANTIZE_BITGROOM, NSD_2)) ERR;
-
-	/* Write some data. */
-	if (nc_put_var_float(ncid, varid1, float_data)) ERR;
-	if (nc_put_var_double(ncid, varid2, double_data)) ERR;
-
-	/* Close the file. */
-	if (nc_close(ncid)) ERR;
-
-	/* Open the file and check. */
-	if (nc_open(FILE_NAME, NC_WRITE, &ncid)) ERR;
-	if (nc_inq_var_quantize(ncid, 0, &quantize_mode_in, &nsd_in)) ERR;
-	if (quantize_mode_in != NC_QUANTIZE_BITGROOM || nsd_in != NSD_1) ERR;
-	if (nc_inq_var_quantize(ncid, 1, &quantize_mode_in, &nsd_in)) ERR;
-	if (quantize_mode_in != NC_QUANTIZE_BITGROOM || nsd_in != NSD_2) ERR;
+	if (nsd_in != NSD_9) ERR;
 	if (nc_close(ncid)) ERR;
     }
     SUMMARIZE_ERR;
@@ -190,7 +156,59 @@ main(int argc, char **argv)
 	}
     }
     SUMMARIZE_ERR;
-    
+    printf("**** testing quantization values...");
+    {
+	int ncid, dimid, varid1, varid2;
+	int quantize_mode_in, nsd_in;
+	float float_data[DIM_LEN_1] = {1.1111111};
+	double double_data[DIM_LEN_1] = {1.111111111111};
+
+	/* Create a netcdf-4 file with two vars. */
+	if (nc_create(FILE_NAME, NC_NETCDF4|NC_CLOBBER, &ncid)) ERR;
+	if (nc_def_dim(ncid, DIM_NAME_1, DIM_LEN_1, &dimid)) ERR;
+	if (nc_def_var(ncid, VAR_NAME_1, NC_FLOAT, NDIMS1, &dimid, &varid1)) ERR;
+	if (nc_def_var(ncid, VAR_NAME_2, NC_DOUBLE, NDIMS1, &dimid, &varid2)) ERR;
+
+	/* Turn on quantize for both vars. */
+	if (nc_def_var_quantize(ncid, varid1, NC_QUANTIZE_BITGROOM, NSD_3)) ERR;
+	if (nc_def_var_quantize(ncid, varid2, NC_QUANTIZE_BITGROOM, NSD_9)) ERR;
+
+	/* Write some data. */
+	if (nc_put_var_float(ncid, varid1, float_data)) ERR;
+	if (nc_put_var_double(ncid, varid2, double_data)) ERR;
+
+	/* Close the file. */
+	if (nc_close(ncid)) ERR;
+
+	{
+	    float float_in;
+	    double double_in;
+	    union {
+		float f;
+		uint32_t u;
+	    } fin, fout;
+		    
+	    /* Open the file and check metadata. */
+	    if (nc_open(FILE_NAME, NC_WRITE, &ncid)) ERR;
+	    if (nc_inq_var_quantize(ncid, 0, &quantize_mode_in, &nsd_in)) ERR;
+	    if (quantize_mode_in != NC_QUANTIZE_BITGROOM || nsd_in != NSD_3) ERR;
+	    if (nc_inq_var_quantize(ncid, 1, &quantize_mode_in, &nsd_in)) ERR;
+	    if (quantize_mode_in != NC_QUANTIZE_BITGROOM || nsd_in != NSD_9) ERR;
+	    
+	    /* Check the data. */
+	    if (nc_get_var(ncid, varid1, &float_in)) ERR;
+	    if (nc_get_var(ncid, varid2, &double_in)) ERR;
+	    fin.f = float_data[0];
+	    fout.f = float_in;
+	    printf ("\nfloat_data: %10f   : 0x%x  float_data_in: %10f   : 0x%x\n",
+		    float_data[0], fout.u, float_data[0], fin.u);
+	    
+	    /* Close the file again. */
+	    if (nc_close(ncid)) ERR;
+	}
+    }
+    SUMMARIZE_ERR;
+
     FINAL_RESULTS;
 
 
