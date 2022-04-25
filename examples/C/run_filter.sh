4792919795ac7a66525a57e46ed296d3af8d1b11
Dennis Heimbigner <dmh@ucar.edu>
2022-04-24 19:57:18 -0600

    debug5

--- examples/C/run_filter.sh
+++ examples/C/run_filter.sh
@@ -26,8 +26,8 @@ BZIP2PATH="${HDF5_PLUGIN_DIR}/${HDF5_PLUGIN_LIB}"
 if ! test -f ${BZIP2PATH} ; then echo "Unable to locate ${BZIP2PATH}"; exit 1; fi
 
 rm -f ./bzip2.nc
-printenv HDF5_PLUGIN_PATH
 export HDF5_PLUGIN_PATH="${HDF5_PLUGIN_LIB}"
+echo "XXX: ${HDF5_PLUGIN_PATH}"
 echo "*** running filter_example..."
 ${execdir}/filter_example
 ${TOPBUILDIR}/ncdump/ncdump -sh ./bzip2.nc
