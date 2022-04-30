6945f53b984e6efca212d3d6a3d8240b9c91253e
Edward Hartnett <edward.hartnett@noaa.gov>
2022-04-30 07:48:03 -0600

    undoing unintended change

--- nc_test4/tst_parallel6.c
+++ nc_test4/tst_parallel6.c
@@ -23,8 +23,9 @@
 
 int main(int argc, char** argv)
 {
-    int rank, nprocs;
+    int err = NC_NOERR, rank, nprocs;
     int ncid, varid, dimid;
+    size_t start[1], count[1], nrecs;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -33,67 +34,36 @@ int main(int argc, char** argv)
     if (!rank)
         printf("\n*** Testing parallel I/O.\n");
 
-    /* if (!rank) */
-    /*     printf("*** testing record lenth with multiple processes writing records..."); */
-    /* { */
-
-    /*  size_t start[1], count[1], nrecs; */
-    /* 	/\* nc_set_log_level(4); *\/ */
-    /* 	if (nc_create_par(FILENAME, NC_CLOBBER | NC_NETCDF4, MPI_COMM_WORLD, */
-    /* 			  MPI_INFO_NULL, &ncid)) ERR; */
-	
-    /* 	if (nc_def_dim(ncid, "time", NC_UNLIMITED, &dimid)) ERR; */
-    /* 	if (nc_def_var(ncid, "var", NC_INT, 1, &dimid, &varid)) ERR; */
-    /* 	if (nc_var_par_access(ncid, varid, NC_COLLECTIVE)) ERR; */
-    /* 	if (nc_enddef(ncid)) ERR; */
-	
-    /* 	start[0] = rank; */
-    /* 	count[0] = 1; */
-    /* 	if (nc_put_vara_int(ncid, varid, start, count, &rank)) ERR; */
-    /* 	if (nc_inq_dimlen(ncid, dimid, &nrecs)) ERR; */
-    /* 	if (nc_close(ncid)) ERR; */
-    /* 	/\* nc_set_log_level(-1); *\/ */
-	
-    /* 	if (nrecs != nprocs) */
-    /* 	{ */
-    /* 	    printf("Rank %d error at line %d of file %s:\n",rank,__LINE__,__FILE__); */
-    /* 	    printf("\tafter writing start=%zd count=%zd\n", start[0], count[0]); */
-    /* 	    printf("\texpecting number of records = %d but got %ld\n", */
-    /* 		   nprocs, nrecs); */
-    /* 	    ERR; */
-    /* 	} */
-    /* } */
-    /* if (!rank) */
-    /*     SUMMARIZE_ERR; */
-
     if (!rank)
-        printf("*** testing a reported enddef problem...");
-    {
-	int i;
-	char varname[NC_MAX_NAME + 1];
-    	nc_set_log_level(4);	
-	if (nc_create_par(FILENAME, NC_CLOBBER | NC_MPIIO | NC_NETCDF4, MPI_COMM_WORLD,
-			  MPI_INFO_NULL, &ncid)) ERR;
-	if (nc_def_dim(ncid, "time", NC_UNLIMITED, &dimid)) ERR;
-	if (nc_def_var(ncid, "time", NC_INT, 1, &dimid, &varid)) ERR;
-	if (nc_put_att_text(ncid, varid, "Att1", 1, "a")) ERR;
-	if (nc_put_att_text(ncid, varid, "Att2", 1, "a")) ERR;
-	if (nc_put_att_text(ncid, varid, "Att3", 1, "a")) ERR;
-	if (nc_put_att_text(ncid, varid, "Att4", 1, "a")) ERR;
-	for (i = 0; i < 253; i++)
-	{
-	    snprintf(varname, 32, "dummy_var_%d", i);
-	    if (nc_def_var(ncid, varname, NC_INT, 1, &dimid, &varid)) ERR;
-	}
-	
-	if (nc_enddef(ncid)) ERR;
-	if (nc_close(ncid)) ERR;
-    	nc_set_log_level(-1);	
+        printf("*** testing record lenth with multiple processes writing records...");
 
+    /* nc_set_log_level(4); */
+    if (nc_create_par(FILENAME, NC_CLOBBER | NC_NETCDF4, MPI_COMM_WORLD,
+		      MPI_INFO_NULL, &ncid)) ERR;
+
+    if (nc_def_dim(ncid, "time", NC_UNLIMITED, &dimid)) ERR;
+    if (nc_def_var(ncid, "var", NC_INT, 1, &dimid, &varid)) ERR;
+    if (nc_var_par_access(ncid, varid, NC_COLLECTIVE)) ERR;
+    if (nc_enddef(ncid)) ERR;
+
+    start[0] = rank;
+    count[0] = 1;
+    if (nc_put_vara_int(ncid, varid, start, count, &rank)) ERR;
+    if (nc_inq_dimlen(ncid, dimid, &nrecs)) ERR;
+    if (nc_close(ncid)) ERR;
+    /* nc_set_log_level(-1); */
+
+    if (nrecs != nprocs)
+    {
+	printf("Rank %d error at line %d of file %s:\n",rank,__LINE__,__FILE__);
+	printf("\tafter writing start=%zd count=%zd\n", start[0], count[0]);
+	printf("\texpecting number of records = %d but got %ld\n",
+	       nprocs, nrecs);
+	ERR;
     }
+
     if (!rank)
         SUMMARIZE_ERR;
-
 
     MPI_Finalize();
 
