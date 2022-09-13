0dcfe90b1358e96401d6aacd8577e6cc57257ba6
Dennis Heimbigner <dmh@ucar.edu>
2022-09-13 15:34:28 -0600

    core5

--- nc_test/test_threads.sh
+++ nc_test/test_threads.sh
@@ -14,18 +14,7 @@ echo "PASS: thread safe netcdf-3"
 
 if test "x$FEATURE_HDF5" = xyes ; then
 echo "*** Testing netcdf-4 thread safe execution"
-ulimit -c unlimited
-if ${execdir}/tst_threads 3 "tmp_threadsafe_4_%d.nc" ; then
-fail=0
-else
-fail=1
-fi
-ttpid=$!
-echo $ttpid
-ls -l ~/Library/Logs/DiagnosticReports
-ls -ld /cores
-ls -lrt /cores
-touch /cores/test && rm /cores/test
+${execdir}/tst_threads 3 "tmp_threadsafe_4_%d.nc"
 echo "PASS: thread safe netcdf-4"
 fi
 
