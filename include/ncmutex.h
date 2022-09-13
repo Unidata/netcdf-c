807f0704a3246025acfa19562b05a3d790047773
Dennis Heimbigner <dmh@ucar.edu>
2022-09-13 11:31:42 -0600

    barrier5

--- include/ncmutex.h
+++ include/ncmutex.h
@@ -48,8 +48,8 @@ typedef struct pthread_barrier_t {
   size_t                  num;
 } pthread_barrier_t;
 
-EXTERNL int pthread_barrier_init(pthread_barrier_t* bar, int attr, int num);
-EXTERNL int pthread_barrier_wait(pthread_barrier_t *bar);
+extern int pthread_barrier_init(pthread_barrier_t* bar, int attr, int num);
+extern int pthread_barrier_wait(pthread_barrier_t *bar);
 
 #endif /*__APPLE__*/
 #endif /*HAVE_PTHREADS*/
