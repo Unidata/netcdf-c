ba7c9875434600ac5c5945f6075205d27a2438cc
Dennis Heimbigner <dmh@ucar.edu>
2022-09-13 20:43:10 -0600

    three1

--- nc_test/test_threads.sh
+++ nc_test/test_threads.sh
@@ -14,7 +14,7 @@ echo "PASS: thread safe netcdf-3"
 
 if test "x$FEATURE_HDF5" = xyes ; then
 echo "*** Testing netcdf-4 thread safe execution"
-${execdir}/tst_threads 3 "tmp_threadsafe_4_%d.nc"
+${execdir}/tst_threads 4 "tmp_threadsafe_4_%d.nc"
 echo "PASS: thread safe netcdf-4"
 fi
 
