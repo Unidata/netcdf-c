55c3eccc803abc65d7f5336f554ecb6b33e49c12
Dennis Heimbigner <dmh@ucar.edu>
2022-11-06 16:40:41 -0700

    update file permission

---
+++ nc_test/nc_test_threads.c
@@ -0,0 +1,8 @@
+/*********************************************************************
+ *   Copyright 1996-2018, UCAR/Unidata
+ *   See COPYRIGHT file for copying and redistribution conditions.
+ *   $Id: nc_test.c 2796 2014-10-28 03:40:29Z wkliao $
+ *********************************************************************/
+
+#define USETHREADS
+#include "nc_test.c"
