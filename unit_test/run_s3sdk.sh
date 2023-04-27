c8d14cb029e18932da76e7bcf2d9ba9684aa6069
Dennis Heimbigner <dmh@ucar.edu>
2023-04-27 13:32:02 -0600

    gdb1

--- unit_test/run_s3sdk.sh
+++ unit_test/run_s3sdk.sh
@@ -3,10 +3,10 @@
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi 
 . ../test_common.sh
 
-set -e
 set -x
 
 #CMD="valgrind --leak-check=full"
+CMD="gdb -batch -ex r -ex bt -ex q --args"
 
 URL="https://s3.us-east-1.amazonaws.com/unidata-zarr-test-data"
 
@@ -18,30 +18,30 @@ S3ISOPATH="/netcdf-c"
 S3ISOPATH="${S3ISOPATH}/$S3ISODIR"
 
 test_cleanup() {
-${CMD} ${execdir}/../nczarr_test/s3util -u "${URL}" -k "${S3ISOPATH}" clear
+${CMD} ${execdir}/../nczarr_test/.libs/s3util -u "${URL}" -k "${S3ISOPATH}" clear
 }
 if test "x$GITHUB_ACTIONS" != xtrue; then
-trap test_cleanup EXIT
+echo trap test_cleanup EXIT
 fi
 
 THISDIR=`pwd`
 cd $ISOPATH
 
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                list
-${CMD} ${execdir}/test_s3sdk -u "${URL}"                                  exists
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/test_s3sdk.txt" write
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/test_s3sdk.txt" read
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/test_s3sdk.txt" size
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                list
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                search
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/test_s3sdk.txt" delete
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}"                                  exists
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/.libs/test_s3sdk.txt" write
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/.libs/test_s3sdk.txt" read
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/.libs/test_s3sdk.txt" size
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                list
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                search
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}/.libs/test_s3sdk.txt" delete
 if test "x$FEATURE_LARGE_TESTS" = xyes ; then
-${CMD} ${execdir}/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                longlist
+${CMD} ${execdir}/.libs/test_s3sdk -u "${URL}" -k "${S3ISOPATH}"                longlist
 fi
 
+exit
 if test "x$GITHUB_ACTIONS" = xtrue; then
 # Cleanup on exit
-test_cleanup
+echo test_cleanup
 fi
 
 exit
