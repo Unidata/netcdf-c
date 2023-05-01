dcbeb8137ffcb3071f5bfc9b24ef87308a9e28eb
Sean McBride <sean@rogue-research.com>
2023-05-01 13:25:53 -0400

    Fixed various UBSan warnings about invalid bit shifting
    Just made sure to use unsigned, so that a bit does not get shifted into a sign bit.

--- libdispatch/ncexhash.c
+++ libdispatch/ncexhash.c
@@ -111,7 +111,7 @@ ncexinit(void)
     int i;
     bitmasks[0] = 0;
     for(i=1;i<NCEXHASHKEYBITS;i++)
-	bitmasks[i] = (1 << i) - 1;
+	bitmasks[i] = (1ULL << i) - 1;
     ncexinitialized = 1;
 }
 
@@ -855,7 +855,7 @@ ncexhashprintstats(NCexhashmap* map)
     fprintf(stderr," |leaf|=%d nactive/nleaves=%g", map->leaflen, leafavg);
     fprintf(stderr," load=%g",leafload);
     fprintf(stderr,"]\n");
-    dirsize = (1<<(map->depth))*((unsigned long long)sizeof(void*));
+    dirsize = (1ULL<<(map->depth))*((unsigned long long)sizeof(void*));
     leafsize = (nleaves)*((unsigned long long)sizeof(NCexleaf));
     total = dirsize + leafsize;
     fprintf(stderr,"\tsizeof(directory)=%llu sizeof(leaves)=%lld total=%lld\n",
