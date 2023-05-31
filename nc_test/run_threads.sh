20e7e8c4bab527017ba89ab0c205e06e6484aa0a
Dennis Heimbigner <dmh@ucar.edu>
2023-05-30 20:23:32 -0600

    gdb2

--- nc_test/run_threads.sh
+++ nc_test/run_threads.sh
@@ -15,13 +15,13 @@ isolate "testdir_nc_tests_threads"
 cd $ISOPATH
 
 echo "*** Testing netcdf-3 thread safe execution"
-gdb -batch -ex run -ex bt --args ${execdir}/tst_threads -3 -F 'tmp_threadsafe_3_%d.nc' -T ${NTHREADS} -C ${NCYCLES}
+gdb -batch -ex run -ex bt --args ${execdir}/.libs/tst_threads -3 -F 'tmp_threadsafe_3_%d.nc' -T ${NTHREADS} -C ${NCYCLES}
 echo "PASS: thread safe netcdf-3"
 rm -f tmp_threadsafe*.nc
 
 if test "x$FEATURE_HDF5" = xyes ; then
 echo "*** Testing netcdf-4 thread safe execution"
-gdb -batch -ex run -ex bt --args ${execdir}/tst_threads -4 -F 'tmp_threadsafe_4_%d.nc' -T ${NTHREADS} -C ${NCYCLES}
+gdb -batch -ex run -ex bt --args ${execdir}/.libs/tst_threads -4 -F 'tmp_threadsafe_4_%d.nc' -T ${NTHREADS} -C ${NCYCLES}
 echo "PASS: thread safe netcdf-4"
 fi
 rm -f tmp_threadsafe*.nc
