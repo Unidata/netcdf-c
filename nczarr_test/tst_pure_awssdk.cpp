dd1ae95b28a3939b87561974e561008ff87974f1
Ward Fisher <wfisher@ucar.edu>
2023-08-29 16:38:21 -0600

    Tweak spacing in pure awssdk test.

--- nczarr_test/tst_pure_awssdk.cpp
+++ nczarr_test/tst_pure_awssdk.cpp
@@ -3,13 +3,17 @@
 int main(int argc, char** argv)
 {
    Aws::SDKOptions options;
-   options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
+
    std::cout << "Running a pure-aws test instantiation to test the aws-cpp-sdk install.\nA failure may manifest as a hang.\n\n";
+
    std::cout << "\t* Testing InitAPI()\n";
+   options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Debug;
    Aws::InitAPI(options);
    std::cout << "\t\t* Passed.\n";
+   
    std::cout << "\t* Testing ShutdownAPI()\n";
    Aws::ShutdownAPI(options);
    std::cout << "\t\t* Passed.\n\nFinished.\n\n";
+
    return 0;
 }
