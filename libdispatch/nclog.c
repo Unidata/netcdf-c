Unidata/netcdf-c

commit df3636b95974ebccdd149c280ce09b3d897a5dd8

Mitigate S3 test interference + Unlimited Dimensions in NCZarr
This PR started as an attempt to add unlimited dimensions to NCZarr.
It did that, but this exposed significant problems with test interference.
So this PR is mostly about fixing -- well mitigating anyway -- test
interference.

The problem of test interference is now documented in the document docs/internal.md.
The solutions implemented here are also describe in that document.
The solution is somewhat fragile but multiple cleanup mechanisms
are provided. Note that this feature requires that the
AWS command line utility must be installed.

## Unlimited Dimensions.
The existing NCZarr extensions to Zarr are modified to support unlimited dimensions.
NCzarr extends the Zarr meta-data for the ".zgroup" object to include netcdf-4 model extensions. This information is stored in ".zgroup" as dictionary named "_nczarr_group".
Inside "_nczarr_group", there is a key named "dims" that stores information about netcdf-4 named dimensions. The value of "dims" is a dictionary whose keys are the named dimensions. The value associated with each dimension name has one of two forms
Form 1 is a special case of form 2, and is kept for backward compatibility. Whenever a new file is written, it uses format 1 if possible, otherwise format 2.
* Form 1: An integer representing the size of the dimension, which is used for simple named dimensions.
* Form 2: A dictionary with the following keys and values"
   - "size" with an integer value representing the (current) size of the dimension.
   - "unlimited" with a value of either "1" or "0" to indicate if this dimension is an unlimited dimension.

For Unlimited dimensions, the size is initially zero, and as variables extend the length of that dimension, the size value for the dimension increases.

libdispatch/nclog.c
@@ -37,7 +37,7 @@
 static int nclogginginitialized = 0;
 
 static struct NCLOGGLOBAL {
-    int nclogging;
+    int loglevel;
     int tracelevel;
     FILE* nclogstream;
     int depth;
@@ -48,11 +48,11 @@ static struct NCLOGGLOBAL {
     } frames[NC_MAX_FRAMES];
 } nclog_global = {0,-1,NULL};
 
-static const char* nctagset[] = {"Note","Warning","Error","Debug"};
-static const int nctagsize = sizeof(nctagset)/sizeof(char*);
+static const char* nctagset[] = {"OFF","ERR","WARN","NOTE","DEBUG",NULL};
 
 /* Forward */
 static const char* nctagname(int tag);
+static int nctagforname(const char* tag);
  
 /*!\defgroup NClog NClog Management
 @{*/
@@ -68,19 +68,17 @@ ncloginit(void)
 	return;
     nclogginginitialized = 1;
     memset(&nclog_global,0,sizeof(nclog_global));
+    ncsetloglevel(NCLOGOFF);
     nclog_global.tracelevel = -1;    
-    ncsetlogging(0);
     nclog_global.nclogstream = stderr;
     /* Use environment variables to preset nclogging state*/
-    /* I hope this is portable*/
     envv = getenv(NCENVLOGGING);
     if(envv != NULL) {
-	ncsetlogging(1);
+	int level = nctagforname(envv);
+        if(level < 0) ncsetloglevel(level);
     }
     envv = getenv(NCENVTRACING);
-    if(envv != NULL) {
-	nctracelevel(atoi(envv));
-    }
+    if(envv != NULL) nctracelevel(atoi(envv));
 }
 
 /*!
@@ -92,12 +90,13 @@ Enable/Disable logging.
 */
 
 int
-ncsetlogging(int tf)
+ncsetloglevel(int level)
 {
     int was;
     if(!nclogginginitialized) ncloginit();
-    was = nclog_global.nclogging;
-    nclog_global.nclogging = tf;
+    was = nclog_global.loglevel;
+    if(level >= 0 && level <= NCLOGDEBUG)
+	nclog_global.loglevel = level;
     if(nclog_global.nclogstream == NULL) nclogopen(NULL);
     return was;
 }
@@ -131,23 +130,21 @@ nclog(int tag, const char* fmt, ...)
     }
 }
 
-int
-ncvlog(int tag, const char* fmt, va_list ap)
+void
+ncvlog(int level, const char* fmt, va_list ap)
 {
     const char* prefix;
-    int was = -1;
 
     if(!nclogginginitialized) ncloginit();
-    if(tag == NCLOGERR) was = ncsetlogging(1);
-    if(!nclog_global.nclogging || nclog_global.nclogstream == NULL) return was;
-    prefix = nctagname(tag);
+    if(nclog_global.loglevel > level || nclog_global.nclogstream == NULL)
+	return;
+    prefix = nctagname(level);
     fprintf(nclog_global.nclogstream,"%s: ",prefix);
     if(fmt != NULL) {
       vfprintf(nclog_global.nclogstream, fmt, ap);
     }
     fprintf(nclog_global.nclogstream, "\n" );
     fflush(nclog_global.nclogstream);
-    return was;
 }
 
 void
@@ -164,10 +161,10 @@ Each line will be sent using nclog with the specified tag.
 */
 
 void
-nclogtextn(int tag, const char* text, size_t count)
+nclogtextn(int level, const char* text, size_t count)
 {
-    NC_UNUSED(tag);
-    if(!nclog_global.nclogging || nclog_global.nclogstream == NULL) return;
+    if(nclog_global.loglevel > level || nclog_global.nclogstream == NULL)
+	return;
     fwrite(text,1,count,nclog_global.nclogstream);
     fflush(nclog_global.nclogstream);
 }
@@ -175,9 +172,20 @@ nclogtextn(int tag, const char* text, size_t count)
 static const char*
 nctagname(int tag)
 {
-    if(tag < 0 || tag >= nctagsize)
+    if(tag < NCLOGOFF || tag >= NCLOGDEBUG)
 	return "unknown";
     return nctagset[tag];
+}
+
+static int
+nctagforname(const char* tag)
+{
+    int level;
+    const char** p = NULL;
+    for(level=0,p=nctagset;*p;p++,level++) {
+	if(strcasecmp(*p,tag)==0) return level;
+    }
+    return -1;
 }
 
 /*!
@@ -194,10 +202,8 @@ nctracelevel(int level)
     oldlevel = nclog_global.tracelevel;
     if(level < 0) {
       nclog_global.tracelevel = level;
-      ncsetlogging(0);
     } else { /*(level >= 0)*/
         nclog_global.tracelevel = level;
-        ncsetlogging(1);
 	nclogopen(NULL); /* use stderr */    
     }
     return oldlevel;
@@ -226,7 +232,6 @@ ncvtrace(int level, const char* fcn, const char* fmt, va_list ap)
 {
     struct Frame* frame;
     if(!nclogginginitialized) ncloginit();
-    if(nclog_global.tracelevel < 0) ncsetlogging(0);
     if(fcn != NULL) {
         frame = &nclog_global.frames[nclog_global.depth];
         frame->fcn = fcn;

commit 25669666ef35e8aa09ea94e29d0b4fd38cb17c93

Fix bug with displaying log messages
One of the changes in df3636b introduced the ability to filter log
messages by level. This fixes a typo that flipped the intended
filtering, so that `NCLOGOFF` _enabled_ all messages instead of
_disabling_ them.

libdispatch/nclog.c
@@ -82,9 +82,11 @@ ncloginit(void)
 }
 
 /*!
-Enable/Disable logging.
+Enable logging messages to a given level. Set to NCLOGOFF to disable
+all messages, NCLOGERR for errors only, NCLOGWARN for warnings and
+errors, and so on
 
-\param[in] tf If 1, then turn on logging, if 0, then turn off logging.
+\param[in] level Messages above this level are ignored
 
 \return The previous value of the logging flag.
 */
@@ -136,8 +138,9 @@ ncvlog(int level, const char* fmt, va_list ap)
     const char* prefix;
 
     if(!nclogginginitialized) ncloginit();
-    if(nclog_global.loglevel > level || nclog_global.nclogstream == NULL)
-	return;
+    if(nclog_global.loglevel < level || nclog_global.nclogstream == NULL) {
+        return;
+    }
     prefix = nctagname(level);
     fprintf(nclog_global.nclogstream,"%s: ",prefix);
     if(fmt != NULL) {