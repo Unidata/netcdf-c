fb537e61eb4ad3ced4f19520248adacf54bf43d9
Ward Fisher <wfisher@ucar.edu>
2024-02-07 10:18:37 -0700

    Correct previous fix to remove inclusion of internal header.

--- include/ncjson.h
+++ include/ncjson.h
@@ -15,7 +15,18 @@ and do the command:
     make makepluginjson
 */
 
-#include "ncexternl.h"
+#if defined(DLL_NETCDF) /* define when library is a DLL */
+#  if defined(DLL_EXPORT) /* define when building the library */
+#   define MSC_EXTRA __declspec(dllexport)
+#  else
+#   define MSC_EXTRA __declspec(dllimport)
+#  endif
+#else
+#  define MSC_EXTRA
+#endif	/* defined(DLL_NETCDF) */
+#ifndef EXTERNL
+# define EXTERNL MSC_EXTRA extern
+#endif
 
 /* Override for plugins */
 #ifdef NETCDF_JSON_H
