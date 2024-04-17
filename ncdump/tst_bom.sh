963f6e4c7454bbaf960e4f6b01fd9a6a233b3855
Ward Fisher <wfisher@ucar.edu>
2024-04-17 11:55:07 -0600

    Add debugging statement.

--- ncdump/tst_bom.sh
+++ ncdump/tst_bom.sh
@@ -27,6 +27,11 @@ EOF
 echo "*** Generate a cdl file with leading UTF-8 BOM."
 ${execdir}/bom 8 >tst_bom8.cdl
 cat tst_bom.cdl >> tst_bom8.cdl
+cat tst_bom.cdl >> tst_bom8.cdl
+echo ""
+echo "Viewing tst_bom16.cdl:"
+cat tst_bom8.cdl
+echo ""
 
 echo "*** Verify .nc file"
 ${NCGEN} -k nc3 -o tst_bom8.nc tst_bom8.cdl
@@ -40,6 +45,11 @@ rm -f tmp_bom.cdl tst_bom8.* tst_bom16.*
 echo "*** Generate a cdl file with leading UTF-16 BOM."
 ${execdir}/bom 16 >tst_bom16.cdl
 cat tst_bom.cdl >> tst_bom16.cdl
+echo ""
+echo "Viewing tst_bom16.cdl:"
+cat tst_bom16.cdl
+echo ""
+
 
 echo "*** Verify UTF-16 file fails"
 if ${NCGEN} -k nc3 -o tst_bom16.nc tst_bom16.cdl ; then
