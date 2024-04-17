5dedc4d40d4c2d2d2be7a590ef46e284905d94a2
Ward Fisher <wfisher@ucar.edu>
2024-04-17 13:29:18 -0500

    Add CI for a Windows Runner on Github Actions.

--- ncdump/tst_bom.sh
+++ ncdump/tst_bom.sh
@@ -6,7 +6,7 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 # This shell script tests BOM support in ncgen
 
 set -e
-
+set -x
 # add hack for sunos
 export srcdir;
 
@@ -28,7 +28,13 @@ echo "*** Generate a cdl file with leading UTF-8 BOM."
 ${execdir}/bom 8 >tst_bom8.cdl
 cat tst_bom.cdl >> tst_bom8.cdl
 
+echo ""
+echo "Viewing tst_bom8.cdl:"
+cat tst_bom8.cdl
+echo ""
+
 echo "*** Verify .nc file"
+
 ${NCGEN} -k nc3 -o tst_bom8.nc tst_bom8.cdl
 ${NCDUMP} -n tst_bom tst_bom8.nc > tmp_bom.cdl
 diff -w tst_bom.cdl tmp_bom.cdl
@@ -40,6 +46,11 @@ rm -f tmp_bom.cdl tst_bom8.* tst_bom16.*
 echo "*** Generate a cdl file with leading UTF-16 BOM."
 ${execdir}/bom 16 >tst_bom16.cdl
 cat tst_bom.cdl >> tst_bom16.cdl
+echo ""
+echo "Viewing tst_bom16.cdl:"
+cat tst_bom16.cdl
+echo ""
+
 
 echo "*** Verify UTF-16 file fails"
 if ${NCGEN} -k nc3 -o tst_bom16.nc tst_bom16.cdl ; then
