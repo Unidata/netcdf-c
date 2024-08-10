2b7e890864c4ef85d70294a055096cce5e9a6561
Edward Hartnett <edward.hartnett@noaa.gov>
2024-08-10 07:57:03 -0600

    updating

--- nc_test4/run_zstd_test.sh
+++ nc_test4/run_zstd_test.sh
@@ -10,8 +10,16 @@ set -e
 if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 . ../test_common.sh
 
+# Load the findplugins function
+. ${builddir}/findplugin.sh
+echo "findplugin.sh loaded"
+
 echo
 echo "Testing Zstandard compression..."
+
+HDF5_PLUGIN_PATH="${HDF5_PLUGIN_DIR}"
+echo "set HDF5_PLUGIN_PATH: ${HDF5_PLUGIN_PATH}"
+
 echo "SUCCESS!"
 
 
