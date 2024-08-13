adbd9400aecb08aa2b3d9b87af8b7a6c0305b82a
Manuel Reis <reis@dkrz.de>
2024-08-13 09:19:06 +0200

    Address PR#2969 suggestions

--- unit_test/run_aws_config.sh
+++ unit_test/run_aws_config.sh
@@ -12,17 +12,43 @@ isolate "testdir_ut_aws_config"
 THISDIR=`pwd`
 cd $ISOPATH
 
+mkdir -p $THISDIR/.aws/
+echo """[uni]
+region = somewhere-1
+endpoint_url = https://example.com/bucket/prefix/1
+key = value
+extrakey = willbepropagated
+
+[profile unidata]
+region = us-east-1
+endpoint_url = https://play.min.io/
+dummy_key = dummy_value
+
+[profile play]
+region = us-east-1
+endpoint_url = https://play.min.io/""" > $THISDIR/.aws/config
+
+
+echo """[play] 
+aws_access_key_id = DummyKeys
+aws_secret_access_key = DummySecret
+
+[uni]
+region = somewhere-2
+endpoint_url = https://example.com/bucket/prefix/2
+key = value2""" > $THISDIR/.aws/credentials
+
 echo -e "Testing loading AWS configuration in ${THISDIR}/.aws/config"
-env NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=unidata ${CMD} ${execdir}/aws_config  endpoint_url region dummy_key
+NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=unidata ${CMD} ${execdir}/aws_config  endpoint_url region dummy_key
 echo "Status: $?"
 
-env NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=play ${CMD} ${execdir}/aws_config  endpoint_url region 
+NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=play ${CMD} ${execdir}/aws_config  endpoint_url region 
 echo "Status: $?"
 
-env NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=uni ${CMD} ${execdir}/aws_config  endpoint_url region key 
+NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=uni ${CMD} ${execdir}/aws_config  endpoint_url region key 
 echo "Status: $?"
 
-env NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=uni ${CMD} ${execdir}/aws_config region=somewhere-2 endpoint_url=https://example.com/bucket/prefix/2 key=value2 extrakey=willbepropagated
+NC_TEST_AWS_DIR=${THISDIR} AWS_PROFILE=uni ${CMD} ${execdir}/aws_config region=somewhere-2 endpoint_url=https://example.com/bucket/prefix/2 key=value2 extrakey=willbepropagated
 echo "Status: $?"
 
 echo ${CMD} ${execdir}/aws_config
