18d7499b00f62850d4c489599964d3bb6d3b21a1
Dennis Heimbigner <dennis.heimbigner@gmail.com>
2025-08-03 19:20:37 -0600

    test1

--- nc_test4/tst_specific_filters.sh
+++ nc_test4/tst_specific_filters.sh
@@ -22,7 +22,7 @@ BLOSCARGSALT="32001,0,0,4,256,5,1,1"
 BLOSCCODEC='[{\"id\": \"blosc\",\"clevel\": 5,\"blocksize\": 256,\"cname\": \"lz4\",\"shuffle\": 1}]'
 
 echo "WD=$WD"
-if ls -l "$WD/../plugins ; then ignore=1 ; else ignore=0; fi
+if ls -l "$WD/../plugins" ; then ignore=1 ; else ignore=0; fi
 # Load the findplugins function
 . ${builddir}/findplugin.sh
 echo "findplugin.sh loaded"
