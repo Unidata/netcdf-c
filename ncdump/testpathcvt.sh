9c4aa45972b60908720d549dabf3ea7625b88205
Dennis Heimbigner <dennis.heimbigner@gmail.com>
2025-08-10 15:34:53 -0600

    path16

--- ncdump/testpathcvt.sh
+++ ncdump/testpathcvt.sh
@@ -6,6 +6,12 @@ if test "x$srcdir" = x ; then srcdir=`pwd`; fi
 set -x
 set -e
 
+# For testing purposes: we want MSYS_NO_PATHCONV and MSYS2_ARG_CONV_EXCL set.
+#     Suppress converting paths of form /[a-z]/... to [a-z]:/...
+      export MSYS_NO_PATHCONV=1
+#     Disable automatic path conversions in MINGW shell:
+      export MSYS2_ARG_CONV_EXCL='*'
+
 # '@' will get translated to embedded blank
 TESTPATHS1=
 TESTPATHS1="${TESTPATHS1} /xxx/x/y"
