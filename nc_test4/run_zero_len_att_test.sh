4bbb4633721d9e476a9920c670f82d30df6c7e60
Ward Fisher <wfisher@ucar.edu>
2025-09-10 12:15:44 -0600

    Wiring tests into autotools-based tests.

---
+++ nc_test4/run_zero_len_att_test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+n=0
+
+while [ "$n" -le 4 ]; do
+  echo ''
+
+  if [ $n -eq 0 ]; then
+    value=""
+  else
+    value=$(echo XXXXXXXXXXXXXXXXXXXXXXXXXXXX | cut -c 1-$n)
+  fi
+
+  # Create new netcdf file with character attribute of length N.
+  sed -e "s/STR1/$value/" << EOF | ncgen -4 -o zero-len-attribute-test.nc
+netcdf zero-len-attribute-test {
+variables:
+  int var1 ;
+
+// global attributes:
+    :comment = "STR1" ;
+}
+EOF
+
+  echo Before:
+    h5dump -a comment zero-len-attribute-test.nc \
+      | grep -e STRSIZE -e DATASPACE -e \:  \
+      | tr -s '\n ' ' '
+
+  echo ''
+
+    # Overwrite the character attribute with length 0.
+    ./zero-len-attribute-test
+
+  echo After : 
+  h5dump -a comment zero-len-attribute-test.nc \
+    | grep -e STRSIZE -e DATASPACE -e \:  \
+    | tr -s '\n ' ' '
+  echo ''
+  
+  CHECKVAL=$(h5dump -a comment zero-len-attribute-test.nc \
+    | grep -e STRSIZE -e DATASPACE -e \:  \
+    | tr -s '\n ' ' ' | cut -d ' ' -f 5 | tr -s '\n ' ' ')
+
+  if [ " ${CHECKVAL}" != "NULL" ]; then
+    echo ""
+    echo "ERROR: N size ${n}, expected NULL, received ${CHECKVAL}"
+    echo "Exiting"
+    echo ""
+    exit -1
+  fi
+
+
+  n=$((n + 1))
+done
