91d2fb7640cd9e91e15200c2536429f3970e03fc
Dennis Heimbigner <dennis.heimbigner@ieee.org>
2025-10-14 14:56:27 -0600

    remove OSX change

--- include/netcdf_filter_hdf5_build.h
+++ include/netcdf_filter_hdf5_build.h
@@ -210,7 +210,7 @@ typedef const void* (*H5PL_get_plugin_info_proto)(void);
 
 /* Protect old HDF5 code (pre 1.8.12) */
 #ifdef USE_HDF5
-# if H5_VERSION_LE(1,8,11) || ( defined(__APPLE__) && defined(__MACH__))
+# if H5_VERSION_LE(1,8,11)
 #undef H5allocate_memory
 #undef H5free_memory
 #undef H5resize_memory
