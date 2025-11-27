381cc7f92fb75128ae8ed160f9b9c8944b572482
Manuel Reis <reis@dkrz.de>
2025-11-27 16:19:32 +0100

    tests: exercise consolidated zmetadata fallbacks in various scenarios

--- nczarr_test/run_consolidated_zarr.sh
+++ nczarr_test/run_consolidated_zarr.sh
@@ -61,8 +61,48 @@ testcase_csl_vs_no(){
     set +x
 }
 
+testcase_csl_fallback() {
+    zext=$1
+    set -x
+    echo "*** Test: reading dataset no,empty and invalids .zmetadata file; format=$zext"
+    cp -r $srcdir/ref_consolidated_zarr_2.18.2_python.zarr/. ref_consolidated_zarr_2.18.2_python.zarr.$zext
+    fileargs ref_consolidated_zarr_2.18.2_python.zarr "mode=zarr"
+
+    ${NCDUMP} $fileurl > expected.cdl
+    
+    local mdfile="ref_consolidated_zarr_2.18.2_python.zarr.$zext/.zmetadata"
+    # no .zmetadata file
+    rm "${mdfile}"
+    ${NCDUMP} -L0 $fileurl > no-metadata.cdl 2> no-metadata.log
+    diff -b expected.cdl no-metadata.cdl
+    cat no-metadata.log
+    grep -q "NOTE: Dataset not consolidated! Doing so will improve performance" no-metadata.log
+
+    # empty .zmetadata file
+    truncate -s 0 "${mdfile}"
+    ${NCDUMP} -L0 $fileurl > empty-metadata.cdl 2> empty-metadata.log
+    diff -b expected.cdl empty-metadata.cdl
+    cat empty-metadata.log
+    grep -q "NOTE: Dataset not consolidated! Doing so will improve performance" empty-metadata.log
+
+    # invalid .zmetadata file
+    echo "{}" > "${mdfile}"
+    ${NCDUMP} -L0 $fileurl > invalid-metadata.cdl 2> invalid-metadata.log
+    diff -b expected.cdl invalid-metadata.cdl
+    grep -q "WARN: Consolidated metadata is invalid, ignoring it!" invalid-metadata.log
+
+    # empty dataset only on .zmetadata file
+    echo "{"metadata":{},"zarr_consolidated_format":1}" > "${mdfile}"
+    ${NCDUMP} -L0 $fileurl > empty-dataset.cdl 2> empty-dataset.log
+    diff -b expected.cdl empty-dataset.cdl
+    grep -q "WARN: Consolidated metadata is invalid, ignoring it!" empty-dataset.log
+
+    set +x
+}
+
 testcase file
 testcase_csl_vs_no file
+testcase_csl_fallback file
 if test "x$FEATURE_NCZARR_ZIP" = xyes ; then testcase zip; fi
 if test "x$FEATURE_S3TESTS" = xyes ; then testcase s3; fi
 
