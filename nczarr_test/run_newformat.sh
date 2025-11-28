ba3b7b26630e437c4c33d84b04176718e9004f94
Manuel Reis <reis@dkrz.de>
2025-11-28 12:35:28 +0100

    tests: clean fix tests - this

--- nczarr_test/run_newformat.sh
+++ nczarr_test/run_newformat.sh
@@ -22,14 +22,6 @@ ${NCDUMP} -n ref_oldformat "$fileurl" > ./tmp_oldformat.cdl
 diff -w ${srcdir}/ref_oldformat.cdl ./tmp_oldformat.cdl
 }
 
-testcaseoldconsolidated() {
-echo "*** Test old format support on consolidated zarr"
-zext=$1
-fileargs ${srcdir}/ref_oldformat_only_consolidated
-${NCDUMP} -n ref_oldformat "$fileurl" > ./tmp_oldformat_consolidated.cdl
-diff -w ${srcdir}/ref_oldformat.cdl ./tmp_oldformat_consolidated.cdl
-}
-
 testcasecvt() {
 echo "*** Test old format to new format nczarr copy"
 zext=$1
@@ -51,29 +43,17 @@ diff -w ${srcdir}/ref_newformatpure.cdl ./tmp_newpure.cdl
 testcaseconsolidated() {
 echo "*** Test old format to new format consolidated nczarr copy"
 zext=$1
-fileargs ${srcdir}/ref_oldformat_only_consolidated
-${NCCOPY} "$fileurl" "file://tmp_newformat_consolidated.file#mode=nczarr,file"
-${NCDUMP} -n ref_oldformat "file://tmp_newformat_consolidated.file#mode=nczarr,file" > ./tmp_oldformat_consolidated.cdl
+fileargs ${srcdir}/ref_oldformat
+NCZARR_CONSOLIDATED=TRUE ${NCCOPY} "$fileurl" "file://tmp_newformat_consolidated.file#mode=nczarr,file"
+test -f tmp_newformat_consolidated.file/.zmetadata
+NCZARR_CONSOLIDATED=TRUE ${NCDUMP} -n ref_oldformat "file://tmp_newformat_consolidated.file#mode=nczarr,file" > ./tmp_oldformat_consolidated.cdl
 diff -w ${srcdir}/ref_oldformat.cdl ./tmp_oldformat_consolidated.cdl
 }
 
-testcasepureconsolidated() {
-echo "*** Test old format to new format consolidated pure zarr copy"
-zext=$1
-fileargs ${srcdir}/ref_oldformat_only_consolidated
-# For the moment is not possible to write consolidated datasets
-#${NCCOPY} "$fileurl" "file://tmp_newformat_consolidated.file#mode=nczarr,file"
-# so unziping will act like nccopy:
-unzip -qq ${srcdir}/ref_oldformat_only_consolidated.zip -d tmp_newformat_consolidated.file
-${NCDUMP} -n ref_oldformat "file://tmp_newformat_consolidated.file#mode=zarr,file" > ./tmp_new_consolidated.cdl
-diff -w ${srcdir}/ref_newformatpure.cdl ./tmp_newpure.cdl
-}
 # Do zip tests only
 if test "x$FEATURE_NCZARR_ZIP" = xyes ; then
     testcaseold zip
-    testcaseoldconsolidated zip
     testcasecvt zip
     testcasepure zip
     testcaseconsolidated zip
-    testcasepureconsolidated zip
 fi
