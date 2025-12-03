6f76da5b0250475043bcaa260b4dd52e68ce297e
Manuel Reis <reis@dkrz.de>
2025-12-03 11:47:43 +0100

    zarr: set metadata handler on ncz_open_dataset

--- libnczarr/zarr.c
+++ libnczarr/zarr.c
@@ -143,6 +143,10 @@ ncz_open_dataset(NC_FILE_INFO_T* file, NClist* controls)
     if((stat = nczmap_open(zinfo->controls.mapimpl,nc->path,mode,zinfo->controls.flags,NULL,&zinfo->map)))
 	goto done;
 
+    if((stat = NCZMD_set_metadata_handler(zinfo))) {
+        goto done;
+    }
+
     /* Ok, try to read superblock */
     if((stat = ncz_read_superblock(file,&nczarr_version,&zarr_format))) goto done;
 
