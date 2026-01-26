db89c35a92b0a7f683ceef5deb93199b67179a13
Manuel Reis <reis@dkrz.de>
2026-01-26 17:31:41 +0100

    refactor!: introduce NCZ_infer_nczarr_format
        - It fully replaces ncz_read_superblock

--- libnczarr/zformat.h
+++ libnczarr/zformat.h
@@ -2,6 +2,7 @@
 #define ZFORMAT_H
 
 int NCZ_infer_zarr_format(NC_FILE_INFO_T *file);
+int NCZ_infer_nczarr_format(NC_FILE_INFO_T *file);
 int NCZ_get_map(NC_FILE_INFO_T *file, NCURI *uri, mode_t mode,
                 size64_t constraints, void *params, NCZMAP **mapp);
 #endif
--- libnczarr/zinfer.c
+++ libnczarr/zinfer.c
@@ -1,5 +1,59 @@
 #include "zincludes.h"
 
+int NCZ_infer_nczarr_format(NC_FILE_INFO_T *file) {
+  int stat = NC_NOERR;
+  const NCjson *jsuperblock = NULL, *jnczarrversion = NULL;
+  NCZ_FILE_INFO_T *zfile = (NCZ_FILE_INFO_T *)file->format_file_info;
+  struct ZARROBJ *zobjs =
+      &(((NCZ_GRP_INFO_T *)file->root_grp->format_grp_info)->zgroup);
+
+  int zarrformat = zfile->zarr.zarr_version;
+
+  zfile->zarr.nczarr_version.major = 0;
+  zfile->zarr.nczarr_version.minor = 0;
+  zfile->zarr.nczarr_version.release = 0;
+
+  if (zarrformat == 2) {
+    /* Fetch /.zattrs and /.zgroup contents */
+    if ((stat = NCZMD_fetch_json_group(zfile, "/", &zobjs->obj)) ||
+        (stat = NCZMD_fetch_json_attrs(zfile, "/", &zobjs->atts))) {
+      stat = NC_ENCZARR;
+      goto done;
+    }
+
+    /* Look for superblock; first in .zattrs and then in .zgroup */
+    if ((NCJ_OK == NCJdictget(zobjs->atts, NCZ_V2_SUPERBLOCK, &jsuperblock) &&
+         jsuperblock != NULL) ||
+        (NCJ_OK == NCJdictget(zobjs->obj, NCZ_V2_SUPERBLOCK, &jsuperblock) &&
+         jsuperblock != NULL && (1 == (zobjs->nczv1 = 1)))) {
+
+      if (1 == zobjs->nczv1) { // Key stored in .zgroup!!
+        zfile->controls.flags |= FLAG_NCZARR_KEY;
+        file->no_write = 1;
+      }
+
+      if (NCJsort(jsuperblock) == NCJ_DICT &&
+          NCJ_OK == NCJdictget(jsuperblock, "version", &jnczarrversion) &&
+          jnczarrversion != NULL && NCJsort(jnczarrversion) == NCJ_STRING) {
+
+        if (sscanf(NCJstring(jnczarrversion), "%lu.%lu.%lu",
+                   &zfile->zarr.nczarr_version.major,
+                   &zfile->zarr.nczarr_version.minor,
+                   &zfile->zarr.nczarr_version.release) != 3) {
+          nclog(NCLOGERR, "Issue detecting NCZARR version from %s",
+                NCJstring(jnczarrversion));
+          stat = NC_ENCZARR;
+        }
+      }
+    } else {
+      zfile->controls.flags |= FLAG_PUREZARR;
+      stat = NC_NOERR;
+    }
+  }
+done:
+  return THROW(stat);
+}
+
 int NCZ_infer_zarr_format(NC_FILE_INFO_T *file) {
   int stat = NC_ENOTZARR;
   NCZ_FILE_INFO_T *zfile = (NCZ_FILE_INFO_T *)file->format_file_info;
