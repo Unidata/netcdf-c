312ced72a5998f31b8a5eac0a2a8d8ce19420348
Ed Hartnett <edwardjameshartnett@gmail.com>
2026-02-10 11:47:53 -0700

    attempting to fix UDF on windows problem

--- nc_test4/tst_udf.c
+++ nc_test4/tst_udf.c
@@ -8,6 +8,7 @@
 */
 
 #include "config.h"
+#include <string.h>
 #include <nc_tests.h>
 #include "err_macros.h"
 #include "netcdf.h"
@@ -65,210 +66,123 @@ tst_get_vara(int ncid, int varid, const size_t *start, const size_t *count,
     return TEST_VAL_42;
 }
 
-/* This is the dispatch object that holds pointers to all the
- * functions that make up the test dispatch interface. */
-static NC_Dispatch tst_dispatcher = {
+/* Dispatch tables initialized at runtime in init_dispatchers() because
+ * MSVC cannot use dllimport function addresses as static initializers
+ * (error C2099: "initializer is not a constant"). */
+static NC_Dispatch tst_dispatcher;
+static NC_Dispatch tst_dispatcher_bad_version;
 
-    NC_FORMATX_UDF0,
-    NC_DISPATCH_VERSION,
+/* Populate both dispatch tables at runtime. Must be called once
+ * before any test code references tst_dispatcher or
+ * tst_dispatcher_bad_version. */
+static void
+init_dispatchers(void)
+{
+    /* --- tst_dispatcher --- */
+    memset(&tst_dispatcher, 0, sizeof(tst_dispatcher));
 
-    NC_RO_create,
-    tst_open,
+    tst_dispatcher.model = NC_FORMATX_UDF0;
+    tst_dispatcher.dispatch_version = NC_DISPATCH_VERSION;
 
-    NC_RO_redef,
-    NC_RO__enddef,
-    NC_RO_sync,
-    tst_abort,
-    tst_close,
-    NC_RO_set_fill,
-    tst_inq_format,
-    tst_inq_format_extended,
+    tst_dispatcher.create = NC_RO_create;
+    tst_dispatcher.open = tst_open;
 
-    NC4_inq,
-    NC4_inq_type,
+    tst_dispatcher.redef = NC_RO_redef;
+    tst_dispatcher._enddef = NC_RO__enddef;
+    tst_dispatcher.sync = NC_RO_sync;
+    tst_dispatcher.abort = tst_abort;
+    tst_dispatcher.close = tst_close;
+    tst_dispatcher.set_fill = NC_RO_set_fill;
+    tst_dispatcher.inq_format = tst_inq_format;
+    tst_dispatcher.inq_format_extended = tst_inq_format_extended;
 
-    NC_RO_def_dim,
-    NC4_inq_dimid,
-    HDF5_inq_dim,
-    NC4_inq_unlimdim,
-    NC_RO_rename_dim,
+    tst_dispatcher.inq = NC4_inq;
+    tst_dispatcher.inq_type = NC4_inq_type;
 
-    NC4_inq_att,
-    NC4_inq_attid,
-    NC4_inq_attname,
-    NC_RO_rename_att,
-    NC_RO_del_att,
-    NC4_get_att,
-    NC_RO_put_att,
+    tst_dispatcher.def_dim = NC_RO_def_dim;
+    tst_dispatcher.inq_dimid = NC4_inq_dimid;
+    tst_dispatcher.inq_dim = HDF5_inq_dim;
+    tst_dispatcher.inq_unlimdim = NC4_inq_unlimdim;
+    tst_dispatcher.rename_dim = NC_RO_rename_dim;
 
-    NC_RO_def_var,
-    NC4_inq_varid,
-    NC_RO_rename_var,
-    tst_get_vara,
-    NC_RO_put_vara,
-    NCDEFAULT_get_vars,
-    NCDEFAULT_put_vars,
-    NCDEFAULT_get_varm,
-    NCDEFAULT_put_varm,
+    tst_dispatcher.inq_att = NC4_inq_att;
+    tst_dispatcher.inq_attid = NC4_inq_attid;
+    tst_dispatcher.inq_attname = NC4_inq_attname;
+    tst_dispatcher.rename_att = NC_RO_rename_att;
+    tst_dispatcher.del_att = NC_RO_del_att;
+    tst_dispatcher.get_att = NC4_get_att;
+    tst_dispatcher.put_att = NC_RO_put_att;
 
-    NC4_inq_var_all,
+    tst_dispatcher.def_var = NC_RO_def_var;
+    tst_dispatcher.inq_varid = NC4_inq_varid;
+    tst_dispatcher.rename_var = NC_RO_rename_var;
+    tst_dispatcher.get_vara = tst_get_vara;
+    tst_dispatcher.put_vara = NC_RO_put_vara;
+    tst_dispatcher.get_vars = NCDEFAULT_get_vars;
+    tst_dispatcher.put_vars = NCDEFAULT_put_vars;
+    tst_dispatcher.get_varm = NCDEFAULT_get_varm;
+    tst_dispatcher.put_varm = NCDEFAULT_put_varm;
 
-    NC_NOTNC4_var_par_access,
-    NC_RO_def_var_fill,
+    tst_dispatcher.inq_var_all = NC4_inq_var_all;
 
-    NC4_show_metadata,
-    NC4_inq_unlimdims,
+    tst_dispatcher.var_par_access = NC_NOTNC4_var_par_access;
+    tst_dispatcher.def_var_fill = NC_RO_def_var_fill;
 
-    NC4_inq_ncid,
-    NC4_inq_grps,
-    NC4_inq_grpname,
-    NC4_inq_grpname_full,
-    NC4_inq_grp_parent,
-    NC4_inq_grp_full_ncid,
-    NC4_inq_varids,
-    NC4_inq_dimids,
-    NC4_inq_typeids,
-    NC4_inq_type_equal,
-    NC_NOTNC4_def_grp,
-    NC_NOTNC4_rename_grp,
-    NC4_inq_user_type,
-    NC4_inq_typeid,
+    tst_dispatcher.show_metadata = NC4_show_metadata;
+    tst_dispatcher.inq_unlimdims = NC4_inq_unlimdims;
 
-    NC_NOTNC4_def_compound,
-    NC_NOTNC4_insert_compound,
-    NC_NOTNC4_insert_array_compound,
-    NC_NOTNC4_inq_compound_field,
-    NC_NOTNC4_inq_compound_fieldindex,
-    NC_NOTNC4_def_vlen,
-    NC_NOTNC4_put_vlen_element,
-    NC_NOTNC4_get_vlen_element,
-    NC_NOTNC4_def_enum,
-    NC_NOTNC4_insert_enum,
-    NC_NOTNC4_inq_enum_member,
-    NC_NOTNC4_inq_enum_ident,
-    NC_NOTNC4_def_opaque,
-    NC_NOTNC4_def_var_deflate,
-    NC_NOTNC4_def_var_fletcher32,
-    NC_NOTNC4_def_var_chunking,
-    NC_NOTNC4_def_var_endian,
-    NC_NOTNC4_def_var_filter,
-    NC_NOTNC4_set_var_chunk_cache,
-    NC_NOTNC4_get_var_chunk_cache,
+    tst_dispatcher.inq_ncid = NC4_inq_ncid;
+    tst_dispatcher.inq_grps = NC4_inq_grps;
+    tst_dispatcher.inq_grpname = NC4_inq_grpname;
+    tst_dispatcher.inq_grpname_full = NC4_inq_grpname_full;
+    tst_dispatcher.inq_grp_parent = NC4_inq_grp_parent;
+    tst_dispatcher.inq_grp_full_ncid = NC4_inq_grp_full_ncid;
+    tst_dispatcher.inq_varids = NC4_inq_varids;
+    tst_dispatcher.inq_dimids = NC4_inq_dimids;
+    tst_dispatcher.inq_typeids = NC4_inq_typeids;
+    tst_dispatcher.inq_type_equal = NC4_inq_type_equal;
+    tst_dispatcher.def_grp = NC_NOTNC4_def_grp;
+    tst_dispatcher.rename_grp = NC_NOTNC4_rename_grp;
+    tst_dispatcher.inq_user_type = NC4_inq_user_type;
+    tst_dispatcher.inq_typeid = NC4_inq_typeid;
+
+    tst_dispatcher.def_compound = NC_NOTNC4_def_compound;
+    tst_dispatcher.insert_compound = NC_NOTNC4_insert_compound;
+    tst_dispatcher.insert_array_compound = NC_NOTNC4_insert_array_compound;
+    tst_dispatcher.inq_compound_field = NC_NOTNC4_inq_compound_field;
+    tst_dispatcher.inq_compound_fieldindex = NC_NOTNC4_inq_compound_fieldindex;
+    tst_dispatcher.def_vlen = NC_NOTNC4_def_vlen;
+    tst_dispatcher.put_vlen_element = NC_NOTNC4_put_vlen_element;
+    tst_dispatcher.get_vlen_element = NC_NOTNC4_get_vlen_element;
+    tst_dispatcher.def_enum = NC_NOTNC4_def_enum;
+    tst_dispatcher.insert_enum = NC_NOTNC4_insert_enum;
+    tst_dispatcher.inq_enum_member = NC_NOTNC4_inq_enum_member;
+    tst_dispatcher.inq_enum_ident = NC_NOTNC4_inq_enum_ident;
+    tst_dispatcher.def_opaque = NC_NOTNC4_def_opaque;
+    tst_dispatcher.def_var_deflate = NC_NOTNC4_def_var_deflate;
+    tst_dispatcher.def_var_fletcher32 = NC_NOTNC4_def_var_fletcher32;
+    tst_dispatcher.def_var_chunking = NC_NOTNC4_def_var_chunking;
+    tst_dispatcher.def_var_endian = NC_NOTNC4_def_var_endian;
+    tst_dispatcher.def_var_filter = NC_NOTNC4_def_var_filter;
+    tst_dispatcher.set_var_chunk_cache = NC_NOTNC4_set_var_chunk_cache;
+    tst_dispatcher.get_var_chunk_cache = NC_NOTNC4_get_var_chunk_cache;
 #if NC_DISPATCH_VERSION >= 3
-    NC_NOOP_inq_var_filter_ids,
-    NC_NOOP_inq_var_filter_info,
+    tst_dispatcher.inq_var_filter_ids = NC_NOOP_inq_var_filter_ids;
+    tst_dispatcher.inq_var_filter_info = NC_NOOP_inq_var_filter_info;
 #endif
 #if NC_DISPATCH_VERSION >= 4
-    NC_NOTNC4_def_var_quantize,
-    NC_NOTNC4_inq_var_quantize,
+    tst_dispatcher.def_var_quantize = NC_NOTNC4_def_var_quantize;
+    tst_dispatcher.inq_var_quantize = NC_NOTNC4_inq_var_quantize;
 #endif
 #if NC_DISPATCH_VERSION >= 5
-    NC_NOOP_inq_filter_avail,
+    tst_dispatcher.inq_filter_avail = NC_NOOP_inq_filter_avail;
 #endif
-};
 
-/* This is the dispatch object that holds pointers to all the
- * functions that make up the test dispatch interface with a bad
- * version number. */
-static NC_Dispatch tst_dispatcher_bad_version = {
-
-    NC_FORMATX_UDF0,
-    NC_DISPATCH_VERSION - 1,
-
-    NC_RO_create,
-    tst_open,
-
-    NC_RO_redef,
-    NC_RO__enddef,
-    NC_RO_sync,
-    tst_abort,
-    tst_close,
-    NC_RO_set_fill,
-    tst_inq_format,
-    tst_inq_format_extended,
-
-    NC4_inq,
-    NC4_inq_type,
-
-    NC_RO_def_dim,
-    NC4_inq_dimid,
-    HDF5_inq_dim,
-    NC4_inq_unlimdim,
-    NC_RO_rename_dim,
-
-    NC4_inq_att,
-    NC4_inq_attid,
-    NC4_inq_attname,
-    NC_RO_rename_att,
-    NC_RO_del_att,
-    NC4_get_att,
-    NC_RO_put_att,
-
-    NC_RO_def_var,
-    NC4_inq_varid,
-    NC_RO_rename_var,
-    tst_get_vara,
-    NC_RO_put_vara,
-    NCDEFAULT_get_vars,
-    NCDEFAULT_put_vars,
-    NCDEFAULT_get_varm,
-    NCDEFAULT_put_varm,
-
-    NC4_inq_var_all,
-
-    NC_NOTNC4_var_par_access,
-    NC_RO_def_var_fill,
-
-    NC4_show_metadata,
-    NC4_inq_unlimdims,
-
-    NC4_inq_ncid,
-    NC4_inq_grps,
-    NC4_inq_grpname,
-    NC4_inq_grpname_full,
-    NC4_inq_grp_parent,
-    NC4_inq_grp_full_ncid,
-    NC4_inq_varids,
-    NC4_inq_dimids,
-    NC4_inq_typeids,
-    NC4_inq_type_equal,
-    NC_NOTNC4_def_grp,
-    NC_NOTNC4_rename_grp,
-    NC4_inq_user_type,
-    NC4_inq_typeid,
-
-    NC_NOTNC4_def_compound,
-    NC_NOTNC4_insert_compound,
-    NC_NOTNC4_insert_array_compound,
-    NC_NOTNC4_inq_compound_field,
-    NC_NOTNC4_inq_compound_fieldindex,
-    NC_NOTNC4_def_vlen,
-    NC_NOTNC4_put_vlen_element,
-    NC_NOTNC4_get_vlen_element,
-    NC_NOTNC4_def_enum,
-    NC_NOTNC4_insert_enum,
-    NC_NOTNC4_inq_enum_member,
-    NC_NOTNC4_inq_enum_ident,
-    NC_NOTNC4_def_opaque,
-    NC_NOTNC4_def_var_deflate,
-    NC_NOTNC4_def_var_fletcher32,
-    NC_NOTNC4_def_var_chunking,
-    NC_NOTNC4_def_var_endian,
-    NC_NOTNC4_def_var_filter,
-    NC_NOTNC4_set_var_chunk_cache,
-    NC_NOTNC4_get_var_chunk_cache,
-#if NC_DISPATCH_VERSION >= 3
-    NC_NOOP_inq_var_filter_ids,
-    NC_NOOP_inq_var_filter_info,
-#endif
-#if NC_DISPATCH_VERSION >= 4
-    NC_NOTNC4_def_var_quantize,
-    NC_NOTNC4_inq_var_quantize,
-#endif
-#if NC_DISPATCH_VERSION >= 5
-    NC_NOOP_inq_filter_avail,
-#endif
-};
+    /* --- tst_dispatcher_bad_version (same but wrong ABI version) --- */
+    memcpy(&tst_dispatcher_bad_version, &tst_dispatcher,
+           sizeof(tst_dispatcher));
+    tst_dispatcher_bad_version.dispatch_version = NC_DISPATCH_VERSION - 1;
+}
 
 /* Simulate a self-registration init function that returns NC_Dispatch*. */
 static NC_Dispatch*
@@ -298,6 +212,7 @@ int mode[NUM_UDFS] = {NC_UDF0, NC_UDF1};
 int
 main(int argc, char **argv)
 {
+    init_dispatchers();
     printf("\n*** Testing user-defined formats.\n");
     printf("*** testing simple user-defined format...");
     {
