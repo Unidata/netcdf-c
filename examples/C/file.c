d3bae29a77a233ecbdd2bd06d1c23b8f63b31765
Ed Hartnett <edwardjameshartnett@gmail.com>
2026-02-19 04:43:55 -0700

    more doc fixes

--- examples/C/file.c
+++ examples/C/file.c
@@ -1,4 +1,6 @@
-/* Copyright 2018 University Corporation for Atmospheric
+/* Copyright 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
+   2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014,
+   2015, 2016, 2017, 2018 University Corporation for Atmospheric
    Research/Unidata.  See COPYRIGHT file for conditions of use. */
 /**
  * @file
