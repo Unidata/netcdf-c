3a58b908d04504c7f2e450b46d0296a3218107f1
Manuel Reis <reis@dkrz.de>
2026-08-11 10:38:34 +0200

    Test error log

--- nczarr_test/run_corrupt.sh
+++ nczarr_test/run_corrupt.sh
@@ -17,6 +17,7 @@ cd $ISOPATH
 export NCLOGGING=WARN
 
 testnoshape1() {
+  echo "*** testing no shape (zip)"
   zext=file
   rm -fr ./ref_noshape.file
   unzip ${srcdir}/ref_noshape.file.zip
@@ -26,12 +27,43 @@ testnoshape1() {
 }
 
 testnoshape2() {
+  echo "*** testing no shape (s3)"
   # Test against the original issue URL
   rm -f tmp_noshape2_gs.cdl
   fileurl="https://storage.googleapis.com/cmip6/CMIP6/CMIP/NASA-GISS/GISS-E2-1-G/historical/r1i1p1f1/day/tasmin/gn/v20181015/#mode=zarr,s3&aws.profile=no"
   ${NCDUMP} -h $flags $fileurl > tmp_noshape2_gs.cdl
 }
- 
+
+test_bad_dtype() {
+  echo "*** testing bad dtype"
+  name='bad_dtype'
+  mkdir -p "${name}.zarr/id"
+  cat > "${name}.zarr/.zgroup" <<-EOF
+{"zarr_format":2}
+EOF
+cat > "${name}.zarr/id/.zarray" <<-EOF
+{
+  "chunks": [225,265],
+  "compressor": null,
+  "dtype": "|O",
+  "fill_value": null,
+  "filters": null,
+  "order": "C",
+  "shape": [1799,1059],
+  "zarr_format": 2
+}
+EOF
+  set +e
+  "${NCDUMP}" -h "file://${name}.zarr#mode=zarr" > ncdump_dtype_output.txt 2>&1
+  ret=$?
+  set -e
+  if test $ret -ne 0 && grep -q 'ERR: Unsupported data type detected in variable "id" (dtype="|O").' ncdump_dtype_output.txt; then
+    return 0;
+  fi
+  return 1;
+}
+
+test_bad_dtype
 testnoshape1
 if test "x$FEATURE_S3TESTS" = xyes && test "x$FEATURE_S3_INTERNAL" = xyes ; then
     # The aws-sdk-cpp driver does not support google storage
