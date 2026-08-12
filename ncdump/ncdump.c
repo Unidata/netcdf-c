59ef239fa93f8861db29a4ac5e19fca8a451b359
Manuel Reis <reis@dkrz.de>
2026-08-12 10:26:16 +0200

    Allow NCLOGDEBUG to be used

--- ncdump/ncdump.c
+++ ncdump/ncdump.c
@@ -2389,15 +2389,13 @@ main(int argc, char *argv[])
 	      goto fail;
 	  }
 	  break;
-        case 'L':
-#ifdef LOGGING
-	  {
-	  int level = atoi(optarg);
-	  if(level >= 0)
-	    nc_set_log_level(level);
-	  }
+	    case 'L':
+	      int level = atoi(optarg); // atoi return 0 (NCLOGOFF) on error
+	      level = !level ? NCLOGNOTE : level; // If -L NCLOGNOTE is default
+#ifdef LOGGING // This is for parallel builds
+	      nc_set_log_level(level);
 #endif
-	  ncsetloglevel(NCLOGNOTE);
+	      ncsetloglevel(level);
 	  break;
 	case 'F':
 	  formatting_specs.filter_atts = true;
